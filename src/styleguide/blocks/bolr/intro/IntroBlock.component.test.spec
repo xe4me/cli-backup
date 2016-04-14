4aab5465173cce53a8c4d46558be9e7038028108
Eric Kwok <eric_kwok@amp.com.au>
2016-04-15 00:27:30 +1000

    DDC-101 Added galen test for Intro Block

--- src/styleguide/blocks/bolr/intro/IntroBlock.component.test.spec
+++ src/styleguide/blocks/bolr/intro/IntroBlock.component.test.spec
@@ -1,11 +1,45 @@
 @objects
+    bolr-intro                          css .bolr-intro
+    bolr-intro-logo                     css .bolr-intro-logo
+    bolr-intro-main                     css .bolr-intro-main
+        bolr-intro-main__title          css .bolr-intro-main__title
+        bolr-intro-main__body           css .bolr-intro-main__body
+        bolr-intro-main__notes          css .bolr-intro-main__notes
+        bolr-into-btn                   css [data-automation-id="btn_bolr-intro-block"]
 
-    textInput                     css [data-automation-id="text_firstname"]
-
-= Text Input base =
+= Bolr Intro Block base =
     @on *
-        = TextInput only has bottom border width solid =
-        textInput:
-            css border-width is "0px 0px 1px"
-            css border-style is "solid"
-            css border-color is "rgb(204, 210, 217)"
+        = Make sure that everything is inside the container =
+        bolr-intro-logo, bolr-intro-main:
+            inside bolr-intro
+
+        = Logo banner has the right height =
+        bolr-intro-logo:
+            height 80px
+            width 100 % of bolr-intro/width
+
+        = All body parts are left aligned =
+        bolr-intro-main.bolr-intro-main__title:
+            aligned vertically left bolr-intro-main.bolr-intro-main__body
+            aligned vertically left bolr-intro-main.bolr-intro-main__notes
+            aligned vertically left bolr-intro-main.bolr-into-btn
+
+        = Ordering of body parts =
+        bolr-intro-main.bolr-intro-main__title:
+            above bolr-intro-main.bolr-intro-main__body 60 px
+
+        bolr-intro-main.bolr-intro-main__body:
+            above bolr-intro-main.bolr-intro-main__notes 60 px
+
+        bolr-intro-main.bolr-intro-main__notes:
+            above bolr-intro-main.bolr-into-btn 60 to 63 px
+
+    @on laptop, desk
+        = Main body has left nav padding =
+        bolr-intro-main:
+            css margin-left is "280px"
+
+    @on tablet
+        = Main body has left nav padding =
+        bolr-intro-main:
+            css margin-left is "40px"
--- src/styleguide/blocks/bolr/intro/basic_usage.ts
+++ src/styleguide/blocks/bolr/intro/basic_usage.ts
@@ -1,20 +1,68 @@
-import {View, Component, AfterViewInit, ChangeDetectorRef} from 'angular2/core';
+import {View, Component, OnInit, ChangeDetectorRef, provide, ViewChild} from 'angular2/core';
 import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
 import {Control, CORE_DIRECTIVES, FORM_DIRECTIVES, FORM_PROVIDERS} from 'angular2/common';
 import {IntroBlockComponent} from '../../../../app/blocks/bolr/initState/IntroBlock.component';
-import {FormModelService} from "amp-ddc-ui-core/src/app/services/formModel.service";
+import {FormModelService} from 'amp-ddc-ui-core/src/app/services/formModel.service';
+import {Observable}     from 'rxjs/Observable';
+import {BlockID} from 'amp-ddc-ui-core/ui-core';
+
+export class MockFormModelService  {
+    private model = {
+        errors: [],
+        currentBlockID: new BlockID(null, 0),         // Defaults to the first block on the current page
+        context: {
+            licensee: 'LIC_CHARTER',
+            practicePrincipal_firstName: 'Darren',
+            practicePrincipal_lastName: 'Mink',
+            payeeId: 'BCABB-F',
+            practiceName: 'Pinnancle Financial pty ltd'
+        }
+    };
+
+    getContext() : Observable<string> {
+        return Observable.create(function (observer) {
+                observer.onNext({
+                    licensee: 'LIC_CHARTER',
+                    practicePrincipal_firstName: 'Darren',
+                    practicePrincipal_lastName: 'Mink',
+                    payeeId: 'BCABB-F',
+                    practiceName: 'Pinnancle Financial pty ltd'
+                });
+                observer.onCompleted();
+
+                return function() {
+                    //Clean up logic
+                };
+        });
+    }
+
+    present(data) {
+        this.model.currentBlockID = new BlockID(null, 1);
+    }
+
+    getModel() {
+        return this.model;
+    }
+}
+
 
 @Component({
     selector: 'bolr-intro-block-basic-usage',
-    providers: [FormModelService]
+    providers: [provide(FormModelService , {useClass: MockFormModelService})]
 })
 @View({
   templateUrl: 'src/styleguide/blocks/bolr/intro/basic_usage.html',
   styles: [require('./basic_usage').toString()],
   directives: [IntroBlockComponent]
 })
-export default class BOLRIntrolBlockBasicUsage {//implements AfterViewInit {
+export default class BOLRIntrolBlockBasicUsage implements OnInit {
+    @ViewChild(IntroBlockComponent)
+    private _introBlockComponent: IntroBlockComponent;
 
-  constructor (private _cd: ChangeDetectorRef) {  }
+    constructor (private _cd: ChangeDetectorRef) {  }
+
+    ngOnInit() {
+        setTimeout(() => this._introBlockComponent._id = new BlockID(null, 0), 0);
+    }
 
 }
