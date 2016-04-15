2a53dc47d1df5d2101e4585ee9b4f848bb062fe9
Eric Kwok <eric_kwok@amp.com.au>
2016-04-15 12:47:43 +1000

    DDC-111 Initial version of Partnership Manager, took 1.5 hrs

---
+++ src/app/blocks/bolr/partnership-manager/partnership-manager.component.ts
@@ -0,0 +1,99 @@
+import {FormBlock, NamedControl} from '../../formBlock';
+import {Component, ViewEncapsulation, OnInit, AfterViewInit, NgZone } from 'angular2/core';
+import {ThemeIDDirective} from '../../../directives/themeId.directive';
+import {FormModelService} from 'amp-ddc-ui-core/src/app/services/formModel.service';
+import {Control} from 'angular2/common';
+import {MdInputComponent} from '../../../components/my-md-input/my-md-input.component.ts';
+
+@Component ({
+  selector: 'partnership-manager-block',
+  template: `
+    <div class="partnership-manager-block" [class.hidden]='!isCurrentBlockActive()'>
+        <h3 class="heading heading-intro mb3">Who is your partnership manager?</h3>
+
+        <!--Partnership Manager name-->
+        <label class="heading heading-contxtual-label mb3" >My partnership manager is </label>
+
+        <!-- First name -->
+        <my-md-input
+            [id]="partnershipMgr.firstName.id"
+            [label]="partnershipMgr.firstName.label"
+            [parentControl]="formControl[0].control"
+            [isRequired]="true"
+            [valPattern]="partnershipMgr.firstName.regex"
+            [valMaxLength]="100">
+        </my-md-input>
+
+        <!-- Last name -->
+        <my-md-input
+            [id]="partnershipMgr.lastName.id"
+            [label]="partnershipMgr.lastName.label"
+            [parentControl]="formControl[1].control"
+            [isRequired]="true"
+            [valPattern]="partnershipMgr.lastName.regex"
+            [valMaxLength]="100">
+        </my-md-input>
+
+        <div class="alert alert-danger">
+          Message
+        </div>
+        <button class='btn btn--secondary btn-ok' (click)='ok()' data-automation-id="btn_partnership-manager-block">
+            OK
+        </button>
+    </div>
+  `,
+  // encapsulation: ViewEncapsulation.Emulated
+  inputs: ['partnershipMgr'],
+  styles: [require('./partnership-manager.component.scss').toString()],
+  directives: [MdInputComponent]
+})
+export class PartnershipManagerBlockComponent extends FormBlock {
+  static CLASS_NAME = 'PartnershipManagerBlockComponent';
+
+  partnershipMgr = {
+      firstName : {
+          id: 'default_fn',
+          label: 'First name',
+          regex: ''
+      },
+      lastName: {
+          id: 'default_ln',
+          label: 'Last name',
+          regex: ''
+      }
+  };
+
+  constructor(public formModelService: FormModelService) {
+      super();
+
+      this.formControl = [
+          new NamedControl(this.partnershipMgr.firstName.id, new Control()),
+          new NamedControl(this.partnershipMgr.lastName.id, new Control())
+      ];
+
+  }
+
+  // SAM - State representation of Model
+  public isCurrentBlockActive() {
+      if (this._id) {
+          return this.formModelService.getModel().currentBlockID.index === this._id.index ||
+                    this.formModelService.getModel().currentBlockID.index < this._id.index;
+      }
+      return true;
+  }
+
+  // TODO: Move this to the parent FormBlock class, as this should be common to all FormBlock components
+  public ok() {
+      // SAM - Action present data to Model
+      this.formModelService.present({
+         action: 'next',
+         blockId: this._id
+      });
+  }
+
+  public preBindControls(_formBlockDef) {
+      this.formControl[0].name = this.partnershipMgr.firstName.id;
+      this.formControl[1].name = this.partnershipMgr.lastName.id;
+  }
+
+}
---
+++ src/styleguide/blocks/bolr/partnership-manager/basic_usage.ts
@@ -0,0 +1,21 @@
+import {View, Component, OnInit, ChangeDetectorRef, provide, ViewChild} from 'angular2/core';
+import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
+import {Control, CORE_DIRECTIVES, FORM_DIRECTIVES, FORM_PROVIDERS} from 'angular2/common';
+import {PartnershipManagerBlockComponent} from '../../../../app/blocks/bolr/partnership-manager/partnership-manager.component';
+import {FormModelService} from 'amp-ddc-ui-core/src/app/services/formModel.service';
+import {Observable}     from 'rxjs/Observable';
+import {BlockID} from 'amp-ddc-ui-core/ui-core';
+import {MockFormModelService} from '../intro/basic_usage';
+
+@Component({
+    selector: 'partnership-manager-block-basic-usage',
+    providers: [provide(FormModelService , {useClass: MockFormModelService})]
+})
+@View({
+  templateUrl: 'src/styleguide/blocks/bolr/partnership-manager/basic_usage.html',
+  styles: [require('./basic_usage.scss').toString()],
+  directives: [PartnershipManagerBlockComponent]
+})
+export default class BOLRPartnershipManagerlBlockBasicUsage {
+    constructor (private _cd: ChangeDetectorRef) {  }
+}
