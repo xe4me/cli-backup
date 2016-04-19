fd1ada719d9b9cd481100d928f7e2e2d79796ade
Eric Kwok <eric_kwok@amp.com.au>
2016-04-20 00:54:42 +1000

    DDC-101 Licensee theme based

--- src/app/directives/themeId.directive.ts
+++ src/app/directives/themeId.directive.ts
@@ -1,6 +1,7 @@
 import {Directive, ElementRef, Input, Renderer} from 'angular2/core';
+import { FormModelService } from 'amp-ddc-ui-core/ui-core';
 
-export enum LICENSEE {AMPFP, HILLROSS, CHARTER};
+// export enum LICENSEE {AMPFP, HILLROSS, CHARTER};
 
 
 @Directive({
@@ -8,13 +9,13 @@ export enum LICENSEE {AMPFP, HILLROSS, CHARTER};
 })
 // Meant for generic theming identity but only have License theme use case at the moment.
 export class ThemeIDDirective {
-    themeId: LICENSEE = LICENSEE.CHARTER;
+    themeId: string;
 
-    constructor(el: ElementRef, renderer: Renderer) {
-      // this.themeId = LICENSEE.HILLROSS;
+    constructor(el: ElementRef, renderer: Renderer, formModelService: FormModelService) {
+      this.themeId = formModelService.getModel().context.licensee;
 
       // renderer.setElementStyle(el.nativeElement, 'background-color', 'yellow');
-      renderer.setElementClass(el.nativeElement, LICENSEE[this.themeId], true);
+      renderer.setElementClass(el.nativeElement, this.themeId, true);
       // Do not directly set the style on the nativeElement, use the Renderer for serverside rendering to work (https://github.com/angular/universal).
       //el.nativeElement.style.backgroundColor = 'yellow';
     }
