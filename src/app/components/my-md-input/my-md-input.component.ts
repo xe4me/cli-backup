d41faeb046376ee2c1a03a5744896736e653b757
milad <smiladhi@gmail.com>
2016-04-20 09:19:16 +1000

    Added some white spaces to make linter happy

--- src/app/components/my-md-input/my-md-input.component.ts
+++ src/app/components/my-md-input/my-md-input.component.ts
@@ -4,7 +4,7 @@ import { Action } from 'amp-ddc-ui-core/src/app/actions/action';
 import { MATERIAL_DIRECTIVES , MATERIAL_PROVIDERS } from 'ng2-material/all';
 import { AmpFitWidthToText } from '../../directives/amp-fit-width-to-text.directive';
 // TODO: Work out how to disable mdMaxLength and mdPattern when they are not set
-@Component (
+@Component(
     {
         selector      : 'my-md-input' ,
         template      : `
@@ -27,9 +27,19 @@ import { AmpFitWidthToText } from '../../directives/amp-fit-width-to-text.direct
         <ng-content></ng-content>
   </md-input-container>
   ` ,
-        styles        : [ require ( './my-md-input.scss' ).toString () ] ,
-        inputs        : [ 'id' , 'isInSummaryState' , 'label' , 'parentControl' , 'placeholder' , 'visibility' , 'valMaxLength' , 'valPattern' , 'isRequired' ] ,
-        directives    : [ MATERIAL_DIRECTIVES , CORE_DIRECTIVES , FORM_DIRECTIVES ,AmpFitWidthToText] ,
+        styles        : [ require( './my-md-input.scss' ).toString() ] ,
+        inputs        : [
+            'id' ,
+            'isInSummaryState' ,
+            'label' ,
+            'parentControl' ,
+            'placeholder' ,
+            'visibility' ,
+            'valMaxLength' ,
+            'valPattern' ,
+            'isRequired'
+        ] ,
+        directives    : [ MATERIAL_DIRECTIVES , CORE_DIRECTIVES , FORM_DIRECTIVES , AmpFitWidthToText ] ,
         encapsulation : ViewEncapsulation.Emulated
     } )
 export class MdInputComponent {
@@ -41,11 +51,11 @@ export class MdInputComponent {
     private visibility : Action;
     private model : any;
 
-    set id ( id : string ) {
+    set id( id : string ) {
         this._id = id;
     }
 
-    set isRequired ( val : string ) {
+    set isRequired( val : string ) {
         if ( val === 'true' ) {
             // Note that you can compose an Array of validators via the Validators.compose(validators: Function[]) :
             // Function API
--- src/app/directives/amp-fit-width-to-text.directive.ts
+++ src/app/directives/amp-fit-width-to-text.directive.ts
@@ -1,10 +1,10 @@
 import { Directive , ElementRef , Input , Renderer } from 'angular2/core';
-;@Directive( {
-                 selector : '[fitWidthToText]' ,
-                host:{
+@Directive( {
+                selector : '[fitWidthToText]' ,
+                host     : {
                     '(blur)' : 'onBlur()'
                 }
-             } ) // Meant for generic theming identity but only have License theme use case at the moment.
+            } ) // Meant for generic theming identity but only have License theme use case at the moment.
 export class AmpFitWidthToText {
     constructor( private el : ElementRef , private renderer : Renderer ) {
 
@@ -14,8 +14,7 @@ export class AmpFitWidthToText {
     }
 
     private onBlur() {
-        let width = ((this.el.nativeElement.value.length + 1) * 8) + 20+'px';
-        console.log( 'width' , width );
+        let width = ((this.el.nativeElement.value.length + 1) * 8) + 20 + 'px';
         this.renderer.setElementStyle( this.el.nativeElement , 'width' , width );
     }
 }
