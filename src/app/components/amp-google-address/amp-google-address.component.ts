6074adba3848d99a6f8224b72e02f922cbede3a5
Eric Kwok <eric_kwok@amp.com.au>
2016-05-02 15:56:15 +1000

    DDC-120 Updated the Google address to provide information on the predictions

--- src/app/components/amp-google-address/amp-google-address.component.ts
+++ src/app/components/amp-google-address/amp-google-address.component.ts
@@ -1,73 +1,85 @@
-import {Component, Directive, Input, OnInit, ViewEncapsulation, NgZone, AfterViewInit} from 'angular2/core';
+import {Component, Directive, Input, OnInit, ViewEncapsulation, NgZone, AfterViewInit, ChangeDetectorRef} from 'angular2/core';
 import {Control, Validators, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
+import {FormBlock, NamedControl} from '../../blocks/formBlock';
 import {Action} from 'amp-ddc-ui-core/src/app/actions/action';
-import {MATERIAL_DIRECTIVES, MATERIAL_PROVIDERS} from 'ng2-material/all';
+import {MdInputComponent} from '../../components/my-md-input/my-md-input.component';
 
 declare var google: any;
 
 @Component({
     selector: 'amp-google-address',
     template: `
-    <md-input-container [class.md-input-has-value]="parentControl.value" [ngClass]="{'md-input-has-placeholder' : placeholder}" flex-gt-sm="" *ngIf="!visibility || visibility.invoke()">
-        <label *ngIf="label && !parentControl.value" [attr.for]="_id">{{label}}</label>
-        <input
-            class="md-input"
-            md-input
-            mdMaxLength="{{valMaxLength}}"
-            mdPattern="{{valPattern}}"
-            [attr.name]="_id"
-            [attr.id]="_id"
-            [attr.data-automation-id]="'text_' + _id"
-            [ngFormControl]="parentControl"
-            [attr.placeholder]="placeholder"/>
-        <ng-content></ng-content>
-    </md-input-container>
+    <my-md-input [id]='id' [label]='label' [parentControl]='parentControl' [placeholder]='placeholder'
+        isRequired='true'
+        valPattern='{{valPattern}}'
+        valMaxLength='{{valMaxLength}}'
+        (input)='showManualAddrOpt()'>
+    </my-md-input>
+    <div class='errors mt' *ngIf='!addrPredictions'>
+        <span class='icon icon--close icon-errors'></span>We are unable to find your address. Try again or tell us your address line by line <a href='' (click)='showManualAddrForm()'>here</a> instead.
+    </div>
+
     `,
     styles: [require('./amp-google-address.component.scss').toString()],
     inputs: ['id', 'label', 'parentControl', 'placeholder', 'visibility', 'valMaxLength', 'valPattern', 'isRequired'],
-    directives: [MATERIAL_DIRECTIVES, CORE_DIRECTIVES, FORM_DIRECTIVES],
+    directives: [MdInputComponent],
     encapsulation: ViewEncapsulation.Emulated
 })
 
 export class AMPGoogleAddressComponent implements AfterViewInit {
     static CLASS_NAME = 'AMPGoogleAddressComponent';
 
-    private _id: string;
+    private id: string;
     private label: string;
     private parentControl: Control;
     private placeholder: string = '';
     private visibility: Action;
     private model: any;
+    private autocomplete: any;
+    private addrPredictions: any = {};
 
-    constructor (private zone: NgZone) {}
-
-    set id(id: string) {
-        this._id = id;
-    }
-
-    set isRequired(val: string) {
-
-        if (val === 'true') {
-            // Note that you can compose an Array of validators via the Validators.compose(validators: Function[]) : Function API
-            this.parentControl.validator = Validators.required;
-        }
-    }
+    constructor (private zone: NgZone, public _cd: ChangeDetectorRef) {}
 
     ngAfterViewInit () {
       // Binding Google Places Address api to google_places_ac input field
-      var input : any = document.getElementById('PracticeAddress');
+      var input : any = document.getElementById(this.id);
       var options = {
-        //   types: [],
+          types: ['address'],
           componentRestrictions: {country: 'au'}
       };
-      var autocomplete = new google.maps.places.Autocomplete(input, options);
 
-      google.maps.event.addListener(autocomplete, 'place_changed', () => {
+      // TODO: Remove the UI components as we are using the AutocompleteService programmatically.
+      this.autocomplete = new google.maps.places.Autocomplete(input, options);
+
+      google.maps.event.addListener(this.autocomplete, 'place_changed', () => {
         this.zone.run(() => {
-          var place = autocomplete.getPlace();
-          this.model[3].parentControl.updateValue(place.formatted_address);
-          // console.log('place change');
+          var place = this.autocomplete.getPlace();
+          this.parentControl.updateValue(place.formatted_address);
         });
       });
+    }
+
+    showManualAddrOpt () {
+        var service = new google.maps.places.AutocompleteService();
+        if (this.parentControl.value) {
+            service.getPlacePredictions(
+                {
+                    input: this.parentControl.value,
+                    types: ['address'],
+                    componentRestrictions: {country: 'au'}
+                },
+                (predictions, status) => {
+                    if (status == google.maps.places.PlacesServiceStatus.OK ||
+                        status == google.maps.places.PlacesServiceStatus.ZERO_RESULTS) {
+                            this.addrPredictions = predictions;
+                            this._cd.detectChanges();
+                    }
+                });
+            }
+    }
+
+    showManualAddrForm() {
+        console.log("Close Google and open Manual entry option");
+        return false;
     }
 }
