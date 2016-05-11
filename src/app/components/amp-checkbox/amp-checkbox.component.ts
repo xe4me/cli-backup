abac5841e30195f270f079405de0faf505a75159
milad <smiladhi@gmail.com>
2016-05-11 16:00:00 +1000

    Finished Acknowledgement block with the checkbox and everything

--- src/app/components/amp-checkbox/amp-checkbox.component.ts
+++ src/app/components/amp-checkbox/amp-checkbox.component.ts
@@ -1,38 +1,45 @@
-import { Component , ElementRef , EventEmitter , Directive , Input , OnInit , ViewEncapsulation } from 'angular2/core';
-import { Control , Validators , CORE_DIRECTIVES , FORM_DIRECTIVES } from 'angular2/common';
-import { Action } from 'amp-ddc-ui-core/src/app/actions/action';
-import { MATERIAL_DIRECTIVES , MATERIAL_PROVIDERS } from 'ng2-material/all';
+import {
+    Component ,
+    ElementRef ,
+    EventEmitter ,
+} from 'angular2/core';
+import { Control , CORE_DIRECTIVES , FORM_DIRECTIVES } from 'angular2/common';
 import { ScrollService } from 'amp-ddc-ui-core/ui-core';
+import { NumberWrapper } from 'angular2/src/facade/lang';
+import { isPresent } from 'angular2/src/facade/lang';
+import { AfterViewInit } from 'angular2/src/core/linker/interfaces';
+export class KeyCodes {
+    static ESCAPE : number;
+    static SPACE : number;
+    static UP : number;
+    static DOWN : number;
+}
 @Component(
     {
         selector   : 'amp-checkbox' ,
         template   : `
             <input
                 [disabled]='disabled'
-                [required]='required'
-                [attr.data-automation-id]='"checkbox_" + id'
+                [checked]='checked'
                 type='checkbox'
+                [attr.data-automation-id]='"checkbox_" + id'
                 [attr.id]='id'
                 [attr.name]='id'
-                [ngFormControl]='parentControl'
-                [value]='value'/>
-          <label [attr.for]='id'>
-                <div class="container">
+                [ngFormControl]='parentControl'/>
+          <label (click)='onSelect($event)' [attr.for]='id'>
+                <div [class.hidden]='isInSummaryState' class="container">
                     <div class="off"></div>
                     <div class="on"></div>
                 </div>
-                <div class="content">
+                <div [class.summary-state]='isInSummaryState' class="content">
                     <ng-content></ng-content>
                 </div>
           </label>             
         ` ,
         host       : {
-            'role'                 : 'checkbox' ,
             '[attr.aria-checked]'  : 'checked' ,
             '[attr.aria-disabled]' : 'disabled' ,
             '[tabindex]'           : 'tabindex' ,
-            '(keydown)'            : 'onKeydown($event)' ,
-            '(click)'              : 'onSelect($event)'
         } ,
         styles     : [ require( './amp-checkbox.scss' ).toString() ] ,
         directives : [ CORE_DIRECTIVES , FORM_DIRECTIVES ] ,
@@ -42,37 +49,102 @@ import { ScrollService } from 'amp-ddc-ui-core/ui-core';
             'scrollOutOn' ,
             'disabled' ,
             'parentControl' ,
-            'buttons' ,
+            'checked' ,
             'id' ,
-            'autoSelectOnOne' ,
-            'value'
+            'tabindex' ,
+            'isInSummaryState'
         ] ,
         outputs    : [ 'select' ]
     } )
-export class AmpCheckboxComponent {
+export class AmpCheckboxComponent implements AfterViewInit {
+    private _disabled : boolean            = false;
+    private _checked : boolean             = false;
+    private _required : boolean            = false;
+    private isInSummaryState : boolean     = false;
+    private _tabindex : number;
     private parentControl : Control;
-    private select                 = new EventEmitter<string>();
     private scrollOutUnless : any;
     private scrollOutOn : any;
     private id : string;
-    private previousValue : string = null;
+    private select : EventEmitter<boolean> = new EventEmitter<boolean>( false );
+    private checkboxValue : boolean        = false;
 
     constructor ( private elem : ElementRef ,
                   private scrollService : ScrollService ) {
     }
 
+    ngAfterViewInit () : any {
+        this.updateValitators();
+        return undefined;
+    }
+
+    set tabindex ( value : number ) {
+        this._tabindex = this.parseTabIndexAttribute( value );
+    }
+
+    parseTabIndexAttribute ( attr : any ) : number {
+        return isPresent( attr ) ? NumberWrapper.parseInt( attr , 10 ) : 0;
+    }
+
+    get tabindex () : number {
+        return this._tabindex;
+    }
+
+    get disabled () {
+        return this._disabled;
+    }
+
+    set disabled ( value ) {
+        this._disabled = this.isTrue( value );
+    }
+
+    get required () {
+        return this._required;
+    }
+
+    set required ( value ) {
+        this._required = this.isTrue( value );
+        this.updateValitators();
+    }
+
+    get checked () {
+        return this._checked;
+    }
+
+    set checked ( value ) {
+        this._checked      = this.isTrue( value );
+        this.checkboxValue = this._checked;
+        this.parentControl.updateValue( this._checked );
+    }
+
     private onSelect ( $event ) {
-        let value = $event.target.value;
-        console.log( 'onSelect inside' , value );
-        if ( this.previousValue !== value ) {
-            this.previousValue = value;
-            this.select.emit( value + '' );
+        if ( this.disabled === true ) {
+            $event.stopPropagation();
+            return;
         }
-        if ( this.scrollOutUnless && value !== this.scrollOutUnless ) {
+        this.checkboxValue = ! this.checkboxValue;
+        this.select.emit( this.checkboxValue );
+        if ( this.scrollOutUnless && this.checkboxValue !== this.scrollOutUnless ) {
             this.scrollService.scrollMeOut( this.elem , 'easeInQuad' , 60 );
-        } else if ( this.scrollOutOn && value === this.scrollOutOn ) {
+        } else if ( this.scrollOutOn && this.checkboxValue === this.scrollOutOn ) {
             this.scrollService.scrollMeOut( this.elem , 'easeInQuad' , 60 );
         }
     }
-}
 
+    private requiredValidation ( c : Control ) {
+        return c.value === true ? null : {
+            checkboxrequired : true
+        };
+    }
+
+    private isTrue ( value ) {
+        return isPresent( value ) && (value === true || value === "true" || false);
+    }
+
+    private updateValitators () {
+        if ( this.parentControl ) {
+            this.parentControl.validator = this.isTrue( this.required ) ? this.requiredValidation : null;
+            this.parentControl.updateValueAndValidity( { emitEvent : true , onlySelf : false } );
+        }
+    }
+}
