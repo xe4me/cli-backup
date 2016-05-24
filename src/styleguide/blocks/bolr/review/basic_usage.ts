385da29343f66beebc1626d081601fa97d1f19df
milad <smiladhi@gmail.com>
2016-05-24 15:26:48 +1000

    Prettify amp group button usage in the styleguide

---
+++ src/styleguide/blocks/bolr/review/basic_usage.ts
@@ -0,0 +1,73 @@
+import { View , Component , OnInit , ChangeDetectorRef , provide , ViewChild } from 'angular2/core';
+import { MATERIAL_DIRECTIVES } from 'ng2-material/all';
+import { Control , CORE_DIRECTIVES , FORM_DIRECTIVES , FORM_PROVIDERS } from 'angular2/common';
+import { ReviewBlockComponent } from '../../../../app/blocks/bolr/review/review-block.component.ts';
+import { FormModelService } from 'amp-ddc-ui-core/ui-core';
+import { Observable }     from 'rxjs/Observable';
+import { BlockID } from 'amp-ddc-ui-core/ui-core';
+import { ControlGroup } from 'angular2/src/common/forms/model';
+export class MockFormModelService {
+    private model = {
+        errors         : [] ,
+        currentBlockID : new BlockID( null , 0 ) ,         // Defaults to the first block on the current page
+        context        : {
+            licensee                   : 'DEA_CHARTER' ,
+            practicePrincipalFirstName : 'Darren' ,
+            practicePrincipalLastName  : 'Mink' ,
+            payeeId                    : 'BCABB-F' ,
+            practiceName               : 'Pinnancle Financial pty ltd'
+        } ,
+        flags          : {
+            introIsDone : false
+        }
+    };
+
+    getFlags ( flag ) {
+        return this.model.flags[ flag ];
+    }
+
+    getContext () : Observable<string> {
+        return Observable.create( function( observer ) {
+            observer.onNext( {
+                licensee                   : 'DEA_CHARTER' ,
+                practicePrincipalFirstName : 'Darren' ,
+                practicePrincipalLastName  : 'Mink' ,
+                payeeId                    : 'BCABB-F' ,
+                practiceName               : 'Pinnancle Financial pty ltd'
+            } );
+            observer.onCompleted();
+            return function() {
+                //Clean up logic
+            };
+        } );
+    }
+
+    present ( data ) {
+        this.model.currentBlockID = new BlockID( null , 1 );
+    }
+
+    getModel () {
+        return this.model;
+    }
+}
+@Component( {
+    selector  : 'bolr-review-block-basic-usage' ,
+    providers : [ provide( FormModelService , { useClass : MockFormModelService } ) ]
+} )
+@View( {
+    templateUrl : 'src/styleguide/blocks/bolr/review/basic_usage.html' ,
+    styles      : [ require( './basic_usage' ).toString() ] ,
+    directives  : [ ReviewBlockComponent ]
+} )
+
+export default class BOLRReviewBlockBasicUsage implements OnInit {
+    @ViewChild( ReviewBlockComponent )
+    private _reviewBlockComponent : ReviewBlockComponent;
+    
+    constructor ( private _cd : ChangeDetectorRef ) {
+    }
+
+    ngOnInit () {
+        setTimeout( () => this._reviewBlockComponent._id = new BlockID( null , 0 ) , 0 );
+    }
+}
---
+++ src/styleguide/components/amp-group-button/amp-group-button.spec.ts
@@ -0,0 +1,167 @@
+import {
+    it ,
+    injectAsync ,
+    describe ,
+    beforeEachProviders ,
+    TestComponentBuilder
+} from 'angular2/testing';
+import { FormModelService , ProgressObserverService , ScrollService } from 'amp-ddc-ui-core/ui-core';
+import { Component , provide , ElementRef } from 'angular2/core';
+import { Control } from 'angular2/common';
+import { AmpGroupButtonComponent } from '../../../../src/app/components/amp-group-button/amp-group-button.component';
+import { MockScrollService } from '../../../styleguide/blocks/bolr/dialogue/contact-details/mock-scroll.service';
+import { MockFormModelService } from '../../../styleguide/blocks/bolr/dialogue/contact-details/mock-form-mode.service';
+describe( 'amp-group-button directive' , () => {
+    beforeEachProviders( () => {
+        return [
+            provide( FormModelService , { useClass : MockFormModelService } ) ,
+            provide( MockFormModelService , { useClass : MockFormModelService } ) ,
+            provide( ElementRef , { useClass : ElementRef } ) ,
+            provide( ScrollService , { useClass : MockScrollService } ) ,
+            provide( ProgressObserverService , { useClass : ProgressObserverService } ) ,
+            provide( MockScrollService , { useClass : MockScrollService } ) ,
+            provide( Window , { useValue : window } )
+        ];
+    } );
+    @Component( {
+        template   : `
+      <form  #formModel='ngForm' class='nl-form' >
+        <amp-group-button
+                scrollOutOn='full'
+                class='1/5'
+                (select)='onButtonClick($event)'
+                [buttons]='fullOrPartialButtons.buttons'
+                [parentControl]='control'
+                [groupName]='fullOrPartialButtons.fullOrPartial'
+        >
+        </amp-group-button>
+      </form>
+      ` ,
+        directives : [ AmpGroupButtonComponent ]
+    } )
+    class AmpGroupButtonTest {
+        control : Control            = new Control();
+        private fullOrPartialButtons = {
+            buttons       : [
+                {
+                    id    : 'fullId' ,
+                    value : 'full' ,
+                    label : 'Full sale'
+                } ,
+                {
+                    id    : 'partialId' ,
+                    value : 'partial' ,
+                    label : 'Partial sale'
+                }
+            ] ,
+            fullOrPartial : 'fullOrPartial'
+        };
+
+        private onButtonClick () {
+        }
+    }
+    it( 'Should contain 2 radio input field  with proper data-automation-id and name attributes ' ,
+        injectAsync( [
+            TestComponentBuilder ,
+            ProgressObserverService ,
+            ElementRef ,
+            FormModelService ,
+            ScrollService
+        ] , ( tcb , progressObserver , el , formModelService , scrollService ) => {
+            return tcb
+                .createAsync( AmpGroupButtonTest )
+                .then( ( fixture : any ) => {
+                    fixture.detectChanges();
+                    const Element = fixture.nativeElement;
+                    //let AmpGroupButtonTest = fixture.debugElement;
+                    let Form      = Element.querySelector( 'form' );
+                    let Inputs    = Element.querySelectorAll( 'input' );
+                    expect( Inputs[ '0' ].name ).toBe( 'fullOrPartial' );
+                    expect( Inputs[ '0' ].getAttribute( 'data-automation-id' ) ).toBe( 'radio_button_fullId' );
+                    expect( Inputs.length ).toBe( 2 );
+                } );
+        } ) );
+    it( 'Should contain 2 label field with proper text ' ,
+        injectAsync( [
+            TestComponentBuilder ,
+            ProgressObserverService ,
+            ElementRef ,
+            FormModelService ,
+            ScrollService
+        ] , ( tcb , progressObserver , el , formModelService , scrollService ) => {
+            return tcb
+                .createAsync( AmpGroupButtonTest )
+                .then( ( fixture : any ) => {
+                    fixture.detectChanges();
+                    const Element = fixture.nativeElement;
+                    let Labels    = Element.querySelectorAll( 'label' );
+                    expect( Labels.length ).toBe( 2 );
+                    expect( Labels[ '0' ].innerHTML ).toBe( 'Full sale' );
+                    expect( Labels[ '1' ].innerHTML ).toBe( 'Partial sale' );
+                } );
+        } ) );
+    it( 'Should update component control value to full after clicking on Full button' ,
+        injectAsync( [
+            TestComponentBuilder ,
+            ProgressObserverService ,
+            ElementRef ,
+            FormModelService ,
+            ScrollService
+        ] , ( tcb , progressObserver , el , formModelService , scrollService ) => {
+            return tcb
+                .createAsync( AmpGroupButtonTest )
+                .then( ( fixture : any ) => {
+                    fixture.detectChanges();
+                    const Element   = fixture.nativeElement;
+                    const Component = fixture.componentInstance;
+                    let Labels      = Element.querySelectorAll( 'label' );
+                    Labels[ '0' ].click();
+                    fixture.detectChanges();
+                    expect( Component.control.value ).toBe( 'full' );
+                    expect( Component.control.value ).not.toBe( 'partial' );
+                } );
+        } ) );
+    it( 'Should make the Full button checked after the Component programmatically updated its control value' ,
+        injectAsync( [
+            TestComponentBuilder ,
+            ProgressObserverService ,
+            ElementRef ,
+            FormModelService ,
+            ScrollService
+        ] , ( tcb , progressObserver , el , formModelService , scrollService ) => {
+            return tcb
+                .createAsync( AmpGroupButtonTest )
+                .then( ( fixture : any ) => {
+                    fixture.detectChanges();
+                    const Element   = fixture.nativeElement;
+                    const Component = fixture.componentInstance;
+                    let Inputs      = Element.querySelectorAll( 'input' );
+                    Component.control.updateValue( 'full' );
+                    fixture.detectChanges();
+                    expect( Inputs[ '0' ].checked ).toBeTruthy();
+                    expect( Inputs[ '1' ].checked ).toBeFalsy();
+                } );
+        } ) );
+    it( 'Should make all the inputs unchecked ' ,
+        injectAsync( [
+            TestComponentBuilder ,
+            ProgressObserverService ,
+            ElementRef ,
+            FormModelService ,
+            ScrollService
+        ] , ( tcb , progressObserver , el , formModelService , scrollService ) => {
+            return tcb
+                .createAsync( AmpGroupButtonTest )
+                .then( ( fixture : any ) => {
+                    fixture.detectChanges();
+                    const Element   = fixture.nativeElement;
+                    const Component = fixture.componentInstance;
+                    let Inputs      = Element.querySelectorAll( 'input' );
+                    Component.control.updateValue( 'null' );
+                    fixture.detectChanges();
+                    for ( let i = 0 ; i < Inputs.length ; i ++ ) {
+                        expect( Inputs[ i ].checked ).toBeFalsy();
+                    }
+                } );
+        } ) );
+} );
