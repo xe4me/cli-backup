6169be4f35139620ddd3aa21a0904f58bdc4be51
Eric Kwok <eric_kwok@amp.com.au>
2016-06-03 18:39:45 +1000

    DDC-108 Fixed Full list of impact adviser name in the review block

--- src/app/blocks/bolr/notification-form/review/review-block.component.ts
+++ src/app/blocks/bolr/notification-form/review/review-block.component.ts
@@ -111,7 +111,7 @@ import { AmpButton } from '../../../../components/amp-button/amp-button.componen
                                             <span *ngIf=' i < ( advisers.length - 1 ) && i >0 '> , </span>
                                             <span *ngIf=' i === ( advisers.length - 1 ) '> and </span>
                                         </span>
-                                        {{ item.adviserName }} ({{ item.adviserId }})
+                                        {{ item.firstName }} {{ item.lastName }} ({{ item.ownernum }})
                                     </span>
                                     will be impacted by this decision.
                                 </div>
