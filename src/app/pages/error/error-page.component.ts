17560e75fc60fc5b5316db638538af2b14313a5b
tamas <Tamas_Ridly@amp.com.au>
2016-06-27 10:51:33 +1000

    Added error page

---
+++ src/app/pages/error/error-page.component.ts
@@ -0,0 +1,37 @@
+import { FormPage } from '../formPage';
+import { Component } from '@angular/core';
+import { ThemeIDDirective } from '../../directives/themeId.directive';
+import { Router , CanDeactivate , ComponentInstruction } from '@angular/router-deprecated';
+@Component( {
+    selector : 'error-page' ,
+    template : `
+        <div class='error-page'>
+        <div class='error-page_logo'></div>
+
+            <div class='error-page_message'>
+                <h3>Sorry, something went wrong!</h3>
+                <br>
+                {{message}}
+            </div>
+
+        </div>
+    ` ,
+    styles   : [ require( './error-page.component.scss' ).toString() ],
+    directives: [ThemeIDDirective],
+    inputs:['message']
+} )
+export class ErrorPageComponent extends FormPage implements CanDeactivate {
+    static CLASS_NAME = 'ErrorPageComponent';
+    public content    = {
+        id    : 'DefaultContentId' ,
+        label : 'Default content label'
+    };
+
+    constructor ( public _router : Router ) {
+        super();
+    }
+
+    routerCanDeactivate ( next : ComponentInstruction , prev : ComponentInstruction ) : any {
+        return false;
+    }
+}
