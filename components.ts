097508383cacd7336cbaf82f37721b9c059bc859
Iain Todd <iain_todd@amp.com.au>
2016-07-19 09:55:56 +1000

    Adding components to components.ts

--- components.ts
+++ components.ts
@@ -1,1 +1,6 @@
-export { AmpDropdownComponent } from './src/app/components/amp-dropdown/amp-dropdown.component';
+export { AmpButton }                    from './src/app/components/amp-button/amp-button.component';
+export { AmpDropdownComponent }	        from './src/app/components/amp-dropdown/amp-dropdown.component';
+export { AMPGoogleAddressComponent }	from './src/app/components/amp-google-address/amp-google-address.component.ts';
+export { AmpGroupButtonComponent }	    from './src/app/components/amp-group-button/amp-group-button.component';
+export { InputWithLabelGroupComponent }	from './src/app/component-groups/input-with-label-group/input-with-label-group.component';
+export { MdInputComponent }	      	    from './src/app/components//my-md-input/my-md-input.component.ts';
