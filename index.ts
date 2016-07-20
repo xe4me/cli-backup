168e80a9988dea0ce67ef02072b6d9f9b9e6a672
Iain Todd <iain_todd@amp.com.au>
2016-07-20 11:54:19 +1000

    Reordering directives and components for import

--- index.ts
+++ index.ts
@@ -1,4 +1,12 @@
+// directives
+export * from './src/app/directives/clicked-outside/clicked-outside.directive'
+export * from './src/app/directives/amp-sticky-on-scroll.directive'
+export * from './src/app/directives/themeId.directive';
+// components
 export * from './src/app/components/amp-button/amp-button.component';
+export * from './src/app/components/amp-group-button/amp-group-button.component';
+export * from './src/app/components/my-input/my-input.component';
+export * from './src/app/components/my-md-input/my-md-input.component';
 export * from './src/app/components/amp-checkbox/amp-checkbox.component';
 export * from './src/app/components/amp-default-router/amp-default-router.component';
 export * from './src/app/components/amp-dropdown/amp-dropdown.component';
@@ -9,11 +17,5 @@ export * from './src/app/components/amp-linear-progress-bar/amp-linear-progress-
 export * from './src/app/components/amp-overlay/amp-overlay.component';
 export * from './src/app/components/amp-radio-button-group/amp-radio-button-group.component';
 export * from './src/app/components/amp-textarea/amp-textarea.component';
-export * from './src/app/components/my-input/my-input.component';
-export * from './src/app/components/my-md-input/my-md-input.component';
 export * from './src/app/component-groups/amp-google-address-group/amp-google-address-group.component';
 export * from './src/app/component-groups/input-with-label-group/input-with-label-group.component';
-//directives
-export * from './src/app/directives/clicked-outside/clicked-outside.directive'
-export * from './src/app/directives/amp-sticky-on-scroll.directive'
-export * from './src/app/directives/themeId.directive';
