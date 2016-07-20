235e604ea64cda3454ced50d60730edbd831d25d
Iain Todd <iain_todd@amp.com.au>
2016-07-20 13:42:47 +1000

    Updating index.ts again

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ export * from './src/app/directives/clicked-outside/clicked-outside.directive'
 export * from './src/app/directives/amp-sticky-on-scroll.directive'
 export * from './src/app/directives/themeId.directive';
 // components
+export * from './src/app/components/amp-overlay/amp-overlay.component';
 export * from './src/app/components/amp-button/amp-button.component';
 export * from './src/app/components/my-input/my-input.component';
 export * from './src/app/components/my-md-input/my-md-input.component';
@@ -13,7 +14,6 @@ export * from './src/app/components/amp-fields-validation/amp-validation-message
 export * from './src/app/components/amp-google-address/amp-google-address.component';
 export * from './src/app/components/amp-group-button/amp-group-button.component';
 export * from './src/app/components/amp-linear-progress-bar/amp-linear-progress-bar.component';
-export * from './src/app/components/amp-overlay/amp-overlay.component';
 export * from './src/app/components/amp-radio-button-group/amp-radio-button-group.component';
 export * from './src/app/components/amp-textarea/amp-textarea.component';
 export * from './src/app/components/amp-group-button/amp-group-button.component';
