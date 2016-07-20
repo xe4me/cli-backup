a9f555e7c67fcc2624c9fffe515737343731ced7
Iain Todd <iain_todd@amp.com.au>
2016-07-20 12:01:44 +1000

    Another update to index.ts for button group

--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@ export * from './src/app/directives/amp-sticky-on-scroll.directive'
 export * from './src/app/directives/themeId.directive';
 // components
 export * from './src/app/components/amp-button/amp-button.component';
-export * from './src/app/components/amp-group-button/amp-group-button.component';
 export * from './src/app/components/my-input/my-input.component';
 export * from './src/app/components/my-md-input/my-md-input.component';
 export * from './src/app/components/amp-checkbox/amp-checkbox.component';
@@ -17,5 +16,6 @@ export * from './src/app/components/amp-linear-progress-bar/amp-linear-progress-
 export * from './src/app/components/amp-overlay/amp-overlay.component';
 export * from './src/app/components/amp-radio-button-group/amp-radio-button-group.component';
 export * from './src/app/components/amp-textarea/amp-textarea.component';
+export * from './src/app/components/amp-group-button/amp-group-button.component';
 export * from './src/app/component-groups/amp-google-address-group/amp-google-address-group.component';
 export * from './src/app/component-groups/input-with-label-group/input-with-label-group.component';
