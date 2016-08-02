69d560fc785a7c3046e3e2290ba793d21d04a302
Milad <smiladhi@gmail.com>
2016-08-02 12:03:06 +1000

    Fixed validation error breaking the build

--- src/app/util/validations.ts
+++ src/app/util/validations.ts
@@ -1,9 +1,9 @@
-import { FormUtils } from 'amp-ddc-components/src/app/util/form-utils';
+import { FormUtils } from '../util/form-utils';
 export class RequiredValidator {
     public static requiredValidation ( isRequired ) {
         return ( c ) => {
             if ( isRequired ) {
-                if ( !c.value || c.value.length === 0 ) {
+                if ( ! c.value || c.value.length === 0 ) {
                     return {
                         required : true
                     };
@@ -17,7 +17,7 @@ export class MaxLengthValidator {
     public static maxLengthValidation ( valMaxLength ) {
         return ( c ) => {
             if ( valMaxLength ) {
-                if ( !c.value || c.value.length <= valMaxLength ) {
+                if ( ! c.value || c.value.length <= valMaxLength ) {
                     return null;
                 }
                 return {
@@ -32,7 +32,7 @@ export class MinLengthValidator {
     public static minLengthValidation ( valMinLength ) {
         return ( c ) => {
             if ( valMinLength ) {
-                if ( !c.value || c.value.length >= valMinLength ) {
+                if ( ! c.value || c.value.length >= valMinLength ) {
                     return null;
                 }
                 return {
@@ -47,7 +47,7 @@ export class PatterValidator {
     public static patternValidator ( pattern ) {
         return ( c ) => {
             if ( pattern ) {
-                if ( !c.value || new RegExp( pattern ).test( c.value ) ) {
+                if ( ! c.value || new RegExp( pattern ).test( c.value ) ) {
                     return null;
                 }
                 return {
@@ -63,7 +63,7 @@ export class MaxDateValidator {
         return ( c ) => {
             if ( pattern !== undefined ) {
                 let diff = FormUtils.getAgeDays( c.value );
-                if ( !c.value || !new RegExp( datePattern ).test( c.value ) || !diff || diff <= pattern ) {
+                if ( ! c.value || ! new RegExp( datePattern ).test( c.value ) || ! diff || diff <= pattern ) {
                     return null;
                 }
                 return {
@@ -79,7 +79,7 @@ export class MinDateValidator {
         return ( c ) => {
             if ( pattern !== undefined ) {
                 let diff = FormUtils.getAgeDays( c.value );
-                if ( !c.value || !new RegExp( datePattern ).test( c.value ) || !diff || diff >= pattern ) {
+                if ( ! c.value || ! new RegExp( datePattern ).test( c.value ) || ! diff || diff >= pattern ) {
                     return null;
                 }
                 return {
@@ -91,23 +91,23 @@ export class MinDateValidator {
     }
 }
 export class MaxFloatValidator {
-  public static maxFloatValidator ( valMaxFloat ) {
-    return ( c ) => {
-      if ( valMaxFloat ) {
-        if ( !c.value || c.value.length > 0 ) {
-          if (c.value) {
-            let newVal = c.value;
-            let replaceValue = newVal.replace(/[^0-9\.]+/g,"");
-              if (replaceValue > valMaxFloat) {
-                return {
-                  mdMaxFloat : true
-                };
-              }
-          }
-        }
-      }
-      return null;
-    };
-  }
+    public static maxFloatValidator ( valMaxFloat ) {
+        return ( c ) => {
+            if ( valMaxFloat ) {
+                if ( ! c.value || c.value.length > 0 ) {
+                    if ( c.value ) {
+                        let newVal       = c.value;
+                        let replaceValue = newVal.replace( /[^0-9\.]+/g , "" );
+                        if ( replaceValue > valMaxFloat ) {
+                            return {
+                                mdMaxFloat : true
+                            };
+                        }
+                    }
+                }
+            }
+            return null;
+        };
+    }
 }
 
