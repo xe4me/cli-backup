6ae34415cb6a9696766ad8bb4d3b2b5d18083c34
Eric Kwok <eric_kwok@amp.com.au>
2016-08-10 17:47:08 +1000

    DDC-487 Wired up the page section component to the formSectionService

--- src/app/sections/page-section.component.ts
+++ src/app/sections/page-section.component.ts
@@ -1,17 +1,20 @@
 import { FormSection } from '../formSection';
-import {Component, ViewEncapsulation, OnInit, AfterViewInit, NgZone, ViewContainerRef} from '@angular/core';
-import { UIControlService, ProgressObserverService } from 'amp-ddc-ui-core/ui-core';
+import { Component, ViewEncapsulation, OnInit, AfterViewInit, NgZone, ViewContainerRef, ChangeDetectorRef } from '@angular/core';
+import { UIControlService, ProgressObserverService, FormSectionService, FormModelService } from 'amp-ddc-ui-core/ui-core';
 
 @Component ({
   selector: 'page-section',
   template: `
-    <div class='section' [id]='label'>
+    <div class='section' [id]='label' [ngClass]='{"section__hide": !isCurrentSection()}'>
       <div [id]='blocksAnchorId'></div>
     </div>
   `,
   styles: [`
     .section {
       border: 0px solid black;
+    }
+    .section__hide {
+      display: none;
     }
   `]
   // encapsulation: ViewEncapsulation.Emulated
@@ -21,7 +24,17 @@ export class PageSectionComponent extends FormSection {
 
     constructor (public _viewContainerRef: ViewContainerRef,
                  public controlService: UIControlService, 
-                 public progressObserver: ProgressObserverService) {
+                 public progressObserver: ProgressObserverService,
+                 public formSectionService: FormSectionService,
+                 public formModelService: FormModelService,
+                 public _cd: ChangeDetectorRef) {
         super(_viewContainerRef, controlService, progressObserver);
+        this.formModelService.dynamicFormLoaded.subscribe((isLoaded) => {
+            this._cd.detectChanges();
+        })
+    }
+
+    isCurrentSection(): boolean {
+        return this.formSectionService.currentSection === this.fullyDistinguishedName;
     }
 }
