77961633311005e883c8fa5fb3a81c72c57d67b3
Eric Kwok <eric_kwok@amp.com.au>
2016-08-11 18:21:04 +1000

    DDC-655 Added dropdown into Component styleguide

---
+++ src/styleguide/components/amp-dropdown/basic_usage.ts
@@ -0,0 +1,48 @@
+import { Component , AfterViewInit , ChangeDetectorRef } from '@angular/core';
+import { Control , CORE_DIRECTIVES , FORM_DIRECTIVES } from '@angular/common';
+import { ScrollService } from 'amp-ddc-ui-core/ui-core'
+import { AmpDropdownComponent } from '../../../app/components/amp-dropdown/amp-dropdown.component';
+@Component( {
+    templateUrl : 'src/styleguide/components/amp-dropdown/basic_usage.html' ,
+    providers   : [ ScrollService ] ,
+    styles      : [ require( './basic_usage.scss' ).toString() ] ,
+    directives  : [
+        FORM_DIRECTIVES ,
+        AmpDropdownComponent ,
+        CORE_DIRECTIVES
+    ] ,
+    selector    : 'amp-dropdown-basic-usage'
+} )
+
+export default class AMPDropDownComponentBasicUsage implements AfterViewInit {
+    control : Control   = new Control();
+    isInSummaryState    = false;
+    titleOptions   =  [
+                { value : 'mr' , label : 'Mr' } ,
+                { value : 'mrs' , label : 'Mrs' } ,
+                { value : 'miss' , label : 'Miss' } ,
+                { value : 'ms' , label : 'Ms' } ,
+                { value : 'dr' , label : 'Dr' }
+            ];
+
+    private acknowledge = {
+        id          : 'acknowledge' ,
+        disabled    : false ,
+        required    : true ,
+        checked     : false ,
+        scrollOutOn : null
+    };
+
+    constructor ( private _cd : ChangeDetectorRef ) {
+    }
+
+    ngAfterViewInit () {
+
+        // To prevent the ExpressionChangedAfterHasBeenCheckedException, new Change Detection rule
+        this._cd.detectChanges();
+    }
+
+    private onAcknowledgeSelect ( value ) {
+        console.log( 'onAcknowledgeSelect value' , value );
+    }
+}
