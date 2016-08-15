3dff7167da8fbe1806992148ee2beae8c57db7e4
AYNPIQ <pandithurai_adiyan@amp.com.au>
2016-08-15 14:13:31 +1000

    DDC-263 - # Renamed amp-review-block to amp-review-section for better readability

--- src/styleguide/blocks/amp-review/basic_usage.ts
+++ src/styleguide/blocks/amp-review/basic_usage.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { AmpReviewBlock } from '../../../app/blocks/amp-review/amp-review-block/amp-review-block.component';
+import { AmpReviewSection } from '../../../app/blocks/amp-review/amp-review-section/amp-review-section.component';
 import { AmpReviewItem } from '../../../app/blocks/amp-review/amp-review-item/amp-review-item.component';
 
 @Component( {
-    selector    : 'amp-review-block-basic-usage' ,
+    selector    : 'amp-review-basic-usage' ,
     templateUrl : 'src/styleguide/blocks/amp-review/basic_usage.html' ,
     styles      : [ require( './basic_usage.scss' ).toString() ] ,
-    directives  : [ AmpReviewBlock, AmpReviewItem ]
+    directives  : [ AmpReviewSection, AmpReviewItem ]
 } )
 
 export default class AMPReviewBasicUsage {
