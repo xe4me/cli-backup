ed523bb7a1b78ec5356d36e4b7cdfc150f5149b6
Milad <smiladhi@gmail.com>
2016-08-16 17:34:25 +1000

    FormSectionCompnent : To use the new children directive to load the child components on demand : This is awesome !

--- src/app/sections/page-section.component.ts
+++ src/app/sections/page-section.component.ts
@@ -1,23 +1,28 @@
 import { FormSection } from '../formSection';
 import { Component, ViewEncapsulation, OnInit, AfterViewInit, NgZone, ViewContainerRef, ChangeDetectorRef } from '@angular/core';
 import { UIControlService, ProgressObserverService, FormSectionService, FormModelService } from 'amp-ddc-ui-core/ui-core';
+import { ChildrenDirective } from "../children.directive";
 
 @Component ({
-  selector: 'page-section',
-  template: `
+    selector: 'page-section',
+    template: `
     <div class='section' [id]='label' [ngClass]='{"section__hide": !isCurrentSection()}'>
-      <div [id]='blocksAnchorId'></div>
+      <!--<div [id]='blocksAnchorId'></div>-->
+        <div *ngIf="isCurrentSection()">
+            <div [children]="_child_blocks" [fdn]="_fdn"></div>
+        </div>
     </div>
   `,
-  styles: [`
+    styles: [`
     .section {
       border: 0px solid black;
     }
     .section__hide {
       display: none;
     }
-  `]
-  // encapsulation: ViewEncapsulation.Emulated
+  `],
+    directives:[ChildrenDirective]
+    // encapsulation: ViewEncapsulation.Emulated
 })
 export class PageSectionComponent extends FormSection {
     public CLASS_NAME = 'PageSectionComponent';
@@ -29,12 +34,14 @@ export class PageSectionComponent extends FormSection {
                  public formModelService: FormModelService,
                  public _cd: ChangeDetectorRef) {
         super(_viewContainerRef, controlService, progressObserver);
-        this.formModelService.dynamicFormLoaded.subscribe((isLoaded) => {
-            this._cd.detectChanges();
-        });
+        // this.formModelService.dynamicFormLoaded.subscribe((isLoaded) => {
+        //     this._cd.detectChanges();
+        // });
     }
 
     isCurrentSection(): boolean {
-        return this.formSectionService.currentSection === this.fullyDistinguishedName;
+        //return this.formSectionService.currentSection === this.fullyDistinguishedName;
+
+        return this.formSectionService.isCurrentSection(this._fdn);
     }
 }
