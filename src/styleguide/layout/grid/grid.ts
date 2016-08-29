db4d457530f65d9c18ef74a8b5949132e917327e
seanrkerr <sean@seankerr.com>
2016-08-29 15:11:32 +1000

    DDC-849- grid component

--- src/styleguide/layout/grid/grid.ts
+++ src/styleguide/layout/grid/grid.ts
@@ -1,3 +1,15 @@
-/**
- * Created by seankerr on 29/08/2016.
- */
+import { Component } from '@angular/core';
+
+
+@Component( {
+    selector    : 'grid' ,
+    templateUrl : 'src/styleguide/layout/grid/grid.html' ,
+    styles      : [ require( './grid.scss' ).toString() ] ,
+    directives  : []
+} )
+
+export default class grid {
+    // Note: This callback method needs to use the fat arrow (=>) to bind it to 'this'
+    private callbackForChangeLink = (target : string) => {
+    }
+}
