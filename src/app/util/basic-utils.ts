c8496222f114b08d61af71c0b7d4aef78e56d141
AYNPIQ <pandithurai_adiyan@amp.com.au>
2016-08-29 13:05:28 +1000

    DDC-615 - # Moved currency pipe  instantiating in to correct class and added public static to functions

--- src/app/util/basic-utils.ts
+++ src/app/util/basic-utils.ts
@@ -2,16 +2,17 @@ import { AmpCurrencyPipe } from 'amp-ddc-ui-core/ui-core';
 
 export class BasicUtils  {
 
-  private currencyPipe : AmpCurrencyPipe = new AmpCurrencyPipe();
+  public static formatCurrency ( amount : any ) : string {
 
-  public formatCurrency ( amount : any ) : string {
+   let currencyPipe : AmpCurrencyPipe = new AmpCurrencyPipe();
+
     if ( !(typeof amount === 'string' || amount instanceof String) ) {
       amount = amount.toString();
     }
-    return this.currencyPipe.transform(amount.toString(), 2, 3);
+    return currencyPipe.transform(amount.toString(), 2, 3);
   }
 
-  public formatStringToNumber ( str ) : number {
+  public static formatStringToNumber ( str ) : number {
     if (str && str.indexOf(',') > -1) { //check if we have a comma, then remove it
       str = str.replace(/,/g , '');
     }
