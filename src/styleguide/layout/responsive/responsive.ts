9ce2ba233eb655640ef763004b8fa980ec5c5497
seanrkerr <sean@seankerr.com>
2016-08-30 17:32:30 +1000

    DDC-849 #inprogress add responsive screen

---
+++ src/styleguide/layout/responsive/responsive.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import{ Highlight } from '../../highlight'
+
+@Component( {
+    selector    : 'grid' ,
+    templateUrl : 'src/styleguide/layout/responsive/responsive.html' ,
+    styles      : [ require( './grid.scss' ).toString() ] ,
+    directives  : []
+} )
+
+export default class grid {
+    // Note: This callback method needs to use the fat arrow (=>) to bind it to 'this'
+    private callbackForChangeLink = (target : string) => {
+    }
+}
