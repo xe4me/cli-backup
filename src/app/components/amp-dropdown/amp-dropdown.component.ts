8903a2eb711cbcae1f43ba93fabef07884af5eed
Milad <smiladhi@gmail.com>
2016-09-10 19:04:38 +1000

    AmpDropDown : Fixed the validation bug and added automatic join to parentGroup control to be aligned with the new rules of controls

--- src/app/components/amp-dropdown/amp-dropdown.component.ts
+++ src/app/components/amp-dropdown/amp-dropdown.component.ts
@@ -1,23 +1,15 @@
 import {
     Component ,
-    ViewChild , EventEmitter
+    ViewChild , EventEmitter , ChangeDetectionStrategy
 } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { isPresent } from '@angular/core/src/facade/lang';
-import {
-    RequiredValidator ,
-    MinLengthValidator ,
-    MaxLengthValidator ,
-    MaxDateValidator ,
-    MinDateValidator ,
-    PatterValidator
-} from '../../util/validations';
+import { RequiredValidator } from '../../util/validations';
 import { FormGroup } from "@angular/forms";
 import { ClickedOutsideDirective } from '../../directives/clicked-outside/clicked-outside.directive';
-
-@Component({
-    selector: 'amp-dropdown',
-    template: `
+@Component( {
+    selector        : 'amp-dropdown' ,
+    template        : `
         <div
             #dropdownEl
             class='amp-dropdown'
@@ -77,174 +69,150 @@ import { ClickedOutsideDirective } from '../../directives/clicked-outside/clicke
                 </div>
             </div>
         </div>
-    `,
-    inputs: [
-        'id',
-        'label',
-        'options',
-        'controlGroup',
-        'numOptions',
-        'disabled',
-        'required',
-        'isInSummaryState',
-        'labelHidden',
+    ` ,
+    inputs          : [
+        'id' ,
+        'label' ,
+        'options' ,
+        'controlGroup' ,
+        'numOptions' ,
+        'disabled' ,
+        'required' ,
+        'isInSummaryState' ,
+        'labelHidden' ,
         'limitTo'
-    ],
-    styles: [require('./amp-dropdown.component.scss').toString()],
-    directives: [ClickedOutsideDirective],
-    outputs: ['select']
-})
-
+    ] ,
+    styles          : [ require( './amp-dropdown.component.scss' ).toString() ] ,
+    directives      : [ ClickedOutsideDirective ] ,
+    changeDetection : ChangeDetectionStrategy.OnPush ,
+    outputs         : [ 'select' ]
+} )
 export class AmpDropdownComponent {
-
-    @ViewChild('selectEl') selectEl;
-    @ViewChild('optionsEl') optionsEl;
-    @ViewChild('dropdownEl') dropDownEl;
-    private controlGroup:FormGroup;
-    private id:string = 'amp-dropdown-' + Math.round(Math.random() * 1e10);
-    private label:string;
-    private disabled:string;
+    @ViewChild( 'selectEl' ) selectEl;
+    @ViewChild( 'optionsEl' ) optionsEl;
+    @ViewChild( 'dropdownEl' ) dropDownEl;
+    private controlGroup : FormGroup;
+    private id : string                = 'amp-dropdown-' + Math.round( Math.random() * 1e10 );
+    private label : string;
+    private disabled : string;
     private options;
-    private numOptions:number = 4;
-    private optionsShown:boolean = false;
-    private hasSelection:boolean = false;
-    private animateSelection:boolean = false;
-    private hasWidth:boolean = false;
-    private _required:boolean = false;
-    private isInSummaryState:boolean = false;
-    private labelHidden:boolean = false;
+    private numOptions : number        = 4;
+    private optionsShown : boolean     = false;
+    private hasSelection : boolean     = false;
+    private animateSelection : boolean = false;
+    private hasWidth : boolean         = false;
+    private _required : boolean        = false;
+    private isInSummaryState : boolean = false;
+    private labelHidden : boolean      = false;
     private currentOption;
-    private _limitTo:number = 999;
-    private select = new EventEmitter();
-    private selectedOption = {
-        label: '',
-        value: ''
+    private _limitTo : number          = 999;
+    private select                     = new EventEmitter();
+    private selectedOption             = {
+        label : '' ,
+        value : ''
     };
-
     private selectElem;
     private dropdownElem;
     private optionsElem;
+    public control : FormControl       = new FormControl();
+    public errors                      = {};
 
-    public control:FormControl = new FormControl();
-    public errors = {};
-
-    ngOnInit():any {
-        this.control['_ampErrors'] = {};
-        Object.keys(this.errors).map((errorName, i)=> {
-            (<any>this.control)._ampErrors[errorName] = this.errors[errorName];
-        });
-        if (this.controlGroup) {
-            this.controlGroup.addControl(this.id, this.control);
-        }
+    ngOnInit () : any {
+        this.joinToParentGroupAndSetAmpErrors();
         return undefined;
     }
 
-
-    ngAfterViewInit():any {
+    ngAfterViewInit () : any {
         this.updateValitators();
-
-        this.selectElem = this.selectEl.nativeElement;
-        this.dropdownElem = this.dropDownEl.nativeElement;
-        this.optionsElem = this.optionsEl.nativeElement;
-
+        this.selectElem               = this.selectEl.nativeElement;
+        this.dropdownElem             = this.dropDownEl.nativeElement;
+        this.optionsElem              = this.optionsEl.nativeElement;
         // Set default value
-        this.selectElem.selectedIndex = Math.max(0, this.selectElem.selectedIndex);
-
-        setTimeout(() => {
-            this.setSelectedOption('initial');
-        });
-
-        var forceRedraw = function (element) {
+        this.selectElem.selectedIndex = Math.max( 0 , this.selectElem.selectedIndex );
+        setTimeout( () => {
+            this.setSelectedOption( 'initial' );
+        } );
+        var forceRedraw                   = function( element ) {
             element.style.display = 'none';
-            var trick = element.offsetHeight;
+            var trick             = element.offsetHeight;
             element.style.display = '';
         };
-
         // Redraw options to set correct width
         this.optionsElem.style.visibility = 'visible';
-        forceRedraw(this.optionsElem.children[0]);
+        forceRedraw( this.optionsElem.children[ 0 ] );
         this.optionsElem.style.visibility = '';
-
-        this.control.registerOnChange((value) => {
-            if (this.selectElem.value !== value) {
-                this.setSelectValue(value);
+        this.control.registerOnChange( ( value ) => {
+            if ( this.selectElem.value !== value ) {
+                this.setSelectValue( value );
             }
-        });
-
+        } );
         return undefined;
     }
 
-    get required() {
+    get required () {
         return this._required;
     }
 
-    set required(value:boolean) {
-        this._required = this.isTrue(value);
+    set required ( value : boolean ) {
+        this._required = this.isTrue( value );
         this.updateValitators();
     }
 
-    get limitTo() {
+    get limitTo () {
         return this._limitTo;
     }
 
-    set limitTo(value:any) {
+    set limitTo ( value : any ) {
         this._limitTo = value;
     }
 
-    private toggleOptions() {
-        if (this.disabled || this.isInSummaryState) {
+    private toggleOptions () {
+        if ( this.disabled || this.isInSummaryState ) {
             return false;
         }
-
-        if (this.optionsShown) {
+        if ( this.optionsShown ) {
             this.hideOptionsWithFocus();
         } else {
-            this.showOptions(true);
+            this.showOptions( true );
         }
     }
 
-    private showOptions(showActive) {
-        let activeOption = this.optionsElem.querySelectorAll('.amp-dropdown__option--active')[0];
-
-        if (!this.hasWidth) {
-            var width = this.optionsElem.offsetWidth;
+    private showOptions ( showActive ) {
+        let activeOption = this.optionsElem.querySelectorAll( '.amp-dropdown__option--active' )[ 0 ];
+        if ( ! this.hasWidth ) {
+            var width                     = this.optionsElem.offsetWidth;
             this.dropdownElem.style.width = width + 'px';
-            this.hasWidth = true;
+            this.hasWidth                 = true;
         }
-
         this.optionsShown = true;
-
-        if (showActive && activeOption) {
-            setTimeout(() => {
+        if ( showActive && activeOption ) {
+            setTimeout( () => {
                 activeOption.focus();
-            });
+            } );
         }
-        setTimeout(() => {
+        setTimeout( () => {
             this.selectElem.focus();
-        }, 10);
+        } , 10 );
     }
 
-    private hideOptions = ():void => {
+    private hideOptions = () : void => {
         this.optionsShown = false;
-
-        setTimeout(() => {
-            if (this.control.touched) {
-                this.control['hasOpened'] = true;
+        setTimeout( () => {
+            if ( this.control.touched ) {
+                this.control[ 'hasOpened' ] = true;
             }
-        });
+        } );
     }
 
-    private hideOptionsWithFocus() {
+    private hideOptionsWithFocus () {
         this.selectElem.focus();
-
-        setTimeout(() => {
+        setTimeout( () => {
             this.hideOptions();
-        });
+        } );
     }
 
-    private onKeypressEvent($event) {
-
-        switch ($event.keyCode) {
+    private onKeypressEvent ( $event ) {
+        switch ( $event.keyCode ) {
             // Enter key
             case 13:
                 $event.preventDefault();
@@ -269,77 +237,76 @@ export class AmpDropdownComponent {
         }
     }
 
-    private changeCurrentOption() {
+    private changeCurrentOption () {
         // Fix for firefox select change event not being fired
         // each option change
-        if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-            setTimeout(() => {
+        if ( navigator.userAgent.toLowerCase().indexOf( 'firefox' ) > - 1 ) {
+            setTimeout( () => {
                 this.onChangeEvent();
-            });
+            } );
         }
     }
 
-    private onChangeEvent() {
-        this.setSelectedOption('change');
-
-        if (this.optionsShown && this.hasSelection) {
-            this.optionsElem.querySelectorAll('[data-option-val="' + this.selectedOption.value + '"]')[0].focus();
+    private onChangeEvent () {
+        this.setSelectedOption( 'change' );
+        if ( this.optionsShown && this.hasSelection ) {
+            this.optionsElem.querySelectorAll( '[data-option-val="' + this.selectedOption.value + '"]' )[ 0 ].focus();
             this.selectElem.focus();
         }
-        setTimeout(() => {
-            this.select.emit(this.control.value);
-        }, 0);
+        setTimeout( () => {
+            this.select.emit( this.control.value );
+        } , 0 );
     }
 
-    private onFocusEvent($event) {
-        this.showOptions(false);
+    private onFocusEvent ( $event ) {
+        this.showOptions( false );
     }
 
-    private setSelectValue(value) {
+    private setSelectValue ( value ) {
         this.selectElem.value = value;
-        this.trigger('change', this.selectElem);
+        this.trigger( 'change' , this.selectElem );
         this.hideOptionsWithFocus();
     }
 
-    private setSelectedOption(type) {
+    private setSelectedOption ( type ) {
         this.currentOption = this.selectElem.selectedIndex;
-
-        if (this.currentOption > 0) {
-
-            if (!this.animateSelection && type === 'change') {
+        if ( this.currentOption > 0 ) {
+            if ( ! this.animateSelection && type === 'change' ) {
                 this.animateSelection = true;
             }
-
-            let selected = this.selectElem.options[this.currentOption];
-
+            let selected        = this.selectElem.options[ this.currentOption ];
             this.selectedOption = {
-                label: selected.text,
-                value: selected.value
+                label : selected.text ,
+                value : selected.value
             };
-
-            this.hasSelection = true;
+            this.hasSelection   = true;
         }
     }
 
-    private trigger(event, el) {
-        var evObj = document.createEvent('HTMLEvents');
-        evObj.initEvent(event, true, true);
-        el.dispatchEvent(evObj);
+    private trigger ( event , el ) {
+        var evObj = document.createEvent( 'HTMLEvents' );
+        evObj.initEvent( event , true , true );
+        el.dispatchEvent( evObj );
     }
 
-    private updateValitators() {
-        if (this.control) {
-            setTimeout(()=> {
-                //TODO: fix need to for validator being undefined when controls are created
-                this.control.validator = RequiredValidator.requiredValidation(this.required);
-                this.control.updateValueAndValidity({emitEvent: true, onlySelf: false});
-            });
+    private updateValitators () {
+        if ( this.control ) {
+            this.control.setValidators( RequiredValidator.requiredValidation( this.required ) );
+            this.control.updateValueAndValidity( { emitEvent : false } );
         }
     }
 
-    private isTrue(value) {
-        return isPresent(value) && (value === true || value === 'true' || false);
+    private isTrue ( value ) {
+        return isPresent( value ) && (value === true || value === 'true' || false);
     }
 
-
+    private joinToParentGroupAndSetAmpErrors () {
+        this.control[ '_ampErrors' ] = {};
+        Object.keys( this.errors ).map( ( errorName , i )=> {
+            (<any>this.control)._ampErrors[ errorName ] = this.errors[ errorName ];
+        } );
+        if ( this.controlGroup ) {
+            this.controlGroup.addControl( this.id , this.control );
+        }
+    }
 }
