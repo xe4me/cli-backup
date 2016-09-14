f9b0b4001e5fe7df89d1211a6ba0165e7194faa6
seanrkerr <sean@seankerr.com>
2016-09-15 08:45:31 +1000

    DF-19 #inprogress add new style for the amp-input

--- src/styleguide/blocks/amp-form-block/blocks/sample-fields-block.ts
+++ src/styleguide/blocks/amp-form-block/blocks/sample-fields-block.ts
@@ -9,6 +9,7 @@ import { ScrollService } from "../../../../app/services/scroll/scroll.service";
 import { FormModelService } from "../../../../app/services/form-model/form-model.service";
 import { ProgressObserverService } from "../../../../app/services/progress-observer/progress-observer.service";
 import { AmpInputComponent } from "../../../../app/components/amp-input/amp-input.component";
+import { AmpDropdownComponent } from "../../../../app/components/amp-dropdown/amp-dropdown.component";
 import { AmpFormRowComponent } from "../../../../app/blocks/amp-form-row/amp-form-row.component";
 import { FDN } from "../Application.fdn";
 import { StoreService } from "../../../../app/redux/services/store/store.service";
@@ -17,6 +18,77 @@ import { FormService } from "../../../../app/services/form/form.service";
     selector        : 'sample-fields-block' ,
     template        : `
         <amp-form-block [context]="context()" [attr.theme]="themeService.theme.attr" [theme]="themeService.theme.attr">
+         <amp-form-row [attr.theme]="themeService.theme.attr">
+         <label class='grid__item_floated palm-1/1 tablet-1/1 lap-and-up-1/1 form-row-label-inline sr-only'>Whats your full name?</label>
+                <div class="grid__item_floated palm-1/1 tablet-1/1 lap-and-up-3/12 mr mt0">
+                    <label class='1/1 sr-only'>What's your first name?</label>
+                    <amp-redux [fdn]="__fdn.concat([__custom.controls[0].id])">
+                      <amp-dropdown
+                        #ampReduxRef
+                        [attr.theme]="themeService.theme.attr"
+                        [isInSummaryState]='false'
+                        [id]='__custom.controls[0].id'
+                        [label]='"Title"'
+                        [labelHidden]='"HiddenLabel"'
+                        [controlGroup]="__controlGroup"
+                        [options]='__custom.controls[0].options'
+                        [required]="true">
+                      </amp-dropdown>
+                    </amp-redux>
+                </div>
+            <label class='grid__item_floated palm-1/1 tablet-1/1 lap-and-up-1/1 form-row-label-inline sr-only'>Whats your full name?</label>
+                <div class="grid__item_floated palm-1/1 tablet-1/1 lap-and-up-3/12 mr mt0">
+                    <label class='1/1 sr-only'>What's your first name?</label>
+                    <amp-redux [fdn]="__fdn.concat([__custom.controls[0].id])">
+                        <amp-input
+                            #ampReduxRef
+                            [attr.theme]="themeService.theme.attr"
+                            [id]="__custom.controls[0].id"
+                            [label]="'First name '"
+                            [controlGroup]="__controlGroup"
+                            [isInSummaryState]="isInSummaryState"
+                            [required]="true"
+                            [maxLength]="'12'"
+                            [maxFloat]="'654654'"
+                            [minLength]="'4'">
+                        </amp-input>
+                    </amp-redux>
+                </div>
+                <div class="grid__item_floated palm-1/1 tablet-1/1 lap-and-up-3/12 mr mt0">
+                    <label class='1/1 sr-only'>What's your first name?</label>
+                    <amp-redux [fdn]="__fdn.concat([__custom.controls[0].id])">
+                        <amp-input
+                            #ampReduxRef
+                            [attr.theme]="themeService.theme.attr"
+                            [id]="__custom.controls[0].id"
+                            [label]="'Last name '"
+                            [controlGroup]="__controlGroup"
+                            [isInSummaryState]="isInSummaryState"
+                            [required]="true"
+                            [maxLength]="'12'"
+                            [maxFloat]="'654654'"
+                            [minLength]="'4'">
+                        </amp-input>
+                    </amp-redux>
+                </div>
+                <div class="grid__item_floated palm-1/1 tablet-1/1 lap-and-up-3/12 mr mt0">
+                    <label class='1/1 sr-only'>What's your first name?</label>
+                    <amp-redux [fdn]="__fdn.concat([__custom.controls[0].id])">
+                        <amp-input
+                            #ampReduxRef
+                            [attr.theme]="themeService.theme.attr"
+                            [id]="__custom.controls[0].id"
+                            [label]="'Last name '"
+                            [controlGroup]="__controlGroup"
+                            [isInSummaryState]="isInSummaryState"
+                            [required]="true"
+                            [maxLength]="'12'"
+                            [maxFloat]="'654654'"
+                            [minLength]="'4'">
+                        </amp-input>
+                    </amp-redux>
+                </div>
+            </amp-form-row>
             <amp-form-row [attr.theme]="themeService.theme.attr">
             <label class='grid__item_floated palm-1/1 tablet-1/1 lap-and-up-1/1 form-row-label-inline'>Whats your full name?</label>
                 <div class="grid__item_floated palm-1/1 tablet-1/1 lap-and-up-3/12 mr mt0">
@@ -96,7 +168,7 @@ import { FormService } from "../../../../app/services/form/form.service";
             </amp-form-row>
         </amp-form-block>
     ` ,
-    directives      : [ AmpFormBlockComponent , AmpInputComponent , AmpFormRowComponent ] ,
+    directives      : [ AmpFormBlockComponent , AmpInputComponent , AmpFormRowComponent, AmpDropdownComponent ] ,
     changeDetection : ChangeDetectionStrategy.OnPush,
     styles          : [ require( '../basic_usage.scss' ).toString() ] ,
 } )
