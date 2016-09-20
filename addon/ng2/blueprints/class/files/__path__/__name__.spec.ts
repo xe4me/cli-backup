5f94d9974e346ba3c2b034ea7b90b75ef9d43be4
Iain Durham <iain_durham@amp.com.au>
2016-09-20 13:02:25 +1000

    Correctly indent test file code

--- addon/ng2/blueprints/class/files/__path__/__name__.spec.ts
+++ addon/ng2/blueprints/class/files/__path__/__name__.spec.ts
@@ -1,10 +1,8 @@
-/* tslint:disable:no-unused-variable */
-
 import { addProviders, async, inject } from '@angular/core/testing';
 import {<%= classifiedModuleName %>} from './<%= fileName %>';
 
 describe('<%= classifiedModuleName %>', () => {
-  it('should create an instance', () => {
-    expect(new <%= classifiedModuleName %>()).toBeTruthy();
-  });
+    it('should create an instance', () => {
+        expect(new <%= classifiedModuleName %>()).toBeTruthy();
+    });
 });
