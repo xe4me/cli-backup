82982d34da15655081f3ed766c305d5ca069bea8
KLYMWQ <Matthew_Kelly_2@amp.com.au>
2016-09-20 17:05:51 +1000

    Add online id check block

---
+++ src/app/blocks/online-or-offline-id-check/online-or-offline-id-check.component.ts
@@ -0,0 +1,26 @@
+import {
+    Component , ChangeDetectorRef , ElementRef , OnInit , ChangeDetectionStrategy , Input ,
+    AfterViewInit
+} from '@angular/core';
+import {
+    FormBlock ,
+    ScrollService ,
+    FormModelService ,
+    ProgressObserverService ,
+    FormService
+} from 'amp-ddc-components';
+@Component( {
+    selector        : 'online-or-offline-id-check' ,
+    templateUrl     : './online-or-offline-id-check.component.html' ,
+    changeDetection : ChangeDetectionStrategy.OnPush
+} )
+export class AddressBlock extends FormBlock {
+    constructor ( formModelService : FormModelService ,
+                  elementRef : ElementRef ,
+                  private formService : FormService ,
+                  _cd : ChangeDetectorRef ,
+                  scrollService : ScrollService ,
+                  progressObserver : ProgressObserverService ) {
+        super( formModelService , elementRef , _cd , progressObserver , scrollService );
+    }
+}
