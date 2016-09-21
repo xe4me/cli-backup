428451ebfeea5dc21ed7f5080e5d54ea568f7b27
KLYMWQ <Matthew_Kelly_2@amp.com.au>
2016-09-21 12:23:28 +1000

    Single or joint convert to form block

--- src/app/blocks/single-or-joint/single-or-joint-block.component.ts
+++ src/app/blocks/single-or-joint/single-or-joint-block.component.ts
@@ -2,28 +2,7 @@ import { Component , ChangeDetectorRef , ElementRef , ChangeDetectionStrategy } 
 import { AmpButton , ProgressObserverService , FormBlock , FormModelService , ScrollService } from 'amp-ddc-components';
 @Component( {
     selector        : 'single-or-joint-block' ,
-    template        : `
-        <div class="utils__align--left" >
-            <h1 class="heading heading-intro color-vlack">
-                Just for you or with someone else?
-            </h1>
-            <p class='mt-50'>
-                Please let us know if you would like to set up the account in your name or with someone else
-            </p>
-            <amp-button
-                [context]="context()"
-                class='btn btn-ok mt+'
-                (click)='onNext()'>
-                Single
-            </amp-button>
-            <amp-button
-                [context]="context()"
-                class='btn btn-ok mt+'
-                (click)='onNext()'>
-                Joint
-            </amp-button>
-        </div>
-    ` ,
+    templateUrl        : './single-or-joint-block.component.html',
     styles          : [ require( './single-or-joint-block.component.scss' ).toString() ] ,
     changeDetection : ChangeDetectionStrategy.OnPush
 } )
@@ -34,5 +13,9 @@ export class SingleOrJointBlockComponent extends FormBlock {
                   elementRef : ElementRef ,
                   progressObserver : ProgressObserverService ) {
         super( formModelService , elementRef , _cd , progressObserver , scrollService );
+    }
+
+    private onSingleJoint() {
+        this.__controlGroup.get(this.__custom.controls[0].id).setValue(true);
     }
 }
