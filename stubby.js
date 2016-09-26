b7fde0a9b1f235000b333f2e9de4fedf5b808aa8
Milad <smiladhi@gmail.com>
2016-09-26 10:06:49 +1000

    Added stubby to components repo to be used with our services for testing

---
+++ stubby.js
@@ -0,0 +1,29 @@
+var fs = require ( 'fs' );
+var Stubby = require ( 'stubby' ).Stubby;
+var helpers = require ( './helpers' );
+
+var stubby = new Stubby ();
+var stubsPort = 1234;
+var adminPort = 2345;
+var httpsPort = 3456;
+//var host = helpers.getIp () ;
+var host = 'localhost';
+function getStubs () {
+    var stubs = [];
+    fs.readdir ( 'api-dev' , function ( key , files ) {
+        for ( var i = 0 ; i < files.length ; i ++ ) {
+            var file = require ( './api-dev/' + files[ i ] );
+            for ( var l = 0 ; l < file.length ; l ++ ) {
+                stubs.push ( file[ l ] );
+            }
+        }
+        stubby.start ( {
+            stubs : stubsPort , admin : adminPort , location : host , data : stubs, tls : httpsPort
+        } );
+        console.log ( '------------*************----------' );
+        console.log ( 'Stubby server started at ' + stubsPort );
+        console.log ( '------------*************----------' );
+    } );
+
+}
+getStubs ();
