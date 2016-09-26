2214400fa01e4e3fb67ab00ee42a3c33d074f362
KLYMWQ <Matthew_Kelly_2@amp.com.au>
2016-09-27 09:52:49 +1000

    DDC-1010 Account confirmation screen

---
+++ src/app/blocks/account-confirmation/account-confirmation.component.ts
@@ -0,0 +1,26 @@
+import {
+    Component , ChangeDetectorRef , ElementRef , OnInit , ChangeDetectionStrategy , Input ,
+    AfterViewInit
+} from '@angular/core';
+import {
+    FormBlock ,
+    ScrollService ,
+    FormModelService ,
+    ProgressObserverService ,
+    FormService
+} from 'amp-ddc-components';
+@Component( {
+    selector        : 'account-confirmation' ,
+    templateUrl     : './account-confirmation.component.html' ,
+    changeDetection : ChangeDetectionStrategy.OnPush
+} )
+export class AccountConfirmationBlock extends FormBlock {
+    constructor ( formModelService : FormModelService ,
+                  elementRef : ElementRef ,
+                  private formService : FormService ,
+                  _cd : ChangeDetectorRef ,
+                  scrollService : ScrollService ,
+                  progressObserver : ProgressObserverService ) {
+        super( formModelService , elementRef , _cd , progressObserver , scrollService );
+    }
+}
