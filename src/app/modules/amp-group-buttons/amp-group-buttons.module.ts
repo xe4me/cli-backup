b08e1f3c829535fd4cf2fb57960b314aee64ca14
Milad <smiladhi@gmail.com>
2016-10-02 15:35:22 +1100

    AmpModules : Created a shared mofuled and replaced all the duplicated used module wit it

--- src/app/modules/amp-group-buttons/amp-group-buttons.module.ts
+++ src/app/modules/amp-group-buttons/amp-group-buttons.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AmpGroupButtonsComponent } from './components/amp-group-buttons/amp-group-buttons.component';
-import { FormsModule , ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+import { AmpSharedRootModule } from '../amp-shared-root/amp-shared-root.module';
 const DECLARATIONS = [ AmpGroupButtonsComponent ];
 @NgModule( {
     declarations : DECLARATIONS ,
     imports      : [
-        BrowserModule ,
-        FormsModule ,
-        ReactiveFormsModule
+        AmpSharedRootModule
     ] ,
     exports      : DECLARATIONS
 } )
--- src/app/modules/amp-standalone-menu/amp-standalone-menu.module.ts
+++ src/app/modules/amp-standalone-menu/amp-standalone-menu.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { AmpStandAloneMenuComponent } from './components/amp-standalone-menu/amp-standalone-menu.component';
-import { FormsModule , ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+import { AmpSharedRootModule } from '../amp-shared-root/amp-shared-root.module';
 const DECLARATIONS = [ AmpStandAloneMenuComponent ];
 @NgModule( {
     declarations : DECLARATIONS ,
     imports      : [
-        BrowserModule ,
-        FormsModule ,
-        ReactiveFormsModule
+        AmpSharedRootModule
     ] ,
     exports      : DECLARATIONS
 } )
 export class AmpStandAloneMenuModule {
 }
+/*
+ * TODO : @Sean : Please consider if you don't need FormsModule or ReactiveFormModule , please remove them from your
+ * imports
+ * */
