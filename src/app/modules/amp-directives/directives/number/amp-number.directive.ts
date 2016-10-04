911110dd93c3f77c3fb0556a167b6473076c6719
AYNPIQ <pandithurai_adiyan@amp.com.au>
2016-10-04 14:48:36 +1100

    #DDC-1026 Tslint fix

--- src/app/modules/amp-directives/directives/number/amp-number.directive.ts
+++ src/app/modules/amp-directives/directives/number/amp-number.directive.ts
@@ -1,7 +1,7 @@
 import {
          Directive ,
          Input } from '@angular/core';
-import { Control } from "@angular/common";
+import { Control } from '@angular/common';
 
 @Directive( {
     selector : '[amp-number]',
@@ -14,7 +14,7 @@ export class AmpNumberDirective {
     @Input('amp-number') inputControl : Control;
 
     onInputChange () {
-        let newValue = this.inputControl.value.replace(/\D/g,'');
-        this.inputControl.updateValue(newValue);
+        let newValue = this.inputControl.value.replace( /\D/g, '' );
+        this.inputControl.updateValue( newValue );
     }
 }
