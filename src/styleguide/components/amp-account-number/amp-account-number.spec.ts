d988b398f62df2915051aeaa6437b4a53a99f9ba
KLYMWQ <Matthew_Kelly_2@amp.com.au>
2016-10-06 15:48:08 +1100

    Add basic usage and unit test

---
+++ src/styleguide/components/amp-account-number/amp-account-number.spec.ts
@@ -0,0 +1,52 @@
+import { async , ComponentFixture , TestBed } from '@angular/core/testing';
+import { Component , provide , ElementRef } from '@angular/core';
+import { FormsModule , ReactiveFormsModule , FormGroup } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+// Load the implementations that should be tested
+import { ComponentFixtureAutoDetect } from '@angular/core/testing/test_bed';
+import { AmpInputsModule } from '../../../app/modules/amp-inputs';
+describe( 'amp-account-number directive' , () => {
+    beforeEach( async( () => {
+        TestBed.configureTestingModule( {
+            imports      : [ FormsModule , ReactiveFormsModule , AmpInputsModule ] ,
+            declarations : [
+                TestComponent
+            ] ,
+            providers    : [
+                { provide : ElementRef , useClass : MockElementRef } ,
+                { provide : Window , useClass : window } ,
+                { provide : ComponentFixtureAutoDetect , useValue : true }
+            ]
+        } );
+        TestBed.compileComponents();
+    } ) );
+    it( 'should contain an input text element with the correct name, id and data-automation-id attribute' , () => {
+        let fixture : ComponentFixture<TestComponent> = TestBed.createComponent( TestComponent );
+        fixture.detectChanges();
+        let compiledTestComponent = fixture.debugElement;
+        let compiledInput         = compiledTestComponent.query( By.css( 'input' ) );
+        expect( compiledInput.nativeElement.name ).toBe( 'account-number' );
+        expect( compiledInput.nativeElement.id ).toBe( 'account-number' );
+        expect( compiledInput.nativeElement.attributes['maxlength'] ).toBe( '9' );
+        expect( compiledInput.nativeElement.type ).toBe( 'text' );
+        expect( compiledInput.nativeElement.attributes[ 'data-automation-id' ].value ).toBe( 'account_number' );
+    } );
+} );
+class MockElementRef implements ElementRef {
+    nativeElement = {};
+}
+// Create a test component to test directives
+@Component( {
+    template : `
+    <form  #formModel='ngForm' class='nl-form' >
+        <amp-account-number
+            [id]="'account-number'"
+            [label]="'Account number'"
+            [controlGroup]='accountNumberControl'
+            [required]='true'></amp-account-number>
+    </form>
+    `
+} )
+class TestComponent {
+    firstnameControl : FormGroup = new FormGroup( {} );
+}
