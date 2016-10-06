5f8da025f894601d5e48ef821c36126eaed5fd8d
seanrkerr <sean@seankerr.com>
2016-10-06 23:13:19 +1100

    DF-249 #inproress start adding tests to menu

--- src/styleguide/components/amp-standalone-menu/amp-standalone-menu.component.spec.ts
+++ src/styleguide/components/amp-standalone-menu/amp-standalone-menu.component.spec.ts
@@ -1,6 +1,6 @@
 import { async , ComponentFixture , TestBed } from '@angular/core/testing';
-import { Component , provide , ElementRef, ViewChild } from '@angular/core';
-import { FormControl , FormsModule , ReactiveFormsModule , FormGroup } from '@angular/forms';
+import { Component , provide , ElementRef, ViewChild, Injector } from '@angular/core';
+import { FormControl , FormsModule , ReactiveFormsModule , FormGroup, FormBuilder } from '@angular/forms';
 import { BrowserDomAdapter } from '@angular/platform-browser/src/browser/browser_adapter';
 import { MockScrollService } from '../../services/mock-scroll.service';
 import { MockFormModelService } from '../../services/mock-form-mode.service';
@@ -9,46 +9,60 @@ import { FormSectionService } from '../../../app/services/form-section/form-sect
 import { ScrollService } from '../../../app/services/scroll/scroll.service';
 import { FormSectionService } from '../../../app/services/form-section/form-section.service';
 import { ProgressObserverService } from '../../../app/services/progress-observer/progress-observer.service';
-import { AmpStandAloneMenuModule } from '../../../app/modules/amp-standalone-menu/amp-standalone-menu.module';
-import { AmpStandAloneMenuComponent } from '../../../app/modules/amp-standalone-menu/components/amp-standalone-menu/amp-standalone-menu.component';
+import { AmpStandAloneMenuModule, AmpStandAloneMenuComponent } from '../../../app/modules/amp-standalone-menu';
 
 class MockElementRef implements ElementRef {
     nativeElement = {};
 }
-describe( 'amp-standalone-menu component ' , () => {
-    beforeEach( async( () => {
-        TestBed.configureTestingModule( {
-            imports      : [ FormsModule , ReactiveFormsModule, AmpStandAloneMenuModule ] ,
-            providers    : [
-                { provide : FormModelService , useClass : MockFormModelService } ,
-                { provide : ElementRef , useClass : MockElementRef } ,
-                { provide : ScrollService , useClass : MockScrollService } ,
-                { provide : Window , useClass : window } ,
-                ProgressObserverService ,
-                BrowserDomAdapter ,
-                FormSectionService ,
+describe('amp-standalone-menu component ', () => {
+    let _fixture;
+    let _compInjector : Injector;
+    let _comp;
+    let _element;
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [FormsModule, ReactiveFormsModule, AmpStandAloneMenuModule],
+            declarations: [
+                AmpStandAloneMenuTest
+            ],
+            providers: [
+                {provide: FormModelService, useClass: MockFormModelService},
+                {provide: ElementRef, useClass: MockElementRef},
+                {provide: ScrollService, useClass: MockScrollService},
+                {provide: Window, useClass: window},
+                ProgressObserverService,
+                BrowserDomAdapter,
+                FormSectionService,
             ]
-        } );
+        });
         TestBed.compileComponents();
-    } ) );
-    it( 'Should contain an aside element ' , () => {
-        let fixture : ComponentFixture<AmpStandAloneMenuComponent> = TestBed.createComponent( AmpStandAloneMenuComponent );
-        fixture.detectChanges();
-        const Element               = fixture.nativeElement;
-       // let myMenuComponent = fixture.componentInstance;
-        let myMenuComponent = fixture.componentInstance.ref;
+        _fixture = TestBed.createComponent( AmpStandAloneMenuTest );
+        _fixture.detectChanges();
+        _compInjector = _fixture.debugElement.injector;
+        _comp         = _fixture.componentInstance;
+        _element      = _fixture.nativeElement;
+    }));
+    it( 'testComponentControlGroup should be defined ' , () => {
+        _fixture.detectChanges();
+        let myMenuComponent = _fixture.componentInstance.ref;
         let containerElement = Element.querySelector( '.steps-nav' );
-
-        // console.log(containerElement);
-
     } );
-
-} );
+});
 @Component(
-    { 
-        selector : 'amp-standalone-menu-test', 
-        template : ` 
+    {
+        selector: 'amp-standalone-menu-test',
+        template: ` 
             <amp-standalone-menu #ref [form]="__form.controls.Application" [sectionObservable]="scrollService.$scrolled">
-            </amp-standalone-menu>
-        ` 
-    } );
+            </amp-standalone-menu>`
+    });
+class AmpStandAloneMenuTest {
+    @ViewChild('ref') ref;
+    public __controlGroup = new FormGroup( {} );
+
+    private form : FormGroup;
+
+    constructor ( private _builder : FormBuilder ) {
+        this.form = this._builder.group( {} );
+    }
+
+}
