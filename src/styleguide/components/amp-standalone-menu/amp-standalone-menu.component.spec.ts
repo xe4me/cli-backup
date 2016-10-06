03354c1c07d3fcc83ca25bedfbb63d448efb19b7
seanrkerr <sean@seankerr.com>
2016-10-07 10:03:19 +1100

    DF-249 #inprogres add unit tests

--- src/styleguide/components/amp-standalone-menu/amp-standalone-menu.component.spec.ts
+++ src/styleguide/components/amp-standalone-menu/amp-standalone-menu.component.spec.ts
@@ -5,25 +5,17 @@ import { BrowserDomAdapter } from '@angular/platform-browser/src/browser/browser
 import { MockScrollService } from '../../services/mock-scroll.service';
 import { MockFormModelService } from '../../services/mock-form-mode.service';
 import { FormModelService } from '../../../app/services/form-model/form-model.service';
-import { FormSectionService } from '../../../app/services/form-section/form-section.service';
 import { ScrollService } from '../../../app/services/scroll/scroll.service';
 import { FormSectionService } from '../../../app/services/form-section/form-section.service';
 import { ProgressObserverService } from '../../../app/services/progress-observer/progress-observer.service';
 import { AmpStandAloneMenuModule, AmpStandAloneMenuComponent } from '../../../app/modules/amp-standalone-menu';
+import { By } from '@angular/platform-browser';
 
-class MockElementRef implements ElementRef {
-    nativeElement = {};
-}
 describe('amp-standalone-menu component ', () => {
-    let _fixture;
-    let _compInjector : Injector;
-    let _comp;
-    let _element;
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [FormsModule, ReactiveFormsModule, AmpStandAloneMenuModule],
             declarations: [
-                AmpStandAloneMenuTest
+                TestComponent
             ],
             providers: [
                 {provide: FormModelService, useClass: MockFormModelService},
@@ -33,36 +25,24 @@ describe('amp-standalone-menu component ', () => {
                 ProgressObserverService,
                 BrowserDomAdapter,
                 FormSectionService,
-            ]
+            ],
+            imports: [FormsModule, ReactiveFormsModule, AmpStandAloneMenuModule]
         });
         TestBed.compileComponents();
-        _fixture = TestBed.createComponent( AmpStandAloneMenuTest );
-        _fixture.detectChanges();
-        _compInjector = _fixture.debugElement.injector;
-        _comp         = _fixture.componentInstance;
-        _element      = _fixture.nativeElement;
     }));
-    it( 'testComponentControlGroup should be defined ' , () => {
-        _fixture.detectChanges();
-        let myMenuComponent = _fixture.componentInstance.ref;
-        let containerElement = Element.querySelector( '.steps-nav' );
+    it( 'amp-menu should be defined ', () => {
+        // let fixture : ComponentFixture<TestComponent> = TestBed.createComponent( TestComponent );
+        // fixture.detectChanges();
+        // let compiledTestComponent = fixture.debugElement;
+        // let compiledLabel         = compiledTestComponent.query( By.css( 'steps-nav' ) );
     } );
 });
+class MockElementRef implements ElementRef {
+    nativeElement = {};
+}
 @Component(
     {
-        selector: 'amp-standalone-menu-test',
-        template: ` 
-            <amp-standalone-menu #ref [form]="__form.controls.Application" [sectionObservable]="scrollService.$scrolled">
-            </amp-standalone-menu>`
-    });
-class AmpStandAloneMenuTest {
-    @ViewChild('ref') ref;
-    public __controlGroup = new FormGroup( {} );
-
-    private form : FormGroup;
-
-    constructor ( private _builder : FormBuilder ) {
-        this.form = this._builder.group( {} );
-    }
-
+        template: `<aside class="steps-nav"> </aside> `
+    })
+class TestComponent {
 }
