ae4342297061de7ec6806fe32c3d0d3679a34b73
Milad <smiladhi@gmail.com>
2016-10-07 15:22:56 +1100

    Updated QAS and Country service urls to use Environment constants

--- src/app/modules/amp-dropdown/services/amp-country.service.ts
+++ src/app/modules/amp-dropdown/services/amp-country.service.ts
@@ -4,11 +4,13 @@
 import { Injectable } from '@angular/core';
 import { Headers , RequestOptions , Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { Environments } from '../../../abstracts/environments/environments.abstract';
 @Injectable()
 export class AmpCountryService {
+    public static BASE_URL     = Environments.property.TamServicePath + Environments.property.GwDDCService.EnvPath + Environments.property.GwDDCService.Path;
+    public static COUNTRY_URL  = AmpCountryService.BASE_URL + '/refdata/countries';
     public countries : Observable<any[]>;
     public countryServiceError = null;
-    private _countryUrl        = '/ddc/public/api/refdata/countries';
     private _countries;
 
     constructor ( private http : Http ) {
@@ -16,11 +18,12 @@ export class AmpCountryService {
 
     public getCountries () {
         let headers = new Headers( {
-            'Content-Type' : 'application/json'
+            'Content-Type' : 'application/json' ,
+            'caller'       : 'components'
         } );
         let options = new RequestOptions( { headers : headers , body : '' } );
         return this.http
-                   .get( this._countryUrl , options )
-                   .map( ( res )  => res.json().payload );
+                   .get( AmpCountryService.COUNTRY_URL , options )
+                   .map( ( res ) => res.json().payload );
     }
 }
