9b943196d6133b6d7991420e623c7a6d0a8f72fe
KLYMWQ <Matthew_Kelly_2@amp.com.au>
2016-10-10 10:27:01 +1100

    BET-3397 checkbox should not be interactable in review mode

--- src/app/modules/amp-checkbox/components/amp-checkbox/amp-checkbox.component.ts
+++ src/app/modules/amp-checkbox/components/amp-checkbox/amp-checkbox.component.ts
@@ -110,7 +110,7 @@ export class AmpCheckboxComponent implements AfterViewInit {
     }
 
     private onSelect ( $event ) {
-        if ( this.disabled === true ) {
+        if ( this.disabled === true || this.isInSummaryState === true) {
             $event.stopPropagation();
             return;
         }
