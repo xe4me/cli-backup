5c2bea188d79fc9abf6b8ffad5b75a955c902a77
Milad <smiladhi@gmail.com>
2016-10-11 12:34:35 +1100

    DF-283 #resolved : Added caching the country list so it wont call the db for multiple use

--- src/app/modules/amp-dropdown/services/amp-country.service.ts
+++ src/app/modules/amp-dropdown/services/amp-country.service.ts
@@ -9,9 +9,9 @@ import { Environments } from '../../../abstracts/environments/environments.abstr
 export class AmpCountryService {
     public static BASE_URL     = Environments.property.TamServicePath + Environments.property.GwDDCService.EnvPath + Environments.property.GwDDCService.Path;
     public static COUNTRY_URL  = AmpCountryService.BASE_URL + '/refdata/countries';
-    public countries : Observable<any[]>;
+    // public static COUNTRY_URL = 'http://localhost:8080/ddc/public/api/refdata/countries';
     public countryServiceError = null;
-    private _countries;
+    private _cachedCountries : Observable<any[]>;
 
     constructor ( private http : Http ) {
     }
@@ -22,8 +22,12 @@ export class AmpCountryService {
             'caller'       : 'components'
         } );
         let options = new RequestOptions( { headers : headers , body : '' } );
-        return this.http
-                   .get( AmpCountryService.COUNTRY_URL , options )
-                   .map( ( res ) => res.json().payload );
+        return this._cachedCountries ? this._cachedCountries :
+            this.http
+                .get( AmpCountryService.COUNTRY_URL , options )
+                .map( ( res ) => {
+                    this._cachedCountries = Observable.of( res.json().payload );
+                    return res.json().payload;
+                } );
     }
 }
