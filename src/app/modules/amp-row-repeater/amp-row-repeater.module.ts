b4068ba8423ca63900ac67f9c0c9a5a57190e87c
Milad <smiladhi@gmail.com>
2016-10-11 17:16:04 +1100

    Added ampButtonModle to AmpRowRepeaterModule

--- src/app/modules/amp-row-repeater/amp-row-repeater.module.ts
+++ src/app/modules/amp-row-repeater/amp-row-repeater.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { AmpRowRepeaterComponent } from './components/amp-row-repeater/amp-row-repeater.component';
 import { AmpSharedRootModule } from '../amp-shared-root/amp-shared-root.module';
+import { AmpButtonModule } from '../amp-button/amp-button.module';
 const DECLARATIONS = [ AmpRowRepeaterComponent ];
 @NgModule( {
     declarations : DECLARATIONS ,
     imports      : [
-        AmpSharedRootModule
+        AmpSharedRootModule,
+        AmpButtonModule
     ] ,
     exports      : DECLARATIONS
 } )
