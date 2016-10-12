9dae5f1e5d7f63ee3bd03137e71aa928a3446b73
Milad <smiladhi@gmail.com>
2016-10-12 13:03:21 +1100

    Created AutoFocusOn Directive to be used when we want to focus in something manually

--- src/app/modules/amp-directives/amp-directives.module.ts
+++ src/app/modules/amp-directives/amp-directives.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ClickedOutsideDirective } from './directives/clicked-outside/clicked-outside.directive';
 import { AutoFocusDirective } from './directives/auto-focus/auto-focus.directive';
+import { AutoFocusOnDirective } from './directives/auto-focus-on/auto-focus-on.directive';
 import { ThemeIDDirective } from './directives/theme-id/themeId.directive';
 import { AmpStickyOnScrollDirective } from './directives/auto-sticky-on-scroll/amp-sticky-on-scroll.directive';
 import { FocuserDirective } from './directives/focuser/focuser.directive';
 import { AmpNumberDirective } from './directives/number/amp-number.directive';
 const DECLARATIONS = [
+    AutoFocusOnDirective ,
     ClickedOutsideDirective ,
     AutoFocusDirective ,
     ThemeIDDirective ,
