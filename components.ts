3ba1225ab7adf1978d100e6847f249bfca7608e9
Milad <smiladhi@gmail.com>
2016-10-13 08:02:32 +1100

    Removed unnessessary files

--- components.ts
+++
@@ -1,7 +0,0 @@
-export { AmpButton }                    from './src/app/components/amp-button/amp-button.component';
-export { AmpDropdownComponent }	        from './src/app/components/amp-dropdown/amp-dropdown.component';
-export { AMPGoogleAddressComponent }	from './src/app/components/amp-google-address/amp-google-address.component.ts';
-export { AMPGoogleAddressComponentGroup }	from './src/app/component-groups/amp-google-address-group/amp-google-address-group.component.ts';
-export { AmpGroupButtonComponent }	    from './src/app/components/amp-group-button/amp-group-button.component';
-export { InputWithLabelGroupComponent }	from './src/app/component-groups/input-with-label-group/input-with-label-group.component';
-export { MdInputComponent }	      	    from './src/app/components//my-md-input/my-md-input.component.ts';
