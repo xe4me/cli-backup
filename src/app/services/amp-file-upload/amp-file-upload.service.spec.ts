b2f7e7c8c74c991c6c3219ed140903a5ba6f323a
AYNPIQ <pandithurai_adiyan@amp.com.au>
2016-10-14 10:29:17 +1100

    Test case fix

--- src/app/services/amp-file-upload/amp-file-upload.service.spec.ts
+++ src/app/services/amp-file-upload/amp-file-upload.service.spec.ts
@@ -3,15 +3,15 @@ import { AmpFileUploadService } from './amp-file-upload.service';
 describe( 'Amp File Upload service' , ( ) => {
     let _fileUploadService;
     beforeEach( ( ) => {
-        _fileUploadService = new AmpFileUploadService( );
+        _fileUploadService = new AmpFileUploadService( null );
     } );
 
     it( 'Should return a valid token url' , ( ) => {
-        expect( _fileUploadService.tokenUrl ).toBe( '/ddc/secure/api/upload/token' );
+        expect( _fileUploadService.tokenUrl ).toBe( 'https://api-upload-ddc.digital-pilot.ampaws.com.au/ddc/secure/api/upload/token' );
         } );
 
     it( 'Should return a valid upload url' , ( ) => {
-            expect( _fileUploadService.uploadUrl ).toBe( '/ddc/secure/api/upload/upload' );
+            expect( _fileUploadService.uploadUrl ).toBe( 'https://api-upload-ddc.digital-pilot.ampaws.com.au/ddc/secure/api/upload/upload' );
         } ) ;
 
 } );
