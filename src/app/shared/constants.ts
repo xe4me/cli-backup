6fc67c6ab1c539bcdd1c1e53aa40329faa8cf8eb
KLYMWQ <Matthew_Kelly_2@amp.com.au>
2016-10-18 12:23:05 +1100

    Sophie's requested changes to the model

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,5 @@
+export class Constants {
+    public static saveUrl = 'ddc/public/api/bett3r/save';
+    public static singleApplicant : string = 'Individual';
+    public static jointApplicant : string = 'JointApplication';
+}
