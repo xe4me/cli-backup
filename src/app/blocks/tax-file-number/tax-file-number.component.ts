2c08693f7724c2ce43579704bc1e00b77379059a
KLYMWQ <Matthew_Kelly_2@amp.com.au>
2016-10-20 15:11:35 +1100

    BETT-3365 TFN story

--- src/app/blocks/tax-file-number/tax-file-number.component.ts
+++ src/app/blocks/tax-file-number/tax-file-number.component.ts
@@ -23,7 +23,7 @@ import {
     changeDetection : ChangeDetectionStrategy.OnPush
 } )
 export class TaxFileNumberBlock extends FormBlock implements AfterViewInit {
-    public tfnExclusion : AbstractControl;
+    public hasTfn : AbstractControl;
 
     constructor ( formModelService : FormModelService ,
                   elementRef : ElementRef ,
@@ -35,11 +35,7 @@ export class TaxFileNumberBlock extends FormBlock implements AfterViewInit {
     }
 
     public ngAfterViewInit() {
-        this.tfnExclusion = this.__controlGroup.get(this.__custom.controls[1].id);
-        this.tfnExclusion.valueChanges.subscribe((val) => {
-            this.__controlGroup.get(this.__custom.controls[0].id).setValue('');
-            this.__controlGroup.get(this.__custom.controls[2].id).setValue(null);
-        });
+        this.hasTfn = this.__controlGroup.get(this.__custom.controls[0].id);
         super.ngAfterViewInit();
     }
 }
