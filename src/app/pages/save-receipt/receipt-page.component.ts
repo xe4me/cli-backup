219128e0385e46ce828f18b89f25c8afee1563a8
Tamas Ridly <tamas_ridly@amp.com.au>
2016-10-21 09:03:34 +1100

    Added back button

--- src/app/pages/save-receipt/receipt-page.component.ts
+++ src/app/pages/save-receipt/receipt-page.component.ts
@@ -29,7 +29,8 @@ import { AmpEmailComponent } from '../../modules/amp-inputs/components/amp-email
         inputs     : ['title',
                       'messageForReference',
                       'messageForEmail',
-                      'emailSentEvent'],
+                      'emailSentEvent',
+                      'backButton'],
         outputs    : ['$sendEmailEvent']
     })
 
@@ -41,7 +42,8 @@ export class SaveReceiptPageComponent implements AfterViewInit {
     private title = 'Your quote/application has been saved';
     private messageForReference = 'Your quote/application is now saved and your reference is ';
     private messageForEmail = 'Enter your email address so instructions to retrieve the quote/application can be sent to you.';
-    private emailSentNotification = null;
+    private emailSentNotification : string = null;
+    private backButton : string = null;
 
     private referenceId : string = null;
     private isInSummaryState : boolean = false;
@@ -50,7 +52,8 @@ export class SaveReceiptPageComponent implements AfterViewInit {
     constructor(private _cd : ChangeDetectorRef,
                 private el : ElementRef,
                 public _viewContainerRef : ViewContainerRef,
-                private route : ActivatedRoute) {
+                private route : ActivatedRoute,
+                private router : Router) {
     }
 
     ngAfterViewInit () {
@@ -72,6 +75,12 @@ export class SaveReceiptPageComponent implements AfterViewInit {
         // We set errors so the button gets disabled after click
         // The user can still edit the email address and the button becomes active again
         this.controlGroup.setErrors({ emailSent: true });
+    }
+
+    private back () {
+        if (this.backButton) {
+            this.router.navigate([this.backButton]);
+        }
     }
 
     private get emailSentMessage() {
