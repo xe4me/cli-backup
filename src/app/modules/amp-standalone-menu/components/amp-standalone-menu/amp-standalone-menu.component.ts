ef0fa2f4bdd00e3b7cf91a913685f4c412ab063a
TNGML2 <michael_trang@amp.com.au>
2016-10-25 14:56:53 +1100

    DF-249: Amp standalone menu to base on page section on the DOM. Fixed linting issues and removing debugging codes.

--- src/app/modules/amp-standalone-menu/components/amp-standalone-menu/amp-standalone-menu.component.ts
+++ src/app/modules/amp-standalone-menu/components/amp-standalone-menu/amp-standalone-menu.component.ts
@@ -28,46 +28,45 @@ export class AmpStandAloneMenuComponent implements OnInit {
     @Input() form;
     @Input() sectionObservable;
 
-    public control: FormControl = new FormControl(null);
+    public control : FormControl = new FormControl(null);
     public errors = {};
-    public showNavigation: boolean = true;
-    private _selected: string = null;
-    private _disabled: boolean = false;
-    private _required: boolean = false;
-    private isInSummaryState: boolean = false;
-    private controlGroup: FormGroup;
+    public showNavigation : boolean = true;
+    private _selected : string = null;
+    private _disabled : boolean = false;
+    private _required : boolean = false;
+    private isInSummaryState : boolean = false;
+    private controlGroup : FormGroup;
     private sections = [];
-    private sectionLabels: string = null;
-    private _currentSectionName: string = null;
-    private _previousSectionName: string = null;
-    private domUtils: DomUtils = null;
-    private isSectionUpdated: boolean = false;
+    private sectionLabels : string = null;
+    private _currentSectionName : string = null;
+    private _previousSectionName : string = null;
+    private domUtils : DomUtils = null;
+    private isSectionUpdated : boolean = false;
 
-    constructor(private _dom: BrowserDomAdapter,
-        private _cd: ChangeDetectorRef,
-        private elem: ElementRef,
-        private scrollService: ScrollService,
-        private formSectionService: FormSectionService) {
+    constructor(
+        private _dom : BrowserDomAdapter,
+        private _cd : ChangeDetectorRef,
+        private elem : ElementRef,
+        private scrollService : ScrollService,
+        private formSectionService : FormSectionService) {
 
         this.domUtils = new DomUtils();
 
     }
 
-    ngOnInit(): any {
+    ngOnInit() : any {
 
         this.sectionObservable.subscribe((blockchanges) => {
 
-            setTimeout(()=>{
-                this.updateSections();                
-                
-            },0)
+            setTimeout(() => {
+                this.updateSections();
+            }, 0);
 
         });
     }
 
     private getDisabledState(_state){
-        console.log('_state.indexOf("visited")',_state.indexOf("visited")===-1);
-        return _state.indexOf("visited")===-1;
+        return _state.indexOf('visited') === -1;
     }
 
     /**
@@ -86,62 +85,21 @@ export class AmpStandAloneMenuComponent implements OnInit {
             let id = section.id;
             let classes = section.className;
             let label = section.getAttribute('label');
-
-            // console.log('id', id);
-            // console.log('classes', classes);
-            // console.log('label', label);
             return {
                 label: label,
                 id: id,
                 state: classes
-            }
+            };
         });
 
-        console.log('my menu sections ', this.sections);
+        // console.log('my menu sections ', this.sections);
 
         this._cd.markForCheck();
 
     }
 
-    private scrollToSection(event) {
-        console.log("Navigate to section ", event);
-        this.scrollService.scrollToComponentSelector(event.id);
+    private scrollToSection(section) {
+        this.scrollService.scrollToComponentSelector(section.id);
     }
 
-    private openCloseMenu(event) {
-        let navElement = this._dom.query('nav');
-        let clsName = 'open';
-        if (this.domUtils.hasClass(navElement, clsName)) {
-            this.domUtils.removeClass(navElement, clsName);
-        } else {
-            this.domUtils.addClass(navElement, clsName);
-        }
-    }
-
-    // this.browserDomAdapter
-
-    /* private addClass(el : any, className : string) {
-         if (el.classList) {
-             el.classList.add(className);
-         } else if (!this.hasClass(el, className)) {
-             el.className += ' ' + className;
-         }
-     }
- 
-     private removeClass(el : any, className : string) {
-         if (el.classList) {
-             el.classList.remove(className);
-         } else if (this.hasClass(el, className)) {
-             let reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-             el.className = el.className.replace(reg, ' ');
-         }
-     }
- 
-     private hasClass(el : any, className : string) {
-         if (el.classList) {
-             return el.classList.contains(className);
-         } else {
-             return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
-         }
-     }*/
 }
