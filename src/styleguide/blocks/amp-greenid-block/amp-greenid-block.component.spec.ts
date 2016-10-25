6d8594eab31dbc719ddd753b500407fb0e33faf1
seanrkerr <sean@seankerr.com>
2016-10-25 19:55:05 +1100

    DF-249 #inprogress adding unit tests

--- src/styleguide/blocks/amp-greenid-block/amp-greenid-block.component.spec.ts
+++ src/styleguide/blocks/amp-greenid-block/amp-greenid-block.component.spec.ts
@@ -6,9 +6,30 @@ import { ComponentFixtureAutoDetect } from '@angular/core/testing/test_bed';
 import { By } from '@angular/platform-browser';
 import { AmpGreenIdServices } from '../../../app/blocks/amp-greenid-block/services/amp-greenid-service';
 import { AmpGreenidBlockComponent } from '../../../app/blocks/amp-greenid-block/amp-greenid-block';
-import {AmpHttpService} from "../../../app/services/amp-http/amp-http.service";
+import {AmpHttpService} from '../../../app/services/amp-http/amp-http.service';
 describe( 'Green id unit tests' , () => {
     beforeEach( async( () => {
+        const modelValue = {
+            firstName: 'John',
+            lastName: 'Smith',
+            middleNames: 'Danger',
+            honorific: 'Mr',
+            dateOfBirth2: '27/11/2013',
+            dateOfBirth:  '2001-04-12',
+            email: 'sample@test.com',
+            verificationId: 'fred',
+            verificationToken: 'fred',
+            verficationStatus: 'fred',
+            address: {
+                country: 'AU',
+                state: 'NSW',
+                streetName: 'SMITH',
+                flatNumber: 'U 2',
+                streetNumber: '53-57',
+                suburb: 'SYDNEY'
+            }
+        };
+
         TestBed.configureTestingModule( {
             declarations : [
                 TestComponent2
@@ -23,13 +44,14 @@ describe( 'Green id unit tests' , () => {
 
             ]
         } );
-        //this.ampGreenIdServices = new AmpGreenIdServices();
+        // this.ampGreenIdServices = new AmpGreenIdServices();
         TestBed.compileComponents();
     } ) );
     it( 'greenid service is defined' , () => {
-       //console.log(this.ampGreenIdServices);
-        //let fixture : ComponentFixture<TestComponent2> = TestBed.createComponent( TestComponent2 );
-        //fixture.detectChanges();
+        let fixture : ComponentFixture<TestComponent2> = TestBed.createComponent( TestComponent2 );
+        fixture.componentInstance.getTheToken(this.modelValue);
+        // fixture.getTheToken(this.modelValue);
+        // fixture.detectChanges();
       /*  let fixture : ComponentFixture<TestComponent2> = TestBed.createComponent( TestComponent2 );
         fixture.detectChanges();*/
     } );
@@ -43,8 +65,12 @@ class MockElementRef implements ElementRef {
     `
 } )
 class TestComponent2 extends AmpGreenIdServices {
-
+    private ampGreenIdServices : AmpGreenIdServices;
     constructor( ) {
         super(null);
+    }
+
+    public getToken(model : any) {
+        return this.ampGreenIdServices.getTheToken(model);
     }
 }
