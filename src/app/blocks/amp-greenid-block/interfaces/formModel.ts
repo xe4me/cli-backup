93ac192e96670a6f2d597d0ea45c42bc0101e862
KLYMWQ <Matthew_Kelly_2@amp.com.au>
2016-10-26 14:42:53 +1100

    TSLint

--- src/app/blocks/amp-greenid-block/interfaces/formModel.ts
+++ src/app/blocks/amp-greenid-block/interfaces/formModel.ts
@@ -1,20 +1,19 @@
 export interface IFormModel {
-    firstName: string,
-    lastName: string,
-    middleNames: string,
-    title: string,
-    dateOfBirth: Date,
-    email: string,
-    verificationId: string,
-    verificationToken: string,
-    verficationStatus: string,
+    firstName : string;
+    lastName : string;
+    middleNames : string;
+    title : string;
+    dateOfBirth : Date;
+    email : string;
+    verificationId : string;
+    verificationToken : string;
+    verficationStatus : string;
     address: {
-        country: string,
-        state: string,
-        streetName: string,
-        flatNumber: string,
-        streetNumber: string,
-        suburb: string,
-    }
-
+        country : string,
+        state : string,
+        streetName : string,
+        flatNumber : string,
+        streetNumber : string,
+        suburb : string,
+    };
 }
