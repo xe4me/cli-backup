be50024390c05288cdbdc98761ad05e430cd9bc0
KLYMWQ <Matthew_Kelly_2@amp.com.au>
2016-10-26 11:47:52 +1100

    Implement age validation

--- src/app/blocks/basic-info/basic-info.component.ts
+++ src/app/blocks/basic-info/basic-info.component.ts
@@ -8,29 +8,47 @@ import {
     AfterViewInit,
     ViewChild
 } from '@angular/core';
+import * as moment from 'moment';
 import {
-    FormBlock ,
-    ScrollService ,
-    FormModelService ,
-    ProgressObserverService ,
+    FormBlock,
+    ScrollService,
+    FormModelService,
+    ProgressObserverService,
     FormService,
     AutoFocusOnDirective
 } from 'amp-ddc-components';
-@Component( {
-    selector        : 'basic-info-block' ,
-    templateUrl     : './basic-info.component.html' ,
-    changeDetection : ChangeDetectionStrategy.OnPush
-} )
-export class BasicInfoBlock extends FormBlock {
+@Component({
+    selector: 'basic-info-block',
+    templateUrl: './basic-info.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BasicInfoBlock extends FormBlock implements OnInit {
+    public maxDate: Date;
+    public ageValidator: any;
+    @ViewChild(AutoFocusOnDirective) public autoFocusOn;
+    constructor(formModelService: FormModelService,
+        elementRef: ElementRef,
+        private formService: FormService,
+        _cd: ChangeDetectorRef,
+        scrollService: ScrollService,
+        progressObserver: ProgressObserverService) {
+        super(formModelService, elementRef, _cd, progressObserver, scrollService);
+    }
 
-    @ViewChild( AutoFocusOnDirective ) public autoFocusOn;
-
-    constructor ( formModelService : FormModelService ,
-                  elementRef : ElementRef ,
-                  private formService : FormService ,
-                  _cd : ChangeDetectorRef ,
-                  scrollService : ScrollService ,
-                  progressObserver : ProgressObserverService ) {
-        super( formModelService , elementRef , _cd , progressObserver , scrollService );
+    public ngOnInit() {
+        this.maxDate = moment().subtract('years', 18).toDate();
+        this.ageValidator = () => {
+            return (control) => {
+                let date = moment(control.value);
+                if (date.isValid && date.toDate() >= this.maxDate) {
+                    return {
+                        underAge : {
+                            text : this.__custom.controls[4].ageValidationMsg
+                        }
+                    };
+                }
+                return null;
+            };
+        };
     }
 }
