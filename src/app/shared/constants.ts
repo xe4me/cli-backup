f64abc5b82ddb1c8682606d771105dd411ddd847
KLYMWQ <Matthew_Kelly_2@amp.com.au>
2016-10-27 11:28:37 +1100

    Update url

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-    public static saveUrl = 'ddc/public/api/bett3r/save';
+    public static saveUrl = '/ddc/public/api/bett3r/save';
     public static singleApplicant : string = 'Individual';
     public static jointApplicant : string = 'JointApplication';
     public static onlineIdCheck : string = 'online';
