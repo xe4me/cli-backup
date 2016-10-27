ba36b27c284ca6d3fece13d3017879607322b921
KLYMWQ <Matthew_Kelly_2@amp.com.au>
2016-10-27 16:37:04 +1100

    Fix module imports

--- src/app/modules/amp-greenid-block/amp-greenid.module.ts
+++ src/app/modules/amp-greenid-block/amp-greenid.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { AmpGreenIdServices } from '../../modules/amp-greenid-block/components/services/amp-greenid-service';
 import { AmpGreenidBlockComponent } from '../../modules/amp-greenid-block/components/amp-greenid-block';
-import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, FormsModule } from '@angular/forms';
 import { AmpErrorModule } from '../amp-error/amp-error.module';
 import { Http } from '@angular/http';
 import { AmpSharedRootModule } from '../amp-shared-root/amp-shared-root.module';
 import { AmpButtonModule } from '../amp-button/amp-button.module';
 import { AmpCheckboxModule } from '../amp-checkbox/amp-checkbox.module';
+
 
 const DECLARATIONS = [
     AmpGreenidBlockComponent
@@ -17,7 +18,8 @@ const DECLARATIONS = [
         AmpSharedRootModule ,
         AmpButtonModule ,
         AmpCheckboxModule ,
-        AmpErrorModule
+        AmpErrorModule ,
+        FormsModule
     ] ,
     providers    : [ AmpGreenIdServices , Http ] ,
     exports      : DECLARATIONS
