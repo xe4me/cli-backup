5e05541b57362b01d248c220305536e7bea5e48c
Iain Todd <iain_todd@amp.com.au>
2016-10-28 14:59:03 +1100

    Moving logo styles to be global and updating error page to accept logo property

--- src/app/pages/error/error-page.component.ts
+++ src/app/pages/error/error-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { Router , CanDeactivate , ActivatedRouteSnapshot , RouterStateSnapshot } from '@angular/router';
+
+import {
+    Router ,
+    CanDeactivate ,
+    ActivatedRouteSnapshot ,
+    RouterStateSnapshot
+} from '@angular/router';
+
 import { Observable } from 'rxjs/Observable';
+
+import { LicenseesAbstract } from '../../abstracts/licensee/licensee.abstract';
+
 @Component( {
     selector : 'error-page' ,
     template : `
         <div class='error-page'>
-            <div class='error-page_logo'></div>
+            <div class='error-page_logo logo--{{ logoImage }}'></div>
             <div class='error-page_message'>
                 <h3>{{headline}}</h3>
                 <br>
@@ -14,14 +24,25 @@ import { Observable } from 'rxjs/Observable';
         </div>
     ` ,
     styles   : [ require( './error-page.component.scss' ).toString() ] ,
-    inputs : [ 'headline' , 'message' ]
+    inputs : [
+        'headline' ,
+        'message' ,
+        'logoImage'
+    ]
 } )
 export class ErrorPageComponent implements CanDeactivate<ErrorPageComponent> {
     static CLASS_NAME = 'ErrorPageComponent';
+
+    public logoImage : string = '';
+
     public content    = {
         id    : 'DefaultContentId' ,
         label : 'Default content label'
     };
+
+    constructor () {
+        this.logoImage = LicenseesAbstract.hasLicensee(this.logoImage) ? this.logoImage : 'DEA_ASSURED';
+    }
 
     canDeactivate ( component : ErrorPageComponent , route : ActivatedRouteSnapshot ,
                     state : RouterStateSnapshot ) : Observable<boolean>|Promise<boolean>|boolean {
