ecb5da31f7e70a4b54f0684b0f47a29c2ef60287
KLYMWQ <Matthew_Kelly_2@amp.com.au>
2016-11-01 10:46:34 +1100

    Green id integration dummy

--- src/app/blocks/id-check/id-check.component.ts
+++ src/app/blocks/id-check/id-check.component.ts
@@ -1,5 +1,10 @@
 import {
-    Component , ChangeDetectorRef , ElementRef , OnInit , ChangeDetectionStrategy , Input ,
+    Component,
+    ChangeDetectorRef,
+    ElementRef,
+    OnInit,
+    ChangeDetectionStrategy,
+    Input,
     AfterViewInit
 } from '@angular/core';
 import {
@@ -7,14 +12,37 @@ import {
     ScrollService ,
     FormModelService ,
     ProgressObserverService ,
-    FormService
+    FormService,
+    IGreenIdFormModel
 } from 'amp-ddc-components';
 @Component( {
     selector        : 'id-check-block' ,
     templateUrl     : './id-check.component.html' ,
     changeDetection : ChangeDetectionStrategy.OnPush
 } )
-export class IdCheckBlock extends FormBlock {
+export class IdCheckBlock extends FormBlock implements OnInit {
+    private modelValue : IGreenIdFormModel = {
+        firstName: 'John',
+        lastName: 'Smith',
+        middleNames: 'Danger',
+        title: 'Mr',
+        dateOfBirth:  '12/04/2001',
+        email: 'sample@test.com',
+        verificationId: 'fred',
+        verificationToken: 'fred',
+        verificationStatus: 'fred',
+        address: {
+            country: 'AU',
+            state: 'NSW',
+            streetName: 'SMITH',
+            flatNumber: 'U 2',
+            streetNumber: '53-57',
+            suburb: 'SYDNEY'
+        }
+    };
+    private configScriptUrl = 'https://test2.edentiti.com/df/javascripts/greenidConfig.js';
+    private uiScriptUrl = 'https://test2.edentiti.com/df/javascripts/greenidui.min.js';
+    private styleUrl = 'https://test2.edentiti.com/df/assets/stylesheets/greenid.css';
     constructor ( formModelService : FormModelService ,
                   elementRef : ElementRef ,
                   private formService : FormService ,
@@ -22,5 +50,8 @@ export class IdCheckBlock extends FormBlock {
                   scrollService : ScrollService ,
                   progressObserver : ProgressObserverService ) {
         super( formModelService , elementRef , _cd , progressObserver , scrollService );
+    }
+
+    public ngOnInit() {
     }
 }
