ef865a7ce9e37fd8c56905b62b65db09e0d4e3b4
Tamas Ridly <tamas_ridly@amp.com.au>
2016-11-01 14:27:51 +1100

    First draft of submit receipt page module

--- src/app/modules/amp-submit-receipt/components/amp-submit-receipt/amp-submit-receipt.component.ts
+++ src/app/modules/amp-submit-receipt/components/amp-submit-receipt/amp-submit-receipt.component.ts
@@ -18,11 +18,11 @@ import { FormGroup } from '@angular/forms';
 })
 export class AmpSubmitReceiptComponent {
 
-    public licensee : string         = 'DEA_AMPFP';
-    public intro_text : string       = 'Your application has been submitted.';
-    public reference_number : string = 'The application reference is: ';
-    public description : string      = 'Your upladed files are:';
+    public licensee : string         = 'AMP';
     public note_text : string        = 'Someone from AMP will contact you to discuss the application. You are also welcome to call the AMP Underwriting team on 1800 655 655.';
+    public intro_text : string       = 'Your application has been submitted.';
+    public description : string      = 'Your uploaded files are:';
+    public reference_number : string = 'The application reference is: ';
 
     constructor () {
     }
