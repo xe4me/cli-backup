c8c58cc03095cf455a4c103a8f1f51209165e9c0
TNGML2 <Michael_Trang@amp.com.au>
2016-11-14 17:59:25 +1100

    DR-388: Included captchas as part of the form control group.

--- src/app/modules/amp-google-recaptcha/components/amp-recaptcha.component.ts
+++ src/app/modules/amp-google-recaptcha/components/amp-recaptcha.component.ts
@@ -9,7 +9,11 @@ import {
     NgZone
 } from '@angular/core';
 import { AmpReCaptchaService } from '../services/amp-recaptcha.service';
-
+import { FormControl,
+         FormGroup,
+         FormBuilder,
+         Validators
+} from '@angular/forms';
 @Component({
    selector: 'amp-google-recaptcha',
    template: `<div #recaptchaId></div>`
@@ -19,6 +23,8 @@ export class AmpReCaptchaComponent implements OnInit {
     // Get the site id key at https://www.google.com/recaptcha/admin
     // for ddc dev sitekey: 6LeqZgsUAAAAAD9-le6RQUkUv5MTjhsSM-SldWKq
     // for localhost sitekey: 6LcWZwsUAAAAABf92GVXFx5XqcINVs8vBfK_fx1W
+    @Input()
+    id : string = 'amp-google-recaptcha-check';
     @Input()
     sitekey : string = null;
     @Input()
@@ -32,6 +38,11 @@ export class AmpReCaptchaComponent implements OnInit {
     @Input()
     language : string = null; // see available language at https://developers.google.com/recaptcha/docs/language
 
+    @Input()
+    controlGroup : FormGroup;
+    @Input()
+    keepControl : boolean = false;
+
     @Output()
     captchaResponse = new EventEmitter<any>();
     @Output()
@@ -40,20 +51,30 @@ export class AmpReCaptchaComponent implements OnInit {
     @ViewChild('recaptchaId') recaptchaRef : ElementRef;
     widgetId : any = null;
 
+    private recaptchControlGroup : FormGroup;
+
     constructor(
         private zone : NgZone,
         private captchaService : AmpReCaptchaService) {
     }
 
-    ngOnInit() {
+    public ngOnInit() : any {
+        // this.createControls();
         this.captchaService.loadScript(this.language)
             .subscribe((loaded) => {
                 if (loaded) {
                     let captchaOptions = this.buildCaptchaOptions();
                     // console.log('captchaOptions', captchaOptions);
                     this.widgetId = (<any> window).grecaptcha.render(this.recaptchaRef.nativeElement, captchaOptions);
+                    this.createControls();
                 }
             });
+    }
+
+    public ngOnDestroy() {
+        if (!this.keepControl && this.controlGroup && this.id) {
+            this.controlGroup.removeControl(this.id);
+        }
     }
 
     public reset() {
@@ -66,6 +87,24 @@ export class AmpReCaptchaComponent implements OnInit {
         if (!!this.widgetId) {
             return (<any> window).grecaptcha.getResponse(this.recaptchaRef.nativeElement);
         }
+    }
+
+    private createRecaptchaControlGroup() {
+        return new FormGroup({ verificationStatus: new FormControl(null, Validators.required) });
+    }
+
+    private createControls() {
+        if (this.controlGroup) {
+            if (this.controlGroup.contains(this.id)) {
+                this.recaptchControlGroup = <FormGroup> this.controlGroup.get(this.id);
+            } else {
+                this.recaptchControlGroup = this.createRecaptchaControlGroup();
+                this.controlGroup.addControl(this.id, this.recaptchControlGroup);
+            }
+        } else {
+            this.recaptchControlGroup = this.createRecaptchaControlGroup();
+        }
+        this.recaptchControlGroup.markAsTouched();
     }
 
     private buildCaptchaOptions() {
@@ -83,14 +122,17 @@ export class AmpReCaptchaComponent implements OnInit {
     }
 
     private recaptchaCallback(response : string) {
+        let success = response != null && response.length ? true : false;
+        this.recaptchControlGroup.controls['verificationStatus'].setValue(success ? 'verified' : '');
         this.captchaResponse.emit({
-            success : response != null && response.length ? true : false,
+            success : success,
             token : response,
             recaptcha : this
         });
     }
 
     private recaptchaExpiredCallback() {
+        this.recaptchControlGroup.controls['verificationStatus'].setValue(null);
         this.captchaExpired.emit();
     }
 }
