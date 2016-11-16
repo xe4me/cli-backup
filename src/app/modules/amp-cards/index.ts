a16d4d1e6b641deb9a7895afb74ce2dffdbc9da4
Iain Todd <iain_todd@amp.com.au>
2016-11-16 14:39:02 +1100

    Creating new card and cards components - primarily for use in corporate-super

---
+++ src/app/modules/amp-cards/index.ts
@@ -0,0 +1,4 @@
+export { AmpCardComponent } from './components/amp-card/amp-card.component';
+export { AmpCardsComponent } from './components/amp-cards/amp-cards.component';
+export { AmpCardsModule } from './amp-cards.module';
+
---
+++ src/styleguide/components/amp-cards/basic_usage.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component( {
+    selector    : 'amp-cards-basic-usage' ,
+    templateUrl : 'src/styleguide/components/amp-cards/basic_usage.html' ,
+    styles      : [ require( './basic_usage.scss' ).toString() ]
+} )
+
+export default class AMPCardsComponentBasicUsage {
+
+    private cards = [
+        {
+            image: '/assets/images/ready-to-apply.jpg',
+            text: 'Ready to apply',
+            click: function() {
+                alert('"Ready to apply" card clicked!');
+            }
+        },
+        {
+            image: '/assets/images/another-action.jpg',
+            text: 'Another action here',
+            click: function() {
+                alert('"Another action here" card clicked!');
+            }
+        }
+    ];
+}
