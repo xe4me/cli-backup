c7434f4b84e7459ed86cb0a4044c0b0e6130752c
Iain Todd <iain_todd@amp.com.au>
2016-11-16 15:08:48 +1100

    Uncommenting line that is need and fixing tslint

--- src/app/modules/amp-cards/index.ts
+++ src/app/modules/amp-cards/index.ts
@@ -1,4 +1,3 @@
 export { AmpCardComponent } from './components/amp-card/amp-card.component';
 export { AmpCardsComponent } from './components/amp-cards/amp-cards.component';
 export { AmpCardsModule } from './amp-cards.module';
-
