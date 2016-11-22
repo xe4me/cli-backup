4aeb8ca5246155146db551427052682c3f6285a3
Milad <smiladhi@gmail.com>
2016-11-22 21:53:55 +1100

    Finished adding padadings and removing the extra paddings and fixing the group-button height in mobile

--- src/app/blocks/amp-form-block/amp-form-block.component.ts
+++ src/app/blocks/amp-form-block/amp-form-block.component.ts
@@ -3,20 +3,20 @@ import { AmpErrorComponent } from '../../modules/amp-error';
 import { AmpButton } from '../../components/amp-button/amp-button.component';
 import { AmpOverlayComponent } from '../../components/amp-overlay/amp-overlay.component';
 @Component( {
-    selector   : 'amp-form-block' ,
-    host       : {
+    selector : 'amp-form-block' ,
+    host     : {
         '[attr.id]'           : 'context?.selectorName' ,
         '[attr.data-section]' : 'context?.__sectionName'
     } ,
     template   : `
-     <div (keyup.enter)="onKeyupEnter($event)" class="FormBlocK">
+      <div (keyup.enter)="onKeyupEnter($event)" class="FormBlocK">
             <amp-overlay [active]='!context?.isActive' *ngIf="withOverlay"></amp-overlay>
             <h2 class="heading heading-intro {{ headingClass }}" [innerHtml]="context?.__custom.blockTitle"></h2>
             <ng-content></ng-content>
              <div *ngIf="!noError">
                 <amp-error [controlGroup]="context?.__controlGroup"></amp-error>
              </div>
-             <div class="block-buttons mt+ palm-mt0" *ngIf="withOkButton">
+             <div class="block-buttons mt-60 " *ngIf="withOkButton">
                  <amp-button
                     [context]="context"
                     *ngIf='!context?.isInSummaryState'
@@ -30,7 +30,7 @@ import { AmpOverlayComponent } from '../../components/amp-overlay/amp-overlay.co
                     *ngIf='context?.isInSummaryState'
                     [attr.theme]="buttonsTheme || theme"
                     (click)='context.onEdit()'
-                    class='btn btn-change btn-ok-margin-top palm-m0'>
+                    class='btn btn-change palm-m0'>
                     Change
                 </amp-button>
             </div>
