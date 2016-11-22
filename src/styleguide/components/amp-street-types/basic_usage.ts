114f37766286bd120ddc1a1e2f588b9b30f4eab0
Milad <smiladhi@gmail.com>
2016-11-22 23:32:22 +1100

    Added src/styleguide/components/amp-street-types/

---
+++ src/styleguide/components/amp-street-types/basic_usage.ts
@@ -0,0 +1,20 @@
+import { Component , ChangeDetectorRef , ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ThemeService } from '../../services/theme';
+import { ScrollService } from '../../../app/services/scroll/scroll.service';
+import { AmpStreetTypesComponent } from '../../../app/modules/amp-dropdown/components/amp-street-types/amp-street-types.component';
+@Component( {
+    templateUrl : 'src/styleguide/components/amp-street-types/basic_usage.html' ,
+    providers   : [ ScrollService ] ,
+    styles      : [ require( './basic_usage.scss' ).toString() ] ,
+    selector    : 'amp-street-types-basic-usage'
+} )
+
+export default class AmpStreetTypesDropDownComponentBasicUsage {
+    @ViewChild( 'dropDown' ) dropDown : AmpStreetTypesComponent;
+    private controlGroup : FormGroup = new FormGroup( {} );
+    private isInSummaryState         = false;
+
+    constructor ( private  themeService : ThemeService , private _cd : ChangeDetectorRef ) {
+    }
+}
