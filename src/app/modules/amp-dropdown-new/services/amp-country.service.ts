3938237a70f865a59ce0b7a48fd2e090324228b6
Milad <smiladhi@gmail.com>
2016-11-22 23:26:16 +1100

    Fixed the country component , preventing from making multiple calls for multuple usage

--- src/app/modules/amp-dropdown-new/services/amp-country.service.ts
+++ src/app/modules/amp-dropdown-new/services/amp-country.service.ts
@@ -3,39 +3,33 @@ import { Headers , RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Environments } from '../../../abstracts/environments/environments.abstract';
 import { AmpHttpService } from '../../../services/amp-http/amp-http.service';
+export interface Country {
+    countryCode : string;
+    country : string;
+}
 @Injectable()
 export class AmpCountryService {
-    public static BASE_URL     = Environments.property.ApiCallsBaseUrl;
-    public static COUNTRY_URL  = AmpCountryService.BASE_URL + '/refdata/countries';
-    // public static COUNTRY_URL = 'https://ddc-uat.digital-np.ampaws.com.au/ddc/public/api/refdata/countries';
-    public countryServiceError = null;
-    private _cachedCountries : Observable<any[]>;
+    public static BASE_URL    = Environments.property.ApiCallsBaseUrl;
+    public static COUNTRY_URL = AmpCountryService.BASE_URL + '/refdata/countries';
+    private stream : Observable<Country[]>;
+    private headers = new Headers( {
+        'Content-Type' : 'application/json' ,
+        'caller'       : Environments.property.experienceName || 'components'
+    } );
+    private options = new RequestOptions( { headers : this.headers , body : '' } );
 
     constructor ( private http : AmpHttpService ) {
     }
 
-    public getCountries () {
-        let headers = new Headers( {
-            'Content-Type' : 'application/json' ,
-            'caller'       : 'components'
-        } );
-        let options = new RequestOptions( { headers : headers , body : '' } );
-        return this._cachedCountries ? this._cachedCountries :
-            this.http
-                .get( AmpCountryService.COUNTRY_URL , options )
-                .map( ( res ) => {
-                    this._cachedCountries = Observable.of( res.json().payload );
-                    return res.json().payload;
-                } );
+    public getCountries () : Observable<Country[]> {
+        if ( ! this.stream ) {
+            this.stream =
+                this.http
+                    .get( AmpCountryService.COUNTRY_URL , this.options )
+                    .map( ( res ) => res.json().payload )
+                    .publishLast();
+            (<any> this.stream).connect();
+        }
+        return this.stream;
     }
-
-    // public getItemLabel ( _value ) {
-    //     if ( _value ) {
-    //         for ( let i = 0 ; i < this._cachedCountries.length ; i ++ ) {
-    //             if ( this._cachedCountries[ i ].countryCode === _value ) {
-    //                 return this._cachedCountries[ i ].country;
-    //             }
-    //         }
-    //     }
-    // }
 }
