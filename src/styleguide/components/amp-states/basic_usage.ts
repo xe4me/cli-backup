4dac34cb163892216eb547bc11b11255898d585f
Milad <smiladhi@gmail.com>
2016-11-23 11:03:48 +1100

    Had to revert this back because the left-nav changes were lost Revert "Revert "Merge branch 'fix/country' into fix/styles""
    This reverts commit 5c8df0068d7838a54f9034699f8b21c420cb7e56.

---
+++ src/styleguide/components/amp-states/basic_usage.ts
@@ -0,0 +1,20 @@
+import { Component , ChangeDetectorRef , ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ThemeService } from '../../services/theme';
+import { ScrollService } from '../../../app/services/scroll/scroll.service';
+import { AmpStatesComponent } from '../../../app/modules/amp-dropdown/components/amp-states/amp-states.component';
+@Component( {
+    templateUrl : 'src/styleguide/components/amp-states/basic_usage.html' ,
+    providers   : [ ScrollService ] ,
+    styles      : [ require( './basic_usage.scss' ).toString() ] ,
+    selector    : 'amp-states-basic-usage'
+} )
+
+export default class AmpStatesDropDownComponentBasicUsage {
+    @ViewChild( 'dropDown' ) dropDown : AmpStatesComponent;
+    private controlGroup : FormGroup = new FormGroup( {} );
+    private isInSummaryState         = false;
+
+    constructor ( private  themeService : ThemeService , private _cd : ChangeDetectorRef ) {
+    }
+}
