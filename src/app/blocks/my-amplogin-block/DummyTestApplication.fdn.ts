306aba464b1d4faa52a88f88448267bcf7593dbd
Eric Kwok <eric_kwok@amp.com.au>
2016-11-25 09:31:06 +1100

    BET-3389 Add initial MyAMP TAM Login component

---
+++ src/app/blocks/my-amplogin-block/DummyTestApplication.fdn.ts
@@ -0,0 +1,17 @@
+export class FDN {
+    public static Welcome                  = [ 'Application' , 'Welcome' ];
+
+    public static ContinueApplicationBlock = [ 'Application' , 'ContinueApplicationBlock' ];
+
+    public static MyAMPLoginBlock          = [ 'Application' , 'MyAMPLoginBlock' ];
+
+    public static SingleOrJoint            = [ 'Application' , 'SingleOrJoint' ];
+
+    public static BetterChoice             = [ 'Application' , 'BetterChoice' ];
+
+    public static LoanOffset               = [ 'Application' , 'LoanOffset' ];
+
+    public static DepositTransition        = [ 'Application' , 'DepositTransition' ];
+
+    public static OffsetTransition         = [ 'Application' , 'OffsetTransition' ];
+}
