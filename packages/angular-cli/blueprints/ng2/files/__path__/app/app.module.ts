3844e11341846fd1a710c1930f560c2e8126e987
Milad <smiladhi@gmail.com>
2016-11-28 21:19:34 +1100

    Added some amp-ddc modules and services to app.ts

--- packages/angular-cli/blueprints/ng2/files/__path__/app/app.module.ts
+++ packages/angular-cli/blueprints/ng2/files/__path__/app/app.module.ts
@@ -5,36 +5,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';<% if (routing) { %>
 import { AppRoutingModule } from './app-routing.module';<% } %>
 import { BrowserDomAdapter } from '@angular/platform-browser/src/browser/browser_adapter';
-import {
-    FormModelService,
-    ScrollService,
-    ProgressObserverService,
-    AmpHttpService,
-    FormSectionService
-} from 'amp-ddc-components';
-
-
 import { APP_RESOLVER_PROVIDERS } from './app.resolver';
 
 import { AppComponent } from './app.component';
+import { AMP_DDC_MODULES } from './app.modules';
+import { AMP_DDC_PROVIDERS } from './app.providers';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ...AMP_DDC_MODULES,
     BrowserModule,
     FormsModule,
-
     HttpModule<% if (routing) { %>,
     AppRoutingModule<% } %>
   ],
 providers : [
-    FormModelService,
-    ScrollService,
-    ProgressObserverService,
-    AmpHttpService,
-    FormSectionService,
+    ...AMP_DDC_PROVIDERS,
     BrowserDomAdapter
 ],
   bootstrap: [AppComponent]
