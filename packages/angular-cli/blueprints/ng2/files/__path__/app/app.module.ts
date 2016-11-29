e03e82549a38919355d6f4a0d872ebb598a2da3c
Milad <smiladhi@gmail.com>
2016-11-29 12:06:28 +1100

    Created app.routes for the app blueprint

--- packages/angular-cli/blueprints/ng2/files/__path__/app/app.module.ts
+++ packages/angular-cli/blueprints/ng2/files/__path__/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule , ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserDomAdapter } from '@angular/platform-browser/src/browser/browser_adapter';
 import { AppComponent } from './app.component';
 import { AMP_DDC_MODULES } from './app.modules';
 import { AMP_DDC_PROVIDERS } from './app.providers';
 import { DYNAMIC_BLOCKS } from './app.dynamic-blocks';
 import { ApplicationFormComponent } from './forms/application-form/application-form.component';
+import { AppRoutingModule } from './app.routes';
 @NgModule( {
     declarations : [
         ...DYNAMIC_BLOCKS,
