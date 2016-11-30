3500b8efc0275de8d42ed13d4c2b94c8fb82614f
TNGML2 <Michael_Trang@amp.com.au>
2016-11-30 13:18:38 +1100

    BET-3889: Added SMS functionality.

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,8 +1,11 @@
+import { Environments } from 'amp-ddc-components';
+
 export class Constants {
     public static baseBett3rUrl            = '/bett3r';
     public static saveUrl                  = Constants.baseBett3rUrl + '/save';
     public static submitUrl                = Constants.baseBett3rUrl + '/submit';
     public static accountsPdfUrl           = Constants.baseBett3rUrl + '/pdf/accounts';
+    public static smsPostUrl               = Environments.property.ApiCallsBaseUrl + '/sms/postMessage';
     public static singleApplicant : string = 'Individual';
     public static jointApplicant : string = 'JointApplicant';
     public static onlineIdCheck : string = 'online';
