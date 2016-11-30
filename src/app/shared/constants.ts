dbb72a936be9ff7699624b8ae4187de6bab3e939
KLYMWQ <Matthew_Kelly_2@amp.com.au>
2016-11-30 16:57:00 +1100

    Call the retrieve service

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,8 +1,9 @@
 export class Constants {
-    public static baseBett3rUrl            = '/bett3r';
-    public static saveUrl                  = Constants.baseBett3rUrl + '/save';
-    public static submitUrl                = Constants.baseBett3rUrl + '/submit';
-    public static accountsPdfUrl           = Constants.baseBett3rUrl + '/pdf/accounts';
+    public static baseBett3rUrl : string = '/bett3r';
+    public static saveUrl : string = `${Constants.baseBett3rUrl}/save`;
+    public static submitUrl : string = `${Constants.baseBett3rUrl}/submit`;
+    public static accountsPdfUrl : string = `${Constants.baseBett3rUrl}/pdf/accounts`;
+    public static retrieveUrl : string = `${Constants.baseBett3rUrl}/retrieve`;
     public static singleApplicant : string = 'Individual';
     public static jointApplicant : string = 'JointApplicant';
     public static onlineIdCheck : string = 'online';
