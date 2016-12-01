0b18edfc27502f1331d99bbebe22ceafe23c584a
Milad <smiladhi@gmail.com>
2016-12-02 10:00:28 +1100

    DF-401 #inprogress : Added the new amp-control-error to checkbox

--- src/app/modules/amp-checkbox/amp-checkbox.module.ts
+++ src/app/modules/amp-checkbox/amp-checkbox.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { AmpCheckboxComponent } from './components/amp-checkbox/amp-checkbox.component';
 import { AmpSharedRootModule } from '../amp-shared-root/amp-shared-root.module';
 import { ScrollService } from '../../services/scroll/scroll.service';
+import { AmpErrorModule } from '../amp-error/amp-error.module';
 const DECLARATIONS = [ AmpCheckboxComponent ];
 @NgModule( {
     declarations : DECLARATIONS ,
     imports      : [
+        AmpErrorModule,
         AmpSharedRootModule
     ] ,
     providers    : [ ScrollService ] ,
