4f9a9a99cf7e1979e4bcf802d4a4176947a0817d
Milad <smiladhi@gmail.com>
2016-12-02 14:46:04 +1100

    DF-401 #resolved , added the error to texst area as well

--- src/app/modules/amp-textarea/amp-textarea.module.ts
+++ src/app/modules/amp-textarea/amp-textarea.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { AmpTextareaComponent } from './components/amp-textarea/amp-textarea.component';
 import { AmpSharedRootModule } from '../amp-shared-root/amp-shared-root.module';
+import { AmpErrorModule } from '../amp-error/amp-error.module';
 const DECLARATIONS = [ AmpTextareaComponent ];
 @NgModule( {
     declarations : DECLARATIONS ,
     imports      : [
+        AmpErrorModule ,
         AmpSharedRootModule
     ] ,
     exports      : DECLARATIONS
