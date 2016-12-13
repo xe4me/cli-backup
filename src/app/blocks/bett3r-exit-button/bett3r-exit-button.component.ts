0dd3115c3d85a5eb485aba5f5cd0b9bdeed0c911
SNGSE2 <sophie_song@amp.com.au>
2016-12-14 10:29:12 +1100

    exit button

--- src/app/blocks/bett3r-exit-button/bett3r-exit-button.component.ts
+++ src/app/blocks/bett3r-exit-button/bett3r-exit-button.component.ts
@@ -7,12 +7,12 @@ import {
 } from 'amp-ddc-components';
 
 @Component( {
-    selector        : 'bett3r-exit' ,
-    templateUrl     : './bett3r-exit.component.html'
+    selector        : 'bett3r-exit-button' ,
+    templateUrl     : 'bett3r-exit-button.component.html'
 } )
 
-export class Bett3rExitBlock {
-    @Input( 'name' ) name : string = 'finish';
+export class Bett3rExitButtonComponent {
+    @Input( 'buttonLabel ' ) buttonLabel : string = 'finish';
     private finish() : void {
         window.open( `${Environments.property.AmpRootUrl}bett3r`, '_self' );
     }
