7d864e5e432142e9f19bed77ea6008726747559c
Milad <smiladhi@gmail.com>
2016-12-14 09:50:26 +1100

    Fixed api customer not getting called after login

--- src/app/blocks/new-or-existing-customer/new-or-existing-customer-block.component.ts
+++ src/app/blocks/new-or-existing-customer/new-or-existing-customer-block.component.ts
@@ -28,7 +28,6 @@ import {
     PrepopMappingService
 } from '../../shared';
 import { FDN } from '../../forms/better-form/Application.fdn';
-import { MyAMPLoginBlockComponent } from '../my-amplogin-block/my-amplogin-block.component';
 @Component( {
     selector        : 'new-or-existing-customer-block' ,
     templateUrl     : './new-or-existing-customer-block.component.html' ,
@@ -43,10 +42,6 @@ export class NewOrExistingCustomerBlock extends FormBlock implements OnInit, Aft
                   _cd : ChangeDetectorRef ,
                   elementRef : ElementRef ,
                   progressObserver : ProgressObserverService ,
-                  private customerDetailsService : CustomerDetailsService,
-                  private applicantGenerator : ApplicantGeneratorService ,
-                  private viewContainerRef : ViewContainerRef ,
-                  private sharedDataService : SharedFormDataService,
                   private viewReference : ViewContainerRef ) {
         super( formModelService , elementRef , _cd , progressObserver , scrollService );
         this.disableAutoSave();
@@ -79,10 +74,7 @@ export class NewOrExistingCustomerBlock extends FormBlock implements OnInit, Aft
             this.loadedLoginBlock = false;
             return this.__removeNext( this.viewReference );
         }
-
-        // New application, so do prepop
-        this.prepopCustomerDetails();
-        return new Promise((resolve) => resolve() );;
+       return new Promise((resolve) => resolve() );;
     }
 
     private newOrLogin ( newOrLoginToMyAmp : string ) {
@@ -103,52 +95,5 @@ export class NewOrExistingCustomerBlock extends FormBlock implements OnInit, Aft
 
     private loginToMyAmp(){
         this.newOrLogin(Constants.existingCustomer);
-    }
-    // TODO move this to a service - this component is getting bloated
-    // https://gitlab.ccoe.ampaws.com.au/DDC/experience-bett3r/issues/1
-    private prepopCustomerDetails () {
-        let isLoggedIn = false; // Default assumption is that we are not logged in.
-                                // TODO: This will have to change if we start the journey from
-                                //       MyAMP and never went thru myAMPLoginBlock
-        // Lets check the myAMPLoginBlock loginResult
-        let ampLoginBlock = (<FormGroup> this.__form.get(FDN.MyAMPLoginBlock));
-        if ( ampLoginBlock && ampLoginBlock.contains(MyAMPLoginBlockComponent.LOGIN_STATUS_CONTROL_NAME )) {
-            let loginResultControl = <FormControl> this.__form.get(
-                FDN.MyAMPLoginBlock.concat(MyAMPLoginBlockComponent.LOGIN_STATUS_CONTROL_NAME));
-            isLoggedIn = Constants.loginSuccess === loginResultControl.value;
-        }
-        if (isLoggedIn) {
-            // Trigger the prepopulation from CMDM
-            this.customerDetailsService
-                    .getCustomerDetails()
-                        .then((data) => {
-                            let FDN_Applicant1_PersonalDetailsSection = [
-                                'Application' , 'Applicant1Section' , 'PersonalDetailsSection' ];
-
-                            let basicInfo = <FormGroup> this.__form.get(
-                                FDN_Applicant1_PersonalDetailsSection.concat('BasicInfo'));
-
-                            let contactDetails = <FormGroup> this.__form.get(
-                                FDN_Applicant1_PersonalDetailsSection.concat('ContactDetails'));
-
-                            // Lets do some prepopulation
-                            PrepopMappingService.prepopBasicInfo(basicInfo, data.payload, this.customerDetailsService);
-                            // TODO: For December release, do not prepopulate addresses as we cannot easily
-                            // map them to the required fields
-                            // this.prepopAddresses(data.payload);
-                            PrepopMappingService.prepopContactDetails(
-                                contactDetails,
-                                data.payload,
-                                this.customerDetailsService);
-
-                            // TODO: Make sure we load the special prepop block and not the manual entry blocks
-                        }).catch((err) => {
-                            console.error('Failed to customer details', err);
-                            // According to the mapping rules if the single CMDM call fails there is no need to retry
-                            // https://teamtools.amp.com.au/confluence/pages/viewpage.action?pageId=55352824
-
-                            // TODO: Make sure we load the manual entry path
-                        });
-        }
     }
 }
