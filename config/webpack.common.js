6ed8d48c55ac8ca17ee3aedc07d299b774ead0bb
Milad <smiladhi@gmail.com>
2016-12-20 16:55:43 +1100

    Updated as per AngularClass update with  https://github.com/AngularClass/angular2-webpack-starter/commit/6193e308f29c82f3cbda4395f487a111a10f7e48

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -117,7 +117,7 @@ module.exports = function (options) {
                         '@angularclass/hmr-loader?pretty=' + !isProd + '&prod=' + isProd,
                         'awesome-typescript-loader',
                         'angular2-template-loader',
-                        'angular2-router-loader'
+                        'angular-router-loader'
                     ],
                     exclude: [/\.(e2e)\.ts$/]
                 },
