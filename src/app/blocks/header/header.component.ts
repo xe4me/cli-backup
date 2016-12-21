bf32851e8a5636da4b3df3be536dc26b1dfe42a2
Milad <smiladhi@gmail.com>
2016-12-21 22:02:13 +1100

    Fully upgraded to 2.2.1 with the lates component all working , just need to fix the id-check block and merge the latest code

--- src/app/blocks/header/header.component.ts
+++ src/app/blocks/header/header.component.ts
@@ -1,19 +1,20 @@
 import {
     Component,
+    ViewEncapsulation,
     ChangeDetectionStrategy
 } from '@angular/core';
-import {
-    Environments
-} from 'amp-ddc-components';
+import { Environments } from 'amp-ddc-components';
 @Component( {
-    selector        : 'header-with-logo' ,
-    templateUrl     : './header.component.html' ,
+    selector : 'header-with-logo',
+    templateUrl : './header.component.html',
     changeDetection : ChangeDetectionStrategy.OnPush,
-    styles : [ require( './header.component.scss').toString() ]
+    encapsulation:ViewEncapsulation.None,
+    styles : [ require( './header.component.scss' ) ]
 } )
 
 export class Header {
     private damContentUrl = `${Environments.property.DamContentUrl}amp/digitalhub/common/images/systems/ddc/`;
+
     constructor () {
     }
 }
