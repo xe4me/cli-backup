7922e435d39c3f6b59923d108f427281dac663fb
Milad <smiladhi@gmail.com>
2016-12-23 09:28:57 +1100

    Temporarily commenting web-animations to check if the jenkins build is working

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -17,4 +17,4 @@ import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 import 'core-js/es7/array';
 import 'zone.js/dist/zone';
-import 'web-animations-js/web-animations.min';
+//import 'web-animations-js/web-animations.min';
