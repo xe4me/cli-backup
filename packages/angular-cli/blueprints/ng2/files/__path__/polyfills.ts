bddd9b3c3c3235a991313f3364ebbd3433220729
Milad <smiladhi@gmail.com>
2016-12-22 11:02:39 +1100

    Applied all the chnages and comits untill https://gitlab.ccoe.ampaws.com.au/DDC/amp-angular-cli/commit/6540ec0ab5e0ead925e517b554f88f774053565f

--- packages/angular-cli/blueprints/ng2/files/__path__/polyfills.ts
+++ packages/angular-cli/blueprints/ng2/files/__path__/polyfills.ts
@@ -16,6 +16,6 @@ import 'core-js/es6/set';
 import 'core-js/es6/reflect';
 
 import 'core-js/es7/reflect';
+import 'core-js/es7/array';
 import 'zone.js/dist/zone';
-
 import 'web-animations-js/web-animations.min';
