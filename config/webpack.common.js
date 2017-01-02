cf7b9294fc99d3ccd4b4a4b355c0ac63305c6a48
Milad <smiladhi@gmail.com>
2017-01-02 19:45:11 +1100

    Updaed package.json and tsconfig and webpack.common

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -14,7 +14,7 @@ const NormalModuleReplacementPlugin = require('webpack/lib/NormalModuleReplaceme
 const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
 const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ForkCheckerPlugin = require('awesome-typescript-loader').ForkCheckerPlugin;
+const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
 const HtmlElementsPlugin = require('./html-elements-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
@@ -69,6 +69,9 @@ module.exports = function (options) {
             alias: {
                 'fdn-loader': require.resolve('./fdn-loader.js')
             }
+        },
+        performance: {
+            hints: false
         },
         /*
          * Options affecting the resolving of modules.
@@ -182,12 +185,12 @@ module.exports = function (options) {
             }),
 
             /*
-             * Plugin: ForkCheckerPlugin
+             * Plugin: CheckerPlugin
              * Description: Do type checking in a separate process, so webpack don't need to wait.
              *
              * See: https://github.com/s-panferov/awesome-typescript-loader#forkchecker-boolean-defaultfalse
              */
-            new ForkCheckerPlugin(),
+            new CheckerPlugin(),
             /*
              * Plugin: CommonsChunkPlugin
              * Description: Shares common code between the pages.
