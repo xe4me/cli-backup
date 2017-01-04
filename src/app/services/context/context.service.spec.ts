d2832e0d442da277806fe1e6a522185ec9ac459b
Milad <smiladhi@gmail.com>
2017-01-04 17:09:53 +1100

    Finished context service

--- src/app/services/context/context.service.spec.ts
+++ src/app/services/context/context.service.spec.ts
@@ -1,41 +1,58 @@
-import { inject, async, TestBed } from '@angular/core/testing';
-import { MockBackend, MockConnection } from '@angular/http/testing';
-import { Http, BaseRequestOptions, Response, ResponseOptions, RequestMethod } from '@angular/http';
+import {
+    inject,
+    async,
+    TestBed,
+    fakeAsync,
+    tick
+} from '@angular/core/testing';
+import {
+    MockBackend,
+    MockConnection
+} from '@angular/http/testing';
+import {
+    Http,
+    BaseRequestOptions,
+    Response,
+    ResponseOptions,
+    RequestMethod
+} from '@angular/http';
 import { ContextService } from './context.service';
 import { AmpHttpService } from '../amp-http/amp-http.service';
+import { Environments } from '../../abstracts/environments/environments.abstract';
 const contextBody = {
     'errors' : [],
-    'data' : {
-        'realUser' : 'BCABB-F',
-        'actingAsUser' : null,
-        'impersonatedUser' : '',
-        'isPrincipal' : true,
-        'practiceName' : 'Pinnacle Financial Pty Ltd',
-        'licensee' : 'DEA_AMPFP',
-        'payeeID' : 'BCABB-F',
+    'data'   : {
+        'realUser'                   : 'BCABB-F',
+        'actingAsUser'               : null,
+        'impersonatedUser'           : '',
+        'isPrincipal'                : true,
+        'initialized'                : false,
+        'practiceName'               : 'Pinnacle Financial Pty Ltd',
+        'licensee'                   : 'DEA_AMPFP',
+        'payeeID'                    : 'BCABB-F',
         'practicePrincipalFirstName' : 'Darron',
-        'practicePrincipalLastName' : 'Smith',
-        'iat' : 1461207449,
-        'exp' : 1466474035,
-        'jwt_realUserFirstName' : 'Tamas',
-        'jwt_realUserLastName' : 'Ridly',
-        'jwt_realUser' : 'BCABB-F',
-        'jwt_iss' : 'Portal',
-        'jwt_impersonatedUser' : ''
+        'practicePrincipalLastName'  : 'Smith',
+        'iat'                        : 1461207449,
+        'exp'                        : 1466474035,
+        'jwt_realUserFirstName'      : 'Tamas',
+        'jwt_realUserLastName'       : 'Ridly',
+        'jwt_realUser'               : 'BCABB-F',
+        'jwt_iss'                    : 'Portal',
+        'jwt_impersonatedUser'       : ''
     }
 };
 
 const mockHttpProvider = {
-    provide : Http,
-    deps : [ MockBackend, BaseRequestOptions ],
+    provide    : Http,
+    deps       : [ MockBackend, BaseRequestOptions ],
     useFactory : ( backend : MockBackend, defaultOptions : BaseRequestOptions ) => {
         return new Http( backend, defaultOptions );
     }
 };
 
-describe( 'Fetch context from server', () => {
+describe( 'Context service', () => {
     let subject : ContextService = null;
-    let backend : MockBackend = null;
+    let backend : MockBackend    = null;
 
     beforeEach( async( () => {
         TestBed.configureTestingModule( {
@@ -55,25 +72,69 @@ describe( 'Fetch context from server', () => {
             backend = mockBackend;
         } ) );
 
-    it( 'gets the context with an http call to /usersession.', ( done ) => {
+    it( 'should get the context with an get http call to /usersession.', ( done ) => {
+        let baseUrl         = Environments.property.TamServicePath + Environments.property.GwDDCService.EnvPath + Environments.property.GwDDCService.Path;
+        let contextEndpoint = '/usersession';
+        let contextUrl      = baseUrl + contextEndpoint;
         backend.connections.subscribe( ( connection : MockConnection ) => {
             let options = new ResponseOptions( {
                 body : JSON.stringify( contextBody )
             } );
             expect( connection.request.method ).toBe( RequestMethod.Get );
-            expect( connection.request.url ).toBe( '/usersession' );
+            expect( connection.request.url ).toBe( contextUrl );
             connection.mockRespond( new Response( options ) );
         } );
-
-        subject.fetchContext()
-            .subscribe(
-                ( response ) => {
-                    expect( response ).toEqual( contextBody );
-                    done();
-                },
-                ( error ) => {
-                    done.fail( 'Failed to obtain the context' );
-                }
-            );
+        let contextDataInitialized         = contextBody.data;
+        contextDataInitialized.initialized = true;
+        subject.getContext()
+               .subscribe(
+                   ( response ) => {
+                       expect( response ).toEqual( contextDataInitialized );
+                       done();
+                   },
+                   ( error ) => {
+                       done.fail( 'Failed to obtain the context' );
+                   }
+               );
     } );
+    it( 'planner context should only be valid if the realUser exist and has value', fakeAsync( () => {
+        let baseUrl         = Environments.property.TamServicePath + Environments.property.GwDDCService.EnvPath + Environments.property.GwDDCService.Path;
+        let contextEndpoint = '/usersession';
+        let contextUrl      = baseUrl + contextEndpoint;
+        backend.connections.subscribe( ( connection : MockConnection ) => {
+            let options = new ResponseOptions( {
+                body : JSON.stringify( contextBody )
+            } );
+            expect( connection.request.method ).toBe( RequestMethod.Get );
+            expect( connection.request.url ).toBe( contextUrl );
+            connection.mockRespond( new Response( options ) );
+        } );
+        let contextDataInitialized         = contextBody.data;
+        contextDataInitialized.initialized = true;
+        subject.getContext();
+        tick();
+        expect( subject.isPlannerContextValid() ).toBe( true );
+
+    } ) );
+    it( 'should have an null context first and then after the first call , it should have value with initialized=true', fakeAsync( () => {
+        let baseUrl         = Environments.property.TamServicePath + Environments.property.GwDDCService.EnvPath + Environments.property.GwDDCService.Path;
+        let contextEndpoint = '/usersession';
+        let contextUrl      = baseUrl + contextEndpoint;
+        backend.connections.subscribe( ( connection : MockConnection ) => {
+            let options = new ResponseOptions( {
+                body : JSON.stringify( contextBody )
+            } );
+            expect( connection.request.method ).toBe( RequestMethod.Get );
+            expect( connection.request.url ).toBe( contextUrl );
+            connection.mockRespond( new Response( options ) );
+        } );
+        let contextDataInitialized         = contextBody.data;
+        contextDataInitialized.initialized = true;
+        expect( subject.context ).toEqual( { initialized: false } );
+        expect( subject.context.initialized ).toBe( false );
+        subject.getContext();
+        tick();
+        expect( subject.context.initialized ).toBe( true );
+        expect( subject.context ).toEqual( contextDataInitialized );
+    } ) );
 } );
--- src/app/services/context/context.service.ts
+++ src/app/services/context/context.service.ts
@@ -1,47 +1,96 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import {
     RequestOptions,
-    Headers,
- } from '@angular/http';
+    Headers
+} from '@angular/http';
 import { AmpHttpService } from '../amp-http/amp-http.service';
-import { Environments }   from '../../abstracts/environments/environments.abstract';
-
+import { Environments } from '../../abstracts/environments/environments.abstract';
+import { ErrorService } from '../error/error.service';
+export class Context {
+    initialized : boolean;
+    realUser : string;
+    actingAsUser : string;
+    impersonatedUser : string;
+    isPrincipal : boolean;
+    practiceName : string;
+    licensee : string;
+    payeeID : string;
+    practicePrincipalFirstName : string;
+    practicePrincipalLastName : string;
+    iat : number;
+    exp : number;
+    jwt_realUserFirstName : string;
+    jwt_realUserLastName : string;
+    jwt_realUser : string;
+    jwt_iss : string;
+    jwt_impersonatedUser : string;
+    'x-jwt-assertion' : string;
+}
 @Injectable()
 export class ContextService {
+    private _context : Context = { initialized : false } as Context;
+    private baseUrl            = Environments.property.TamServicePath + Environments.property.GwDDCService.EnvPath + Environments.property.GwDDCService.Path;
+    private contextEndpoint    = '/usersession';
+    private contextUrl         = this.baseUrl + this.contextEndpoint;
+    private headers            = new Headers( { 'Content-Type' : 'application/json' } );
+    private httpOptions        = new RequestOptions( { headers : this.headers } );
+    private contextReplaySubject : any;
 
-    public $contextChanged : EventEmitter<any> = new EventEmitter<any>();
+    constructor ( private http : AmpHttpService ) {
 
-    private _context    = { initialized: false };
-    private _contextUrl = Environments.property.TamServicePath + Environments.property.GwDDCService.EnvPath + Environments.property.GwDDCService.Path + '/usersession';
-
-    constructor( private http : AmpHttpService ){
     }
 
-    get context () {
+    public getContext ( overrideUrl? : string ) : Observable<Context> {
+        return this.contextReplaySubject ? this.contextReplaySubject : this.createContextReplaySubject( overrideUrl );
+    }
+
+    public createContextReplaySubject ( overrideUrl? : string ) : Observable<Context> {
+        this.contextReplaySubject = this
+            .http
+            .get( overrideUrl || this.contextUrl, this.httpOptions )
+            .map( ( res ) => {
+                return this.context = res.json().data;
+            } )
+            .catch( ErrorService.handleError )
+            .publishReplay();
+        this.contextReplaySubject.connect();
+        return this.contextReplaySubject;
+    }
+
+    isContextValid () : boolean {
+        return this.context.initialized && !!this.context[ 'x-jwt-assertion' ];
+    }
+
+    isPlannerContextValid () {
+        if ( this.context.initialized && this.context.realUser ) {
+
+            // TODO: uncomment this when x-jwt-assertion is implemented by TAM
+            // return isContextValid();
+            return true;
+        }
+        return false;
+    }
+
+    // // Used at the start of the buy back form to make sure that the JWT context provided is valid
+    // // **7/6/2017 relaxing the validity rule to allow practices without a recorded specified officer to coming and add those details themselve.
+    // isPracticeContextValid () {
+    //     // Added rules to filter unauthorized licensee
+    //     if ( this.getModel().context.licensee &&
+    //         this.getModel().context.payeeID &&
+    //         LicenseesAbstract.licensees.hasOwnProperty( this.getModel().context.licensee ) ) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    set context ( _context : Context ) {
+        this._context             = _context;
+        this._context.initialized = true;
+    }
+
+    get context () : Context {
         return this._context;
     }
 
-    set context (ctx) {
-        Object.assign( this._context , ctx );
-        this._context.initialized = true;
-        this.$contextChanged.emit( this._context );
-    }
-
-    public fetchContext () : Observable<any> {
-        let headers = new Headers( { 'Content-Type': 'application/json' } );
-        let options = new RequestOptions( { headers , body: '' } );
-        return this.http.get( this._contextUrl , options )
-                        .map( (res) => res.json() );
-    }
-
-    public whenReady (cb) {
-        if (!this.context.initialized) {
-            this.$contextChanged.subscribe( (newContext) => {
-                cb();
-            });
-        } else {
-            cb();
-        }
-    }
 }
