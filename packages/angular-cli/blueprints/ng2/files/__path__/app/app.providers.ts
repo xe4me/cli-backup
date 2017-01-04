d10c5e3e3eb2ca54be31fc192a221855f148fbab
Milad <smiladhi@gmail.com>
2017-01-05 10:57:45 +1100

    Added HydrationService to providers

--- packages/angular-cli/blueprints/ng2/files/__path__/app/app.providers.ts
+++ packages/angular-cli/blueprints/ng2/files/__path__/app/app.providers.ts
@@ -2,11 +2,13 @@ import {
     FormModelService,
     ScrollService,
     AmpHttpService,
+    HydrationService,
     SaveService
 } from 'amp-ddc-components';
 export const AMP_DDC_PROVIDERS = [
     FormModelService,
     ScrollService,
     AmpHttpService,
+    HydrationService,
     SaveService
 ];
