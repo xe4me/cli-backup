149d94545758d67c63c4cb95ceab53dcbd168f7a
Milad <smiladhi@gmail.com>
2017-01-04 13:36:56 +1100

    Updated process_env variables for test environment

--- config/_process_env.js
+++ config/_process_env.js
@@ -1,6 +1,9 @@
 var _process_env = {
+    ENV: 'development',
+    ApiCallsBaseUrl: '',
+    experienceName: 'components',
     GoogleApiKey: '',
-    TamServicePath: '',
+    TamServicePath: 'http://localhost:1234',
     GwPracticeService: {
         ApiKey: '',
         EnvPath: '',
@@ -10,7 +13,17 @@ var _process_env = {
         ApiKey: '',
         EnvPath: '',
         Path: '',
-    }
+    },
+    GreenId: {
+        configScriptUrl: 'https://test2.edentiti.com/df/javascripts/greenidConfig.js',
+        uiScriptUrl: 'https://test2.edentiti.com/df/javascripts/greenidui.min.js',
+        styleUrl: 'https://test2.edentiti.com/df/assets/stylesheets/greenid.css',
+        environment: 'test'
+    },
+    GoogleRecaptcha: {
+        sitekey: '6LeqZgsUAAAAAD9-le6RQUkUv5MTjhsSM-SldWKq'
+    },
+    DamContentUrl: 'https://www.amp.com.au/content/dam/'
 };
 
 
