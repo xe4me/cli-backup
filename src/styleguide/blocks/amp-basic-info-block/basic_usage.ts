1d8e629ef0d3636ab887171ebb9def78b320f180
--global <cedricnicoloso@qantas.com.au>
2017-01-11 10:50:02 +1100

    Remove scroll trick to make block active

--- src/styleguide/blocks/amp-basic-info-block/basic_usage.ts
+++ src/styleguide/blocks/amp-basic-info-block/basic_usage.ts
@@ -5,34 +5,21 @@ import { FormGroup , FormBuilder } from '@angular/forms';
 import { ScrollService } from '../../../app/services/scroll/scroll.service';
 
 const formDef = require( './form-def.def.json' );
-import { FDN } from './Application.fdn';
 
 @Component( {
     selector    : 'basic-info-block-basic-usage',
     templateUrl : './basic_usage.html',
     styles      : [ require( './basic_usage.scss' ).toString() ]
 } )
-export default class AmpBasicInfoBlockBasicUsage implements AfterViewInit {
+export default class AmpBasicInfoBlockBasicUsage {
 
     private fullyDistinguishedName = [];
     private childBlocks            = formDef;
     private form : FormGroup;
 
     constructor ( public store : Store<any> ,
-                  private _builder : FormBuilder,
-                  private scrollService : ScrollService ) {
+                  private _builder : FormBuilder ) {
         this.form = this._builder.group( {} );
     }
 
-    public ngAfterViewInit () {
-        // Activate the block (not active by default as set in form-block.ts)
-        this.fireMockScrolledEvent(FDN.basicInfo);
-    }
-
-    private fireMockScrolledEvent(fdn) {
-        this.scrollService.$scrolled.emit( {
-            componentSelector : [ ...fdn, 'block' ].join( '-' ),
-            section           : null
-        } );
-    }
 }
