4acfd7620269436b953848308faf13f1181b1869
Iain Durham <iain_durham@amp.com.au>
2017-01-10 11:14:40 +1100

    BET-4081: Stub out login in localhost environment

--- webpackDevProxy.js
+++ webpackDevProxy.js
@@ -1,10 +1,16 @@
 module.exports = {
-    "/ddc/public/ui/bett3r/config.js": {
-        target: "http://localhost:8882"
+    '/ddc/public/ui/bett3r/config.js': {
+        target: 'http://localhost:8882'
     },
-    "/ddc/public/ui/bett3r/assets/images/*": {
+    '/eam/login': {
+        target: 'http://localhost:8882'
+    },
+    '/wps/myportal/sec/xseed/dashboard/mywealth/': {
+        target: 'http://localhost:8882'
+    },
+    '/ddc/public/ui/bett3r/assets/images/*': {
         // rewrite all the local asset request to remove the relative path from them, this is not needed in the environments and is only for local
-        target: "http://localhost:3000",
+        target: 'http://localhost:3000',
         pathRewrite: function (path, req) {
             if (req.url) {
                 let to = req.url.replace(/\/ddc\/public\/ui\/bett3r\/assets\/images/, '/assets/images');
@@ -20,6 +26,6 @@ module.exports = {
     '/ddc/public/api/*': {
         // this will rewrite all the request to api to dev , for save and submit ,
         secure: false,
-        target: "https://ddc-dev.digital-pilot.ampaws.com.au"
+        target: 'https://ddc-dev.digital-pilot.ampaws.com.au'
     }
-};
+}
