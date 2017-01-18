84a8c152a1e92b749afa774ac9dc07b94d17a0d0
Eric Kwok <eric_kwok@amp.com.au>
2017-01-18 12:38:30 +1100

    AMPSA-27 Adding webpack dev proxy for all images under asset for both public and secure

--- packages/angular-cli/blueprints/ng2/files/webpackDevProxy.js
+++ packages/angular-cli/blueprints/ng2/files/webpackDevProxy.js
@@ -8,5 +8,35 @@ module.exports = {
     },
     '/wps/myportal/sec/xseed/dashboard/mywealth/': {
         target: 'http://localhost:8882'
+    },
+    '/ddc/public/ui/<%=appId%>/assets/images/*': {
+        // rewrite all the local asset request to remove the relative path from them, this is not needed in the environments and is only for local
+        target: 'http://localhost:3000',
+        pathRewrite: function (path, req) {
+            if (req.url) {
+                let to = req.url.replace(/\/ddc\/public\/ui\/<%=appId%>\/assets\/images/, '/assets/images');
+                if (to !== req.url) {
+                    console.log('rewriting api request from : ' + req.url + ' to : ' + to);
+                    req.url = to;
+                }
+            } else {
+                console.log('request does not have a url path ' + path, 'req: ', req);
+            }
+        }
+    },
+    '/ddc/secure/ui/<%=appId%>/assets/images/*': {
+        // rewrite all the local asset request to remove the relative path from them, this is not needed in the environments and is only for local
+        target: 'http://localhost:3000',
+        pathRewrite: function (path, req) {
+            if (req.url) {
+                let to = req.url.replace(/\/ddc\/secure\/ui\/<%=appId%>\/assets\/images/, '/assets/images');
+                if (to !== req.url) {
+                    console.log('rewriting api request from : ' + req.url + ' to : ' + to);
+                    req.url = to;
+                }
+            } else {
+                console.log('request does not have a url path ' + path, 'req: ', req);
+            }
+        }
     }
 };
