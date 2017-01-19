884f8365349abb1ea0079f4e61fa3df8e60193a4
Eric Kwok <eric_kwok@amp.com.au>
2017-01-19 16:53:31 +1100

    AMPSA-8 Fixed ngModule declaration for both Menu frame and Residency

--- src/app/modules/amp-menu-frame-block/amp-menu-frame-block.module.ts
+++ src/app/modules/amp-menu-frame-block/amp-menu-frame-block.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { AmpMenuFrameBlockComponent } from './components/amp-menu-frame-block/amp-menu-frame-block.component';
+import { AmpStandAloneMenuModule } from '../amp-standalone-menu';
 import { AmpFormModule } from '../amp-form';
 import { AmpButtonModule } from '../amp-button';
 
-const DECLARATIONS = [ AmpMenuFrameBlockModule ];
+const DECLARATIONS = [ AmpMenuFrameBlockComponent ];
 
 @NgModule( {
-    declarations : DECLARATIONS ,
+    declarations : DECLARATIONS,
     imports      : [
         AmpFormModule,
-        AmpButtonModule
-    ] ,
+        AmpButtonModule,
+        AmpStandAloneMenuModule
+    ],
+    entryComponents : DECLARATIONS,
     exports      : DECLARATIONS
 } )
 export class AmpMenuFrameBlockModule {
