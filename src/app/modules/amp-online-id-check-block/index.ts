742cf84887e302304b470bfb2fe8ff8cc23fe21d
--global <cedricnicoloso@qantas.com.au>
2017-01-20 14:15:20 +1100

    Remove previous work on new green ID block

--- src/app/modules/amp-online-id-check-block/index.ts
+++
@@ -1,2 +0,0 @@
-export { AmpOnlineIdCheckBlockComponent } from './components/amp-online-id-check-block/amp-online-id-check-block.component';
-export { AmpOnlineIdCheckBlockModule } from './amp-online-id-check-block.module';
