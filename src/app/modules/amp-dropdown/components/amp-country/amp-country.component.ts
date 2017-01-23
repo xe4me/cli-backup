be88850465ddd95fb0932312c685befb8211b3ef
SNGSE2 <sophie_song@amp.com.au>
2017-01-23 12:13:31 +1100

    change country component to only request country list if no data there

--- src/app/modules/amp-dropdown/components/amp-country/amp-country.component.ts
+++ src/app/modules/amp-dropdown/components/amp-country/amp-country.component.ts
@@ -41,18 +41,22 @@ export class AmpCountryComponent extends AmpDropdownComponent implements AfterVi
         if ( this.control.value === null ) {
             this.control.setValue( 'Australia' );
         }
-        let subs =
-            this.ampCountryService
-                .getCountries()
-                .subscribe( ( res : any ) => {
-                    this.options = res;
-                    if ( this.control.value === null ) {
-                        this.control.setValue( 'Australia' );
-                    }
-                    if ( subs ) {
-                        subs.unsubscribe();
-                    }
-                } );
+        if (this.ampCountryService.countries.length > 0) {
+            this.options = this.ampCountryService.countries;
+        } else {
+            let subs =
+                this.ampCountryService
+                    .getCountries()
+                    .subscribe( ( res : any ) => {
+                        this.options = res;
+                        if ( this.control.value === null ) {
+                            this.control.setValue( 'Australia' );
+                        }
+                        if ( subs ) {
+                            subs.unsubscribe();
+                        }
+                    } );
+        }
         return undefined;
     }
 }
