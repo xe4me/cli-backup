847106a3af66fb38bde4541fe59fc4180164fa0b
Tamas Ridly <tamas_ridly@amp.com.au>
2017-01-24 13:50:08 +1100

    Fix for currency on-blur being messed up

--- src/app/modules/amp-inputs/components/amp-input/amp-input.component.ts
+++ src/app/modules/amp-inputs/components/amp-input/amp-input.component.ts
@@ -66,7 +66,8 @@ import { BaseControl } from '../../../../base-control';
             'showIconRight',
             'iconRightClickHandler',
             'autoComplete',
-            'showOptional'
+            'showOptional',
+            'noTrim'
         ],
         outputs         : [ 'onBlur', 'onFocus' ],
         changeDetection : ChangeDetectionStrategy.OnPush
@@ -103,6 +104,7 @@ export class AmpInputComponent extends BaseControl implements AfterViewInit {
     protected autoComplete : string   = 'off';
     protected iconRightClickHandler;
     protected showOptional            = true;
+    protected noTrim                  = false;
 
     constructor ( private _cd : ChangeDetectorRef,
                   protected el : ElementRef,
@@ -291,7 +293,9 @@ export class AmpInputComponent extends BaseControl implements AfterViewInit {
         }, 100 );
         let notUsable;
         if ( this.control.value && isNaN( this.control.value ) ) {
-            this.inputCmp.nativeElement.value = this.control.value.trim();
+            if ( !this.noTrim || this.noTrim !== true) {
+                this.inputCmp.nativeElement.value = this.control.value.trim();
+            }
             notUsable           = this.tolowerCase ? this.control.setValue( this.control.value.toLowerCase() ) : '';
             notUsable           = this.toupperCase ? this.control.setValue( this.control.value.toUpperCase() ) : '';
         }
