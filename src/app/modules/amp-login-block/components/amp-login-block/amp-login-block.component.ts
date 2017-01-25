d96d480fae5f6156031f850c99733b8e64a94ed9
Tristan Le Guillou <tleguillou@octo.com>
2017-01-25 11:02:35 +1100

    fix some issues

--- src/app/modules/amp-login-block/components/amp-login-block/amp-login-block.component.ts
+++ src/app/modules/amp-login-block/components/amp-login-block/amp-login-block.component.ts
@@ -45,117 +45,116 @@ export class AmpLoginBlockComponent extends FormBlock implements OnDestroy, Afte
                  private renderer: Renderer,
                  private dom: BrowserDomAdapter ) {
         super( saveService, _cd, scrollService );
-        // this.disableAutoSave();
+         this.disableAutoSave();
     }
 
-    // public ngAfterViewInit() {
-    //     super.ngAfterViewInit();
-    //     if (this.__isRetrieved) {
-    //         this.cleanUp();
-    //     }
-    // }
+    public ngAfterViewInit() {
+        super.ngAfterViewInit();
+        if (this.__isRetrieved) {
+            this.cleanUp();
+        }
+    }
 
-    // public ngOnDestroy() {
-    //     super.ngOnDestroy();
-    //
-    //     this.removeLoginFrameListener();
-    // }
+    public ngOnDestroy() {
+        super.ngOnDestroy();
 
-    // public removeLoginAndProceed() {
-    //     this.cleanUp();
-    //     this.__removeAt( this.__getIndex( this.vcf ) );
-    //     super.onNext();
-    // }
-    //
-    // public onNext() {
-    //     this.login();
-    // }
+        this.removeLoginFrameListener();
+    }
 
-    // TODO: Check with design if the OK / Change buttons are the right ones to use
-    // public login() {
-    //     if (this.canGoNext) {
-    //         // Clear error message
-    //         this.errorCode = null;
-    //
-    //         // Check if we already have a login form already
-    //         if (!this.dom.query( '#myamploginframe' )) {
-    //             // Generate hidden MyAMP TAM Login form/iframe
-    //             let hiddenFormDiv = document.createElement( 'div' );
-    //             hiddenFormDiv.style.display = 'none';
-    //             hiddenFormDiv.innerHTML = this.formTemplate;
-    //             document.body.appendChild( hiddenFormDiv );
-    //
-    //             // Bind the onload event of the iframe back into angular to get the response of the login
-    //             this.removeLoginFrameListener =
-    //                 this.renderer.listen( this.dom.query( '#myamploginframe' ), 'load', this.submitCallback );
-    //         }
-    //
-    //         // Sync login details from the block to the generated hidden form.
-    //         this.renderer.setElementProperty(
-    //             this.dom.query( '#userid' ), 'value', this.__controlGroup.get( this.__custom.controls[ 0 ].id ).value );
-    //         this.renderer.setElementProperty(
-    //             this.dom.query( '#password' ), 'value', this.__controlGroup.get( this.__custom.controls[ 1 ].id ).value );
-    //
-    //         // Finally, submit the form to TAM
-    //         this.renderer.invokeElementMethod( this.dom.query( '#myAmpLoginBtn' ), 'click' );
-    //     }
-    // }
+    public removeLoginAndProceed() {
+        this.cleanUp();
+        this.__removeAt( this.__getIndex( this.vcf ) );
+        super.onNext();
+    }
 
-    // private removeLoginFrameListener: Function = () => {
-    //     return;
-    // }
+    public onNext() {
+        this.login();
+    }
 
-    // private cleanUp() {
-    //     // Remove page level username/password for security concerns
-    //     this.__controlGroup.get( this.__custom.controls[ 0 ].id ).reset();
-    //     this.__controlGroup.get( this.__custom.controls[ 1 ].id ).reset();
-    //
-    //     // Remove username/password from formGroup for security concerns
-    //     this.__controlGroup.removeControl( this.__custom.controls[ 0 ].id );
-    //     this.__controlGroup.removeControl( this.__custom.controls[ 1 ].id );
-    //
-    //     // Clear the errorCode
-    //     this.errorCode = null;
-    //
-    //     this.removeLoginFrameListener();
-    //     this.__controlGroup.markAsTouched();
-    //
-    //     this.hideThisBlock = true;
-    // }
+    public login() {
+        if (this.canGoNext) {
+            // Clear error message
+            this.errorCode = null;
 
-    // private submitCallback: Function = ( event ) => {
-    //     try {
-    //         let landingURL = this.dom.query( '#myamploginframe' ).contentWindow.location.href;
-    //         if (landingURL) {
-    //             let errorCodeStartIndex = landingURL.indexOf( 'errorCode=' );
-    //             if (errorCodeStartIndex > -1) {
-    //                 let tamErrorCode = landingURL.substring( errorCodeStartIndex + 10 );
-    //                 this.onLoginFail( tamErrorCode );
-    //             } else if (landingURL.indexOf( 'wps/myportal/sec/xseed/dashboard/mywealth/' ) > -1) {
-    //                 this.onLoginSuccess();
-    //             } else {
-    //                 this.onLoginFail();
-    //             }
-    //         } else {
-    //             this.onLoginFail();
-    //         }
-    //
-    //     } catch (err) {
-    //         this.onLoginFail();
-    //         // Not very likely, as prerequisite states we must be in the same this.domain.
-    //     }
-    //
-    //     this._cd.markForCheck();
-    // }
+            // Check if we already have a login form already
+            if (!this.dom.query( '#myamploginframe' )) {
+                // Generate hidden MyAMP TAM Login form/iframe
+                let hiddenFormDiv = document.createElement( 'div' );
+                hiddenFormDiv.style.display = 'none';
+                hiddenFormDiv.innerHTML = this.formTemplate;
+                document.body.appendChild( hiddenFormDiv );
 
-    // private onLoginSuccess() {
-    //     this.removeLoginAndProceed();
-    // }
+                // Bind the onload event of the iframe back into angular to get the response of the login
+                this.removeLoginFrameListener =
+                    this.renderer.listen( this.dom.query( '#myamploginframe' ), 'load', this.submitCallback );
+            }
 
-    // private onLoginFail( errorCode?: string ) {
-    //     this.errorCode = errorCode;
-    //     if (!errorCode) {
-    //         this.errorCode = 'default';
-    //     }
-    // }
+            // Sync login details from the block to the generated hidden form.
+            this.renderer.setElementProperty(
+                this.dom.query( '#userid' ), 'value', this.__controlGroup.get( this.__custom.controls[ 0 ].id ).value );
+            this.renderer.setElementProperty(
+                this.dom.query( '#password' ), 'value', this.__controlGroup.get( this.__custom.controls[ 1 ].id ).value );
+
+            // Finally, submit the form to TAM
+            this.renderer.invokeElementMethod( this.dom.query( '#myAmpLoginBtn' ), 'click' );
+        }
+    }
+
+    private removeLoginFrameListener: Function = () => {
+        return;
+    }
+
+    private cleanUp() {
+        // Remove page level username/password for security concerns
+        this.__controlGroup.get( this.__custom.controls[ 0 ].id ).reset();
+        this.__controlGroup.get( this.__custom.controls[ 1 ].id ).reset();
+
+        // Remove username/password from formGroup for security concerns
+        this.__controlGroup.removeControl( this.__custom.controls[ 0 ].id );
+        this.__controlGroup.removeControl( this.__custom.controls[ 1 ].id );
+
+        // Clear the errorCode
+        this.errorCode = null;
+
+        this.removeLoginFrameListener();
+        this.__controlGroup.markAsTouched();
+
+        this.hideThisBlock = true;
+    }
+
+    private submitCallback: Function = ( event ) => {
+        try {
+            let landingURL = this.dom.query( '#myamploginframe' ).contentWindow.location.href;
+            if (landingURL) {
+                let errorCodeStartIndex = landingURL.indexOf( 'errorCode=' );
+                if (errorCodeStartIndex > -1) {
+                    let tamErrorCode = landingURL.substring( errorCodeStartIndex + 10 );
+                    this.onLoginFail( tamErrorCode );
+                } else if (landingURL.indexOf( 'wps/myportal/sec/xseed/dashboard/mywealth/' ) > -1) {
+                    this.onLoginSuccess();
+                } else {
+                    this.onLoginFail();
+                }
+            } else {
+                this.onLoginFail();
+            }
+
+        } catch (err) {
+            this.onLoginFail();
+            // Not very likely, as prerequisite states we must be in the same this.domain.
+        }
+
+        this._cd.markForCheck();
+    }
+
+    private onLoginSuccess() {
+        this.removeLoginAndProceed();
+    }
+
+    private onLoginFail( errorCode?: string ) {
+        this.errorCode = errorCode;
+        if (!errorCode) {
+            this.errorCode = 'default';
+        }
+    }
 }
