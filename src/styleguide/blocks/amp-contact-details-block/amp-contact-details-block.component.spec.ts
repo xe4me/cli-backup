9700933a225a7e03953d3375a7768c7669718224
Cedric Nicoloso <Cedric_Nicoloso@amp.com.au>
2017-01-29 21:08:19 +1100

    Remove test focus

--- src/styleguide/blocks/amp-contact-details-block/amp-contact-details-block.component.spec.ts
+++ src/styleguide/blocks/amp-contact-details-block/amp-contact-details-block.component.spec.ts
@@ -31,7 +31,7 @@ function loadComponent() {
     homeNumberControl = controlGroup['homeNumber'];
 }
 
-fdescribe('amp-contact-details-block component', () => {
+describe('amp-contact-details-block component', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
