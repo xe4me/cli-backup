d7dd3e3364a07919e0863fb6abcaa202cab1c3a2
MIlad <smiladhi@gmail.com>
2017-01-30 16:08:33 +1100

    Added the functionality to amp-block-loader to take care of merging

--- src/app/modules/amp-contact-details-block/components/amp-contact-details-block/amp-contact-details-block.component.ts
+++ src/app/modules/amp-contact-details-block/components/amp-contact-details-block/amp-contact-details-block.component.ts
@@ -14,56 +14,50 @@ import {
 } from '../../../../services';
 import { AmpInputComponent } from '../../../amp-inputs';
 
-@Component({
+@Component( {
     selector        : 'amp-contact-details-block',
-    template        : require('./amp-contact-details-block.component.html'),
+    template        : require( './amp-contact-details-block.component.html' ),
     changeDetection : ChangeDetectionStrategy.OnPush
-})
-export class AmpContactDetailsBlockComponent extends FormBlock implements OnInit {
+} )
+export class AmpContactDetailsBlockComponent extends FormBlock {
 
-    @ViewChild('mobileNumber') mobileNumberCmp : AmpInputComponent;
+    @ViewChild( 'mobileNumber' ) mobileNumberCmp : AmpInputComponent;
 
-    /* tslint:disable:quotemark */
-    private defaultValues = {
-        "blockTitle": "And your contact details...",
-        "controls": [
+    protected __custom = {
+        blockTitle : 'And your contact details...',
+        controls   : [
             {
-                "id": "emailAddress",
-                "label": "Email",
-                "required": true,
-                "tooltipMessage": "Confirmation of your application will be sent to this email address."
+                id             : 'emailAddress',
+                label          : 'Email',
+                required       : true,
+                tooltipMessage : 'Confirmation of your application will be sent to this email address.'
             },
             {
-                "id": "mobileNumber",
-                "label": "Mobile number",
-                "required": true,
-                "tooltipMessage": "A mobile phone number is required to allow AMP Bank to securely protect your account.",
-                "requiredErrorMessage": "Mobile number is a required field.",
-                "patternErrorMessage": "Mobile number must be in the format 04nnnnnnnn."
+                id                   : 'mobileNumber',
+                label                : 'Mobile number',
+                required             : true,
+                tooltipMessage       : 'A mobile phone number is required to allow AMP Bank to securely protect your account.',
+                requiredErrorMessage : 'Mobile number is a required field.',
+                patternErrorMessage  : 'Mobile number must be in the format 04nnnnnnnn.'
             },
             {
-                "id": "homeNumber",
-                "label": "Home number",
-                "required": false
+                id       : 'homeNumber',
+                label    : 'Home number',
+                required : false
             }
         ]
     };
-    /* tslint:enable */
 
-    constructor( saveService : SaveService ,
-                 _cd : ChangeDetectorRef ,
+    constructor( saveService : SaveService,
+                 _cd : ChangeDetectorRef,
                  @Optional() private saveCloseService : SaveAndCloseService,
                  scrollService : ScrollService ) {
         super( saveService, _cd, scrollService );
     }
 
-    ngOnInit() {
-        super.setBlockAttributes(this.defaultValues);
-    }
-
     onNext() {
-        if (this.saveCloseService) {
-            this.saveCloseService.updateMobileNumber(this.mobileNumberCmp.control.value);
+        if ( this.saveCloseService ) {
+            this.saveCloseService.updateMobileNumber( this.mobileNumberCmp.control.value );
         }
         super.onNext();
     }
