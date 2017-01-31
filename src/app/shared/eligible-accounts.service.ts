4ce0713eeed8bb9f1e597ccc67671457e1c35e40
Iain Durham <iain_durham@amp.com.au>
2017-01-31 17:32:43 +1100

    BET-4083 cache the result of the eligible accounts call

--- src/app/shared/eligible-accounts.service.ts
+++ src/app/shared/eligible-accounts.service.ts
@@ -13,19 +13,28 @@ import {
 export class EligibleAccountsService {
     public static BASE_URL = Environments.property.ApiCallsBaseUrl;
     public static API_URL = EligibleAccountsService.BASE_URL + '/bett3r/eligible-accounts';
+    private eligibleAccountsSubject : Observable<any>;
 
     constructor( private http : AmpHttpService ) {
     }
 
     public getEligibleAccounts () : Observable<any> {
+        return this.eligibleAccountsSubject ? this.eligibleAccountsSubject : this.createAccountsSubject();
+    }
+
+    private createAccountsSubject () : Observable<any> {
         const headers = new Headers( {
             'Content-Type' : 'application/json',
             'caller'       : Environments.property.ExperienceName
         } );
         const options = new RequestOptions( { headers, body : '' } );
 
-        return this.http
+        this.eligibleAccountsSubject = this.http
             .get( EligibleAccountsService.API_URL, options )
-            .map( ( res ) => res.json() );
+            .map( ( res ) => res.json() )
+            .publishReplay()
+            .refCount();
+
+        return this.eligibleAccountsSubject;
     }
 }
