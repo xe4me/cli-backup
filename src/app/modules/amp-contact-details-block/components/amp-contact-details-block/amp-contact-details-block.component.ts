04f1f7ede165a8a1c5562e1500f844c59b0b831f
Cedric Nicoloso <Cedric_Nicoloso@amp.com.au>
2017-01-31 23:00:37 +1100

    Default props in an external json file

--- src/app/modules/amp-contact-details-block/components/amp-contact-details-block/amp-contact-details-block.component.ts
+++ src/app/modules/amp-contact-details-block/components/amp-contact-details-block/amp-contact-details-block.component.ts
@@ -13,6 +13,7 @@ import {
     SaveAndCloseService
 } from '../../../../services';
 import { AmpInputComponent } from '../../../amp-inputs';
+const defaultBlockProps = require('./amp-contact-details-block.config.json');
 
 @Component( {
     selector        : 'amp-contact-details-block',
@@ -23,32 +24,7 @@ export class AmpContactDetailsBlockComponent extends FormBlock {
 
     @ViewChild( 'mobileNumber' ) mobileNumberCmp : AmpInputComponent;
 
-    protected __custom = {
-        blockTitle : 'And your contact details...',
-        controls   : [
-            {
-                id             : 'emailAddress',
-                label          : 'Email',
-                required       : true,
-                tooltipMessage : 'Confirmation of your application will be sent to this email address.'
-            },
-            {
-                id             : 'mobileNumber',
-                label          : 'Mobile number',
-                required       : true,
-                tooltipMessage : 'A mobile phone number is required to allow AMP Bank to securely protect your account.',
-                errors         : {
-                    required : 'Mobile number is a required field.',
-                    pattern  : 'Mobile number must be in the format 04nnnnnnnn.'
-                }
-            },
-            {
-                id       : 'homeNumber',
-                label    : 'Home number',
-                required : false
-            }
-        ]
-    };
+    protected __custom = defaultBlockProps;
 
     constructor ( saveService : SaveService,
                   _cd : ChangeDetectorRef,
