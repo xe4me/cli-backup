91047ec08f887046b7c1cf8465aac8d2db59dace
Carmen Ye <carmen_ye@amp.com.au>
2017-01-31 15:57:14 +1100

    remove console log

--- src/app/modules/amp-basic-info-block/components/amp-basic-info-block/amp-basic-info-block.component.ts
+++ src/app/modules/amp-basic-info-block/components/amp-basic-info-block/amp-basic-info-block.component.ts
@@ -25,7 +25,6 @@ export class AmpBasicInfoBlockComponent extends FormBlock {
         super( saveService, _cd, scrollService );
     }
     public ngAfterViewInit () {
-        console.log(this.__isRetrieved);
         if (this.__isRetrieved) {
             this.showSaveAndCloseButton();
         }
