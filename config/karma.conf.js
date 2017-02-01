9029d7e4fa1de3b7dbd2c17d9a1ff6b342f894b6
SNGSE2 <sophie_song@amp.com.au>
2017-02-01 11:05:10 +1100

    New changelist

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -105,7 +105,7 @@ module.exports = function (config) {
          * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
          */
         browsers: [
-            'PhantomJS'
+            'Chrome'
         ],
 
         customLaunchers: {
@@ -119,7 +119,7 @@ module.exports = function (config) {
          * Continuous Integration mode
          * if true, Karma captures browsers, runs the tests and exits
          */
-        singleRun: true
+        singleRun: false
     };
 
     if (process.env.TRAVIS) {
--- config/spec-bundle.js
+++ config/spec-bundle.js
@@ -47,7 +47,8 @@ testing.TestBed.initTestEnvironment(
  * we say do this recursively
  */
 //var testContext = require.context('../src/app/redux', true, /\.spec\.ts/);
-var testContext = require.context('../src', true, /\.spec\.ts/);
+//var testContext = require.context('../src', true, /\.spec\.ts/);
+var testContext = require.context('../src/app/sections', true, /\.spec\.ts/);
 
 /*
  * get all the files, for each file, call the context function
