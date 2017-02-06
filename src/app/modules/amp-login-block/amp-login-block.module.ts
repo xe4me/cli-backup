88ef34f465168268c8a5ac93d4e5ae2a3b0875b7
Tristan Le Guillou <tleguillou@octo.com>
2017-02-06 14:33:19 +1100

    Add entryComponents and providers inside the module definition

--- src/app/modules/amp-login-block/amp-login-block.module.ts
+++ src/app/modules/amp-login-block/amp-login-block.module.ts
@@ -4,18 +4,21 @@ import { AmpSharedRootModule } from '../amp-shared-root/amp-shared-root.module';
 import { AmpButtonModule } from '../amp-button';
 import { AmpFormModule } from '../amp-form';
 import { AmpInputsModule } from '../amp-inputs';
+import { LoginStatusService } from '../../services/login/login-status.service';
 
-const DECLARATIONS = [AmpLoginBlockComponent];
+const DECLARATIONS = [ AmpLoginBlockComponent ];
 
-    @NgModule( {
-    declarations : DECLARATIONS ,
-    imports      : [
-        AmpSharedRootModule ,
-        AmpFormModule ,
-        AmpInputsModule ,
+@NgModule( {
+    declarations: DECLARATIONS,
+    entryComponents: DECLARATIONS,
+    imports: [
+        AmpSharedRootModule,
+        AmpFormModule,
+        AmpInputsModule,
         AmpButtonModule
-    ] ,
-    exports      : DECLARATIONS
+    ],
+    exports: DECLARATIONS,
+    providers: [LoginStatusService]
 } )
 
 export class AmpLoginBlockModule {
