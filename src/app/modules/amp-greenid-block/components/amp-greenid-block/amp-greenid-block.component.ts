a48c293f0e708ae27071a1609af7b3bfe806f3f7
SNGSE2 <sophie_song@amp.com.au>
2017-02-07 12:44:48 +1100

    greenId block grape applicant infor from array

--- src/app/modules/amp-greenid-block/components/amp-greenid-block/amp-greenid-block.component.ts
+++ src/app/modules/amp-greenid-block/components/amp-greenid-block/amp-greenid-block.component.ts
@@ -363,7 +363,6 @@ export class AmpGreenIdBlockComponent extends FormBlock implements OnInit, OnDes
      * Get necessary values from the model, from paths given in the exeperience's form-def
      */
     private mapGreenIdModel () {
-        console.log(this.__custom);
         let rootApplicantFDN = '';
         if (this.__custom.rootApplicantFDN && this.__repeaterIndex) {
             rootApplicantFDN = this.__custom.rootApplicantFDN + '.' + this.__repeaterIndex + '.';
