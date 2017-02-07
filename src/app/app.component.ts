745a601cfddd4ce7f94e592ccc7bc6a7b1a0a192
MIlad <smiladhi@gmail.com>
2017-02-07 16:12:25 +1100

    BET-4298 #resolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
     selector: 'app-root',
     styles: [ require( '../styles/all.scss' )],
-    templateUrl: './app.component.html',
+    template: require('./app.component.html'),
     encapsulation: ViewEncapsulation.None
 
 })
