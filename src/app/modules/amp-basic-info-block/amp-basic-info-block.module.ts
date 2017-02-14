4064ad6e0cfbda3c6b526c22804e856d4bb9d94f
Cedric Nicoloso <Cedric_Nicoloso@amp.com.au>
2017-02-14 16:17:58 +1100

    Register basic-info block for prepop

--- src/app/modules/amp-basic-info-block/amp-basic-info-block.module.ts
+++ src/app/modules/amp-basic-info-block/amp-basic-info-block.module.ts
@@ -4,6 +4,9 @@ import { AmpSharedRootModule } from '../amp-shared-root/amp-shared-root.module';
 import { AmpDropdownModule } from '../amp-dropdown';
 import { AmpFormModule } from '../amp-form';
 import { AmpInputsModule } from '../amp-inputs';
+import { CustomerDetailsService } from '../../services/customer-details/customer-details.service';
+import { LoginStatusService } from '../../services/login/login-status.service';
+import { PrepopAmpBasicInfoService } from './services/prepop-amp-basic-info.service';
 
 const DECLARATIONS = [ AmpBasicInfoBlockComponent ];
 
@@ -15,6 +18,11 @@ const DECLARATIONS = [ AmpBasicInfoBlockComponent ];
         AmpDropdownModule,
         AmpFormModule,
         AmpInputsModule
+    ],
+    providers : [
+        CustomerDetailsService,
+        LoginStatusService,
+        PrepopAmpBasicInfoService
     ],
     exports         : DECLARATIONS
 } )
