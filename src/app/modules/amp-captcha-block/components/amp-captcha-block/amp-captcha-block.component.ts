ce91cfc98085ecd9d43105f59452e7f817fd4611
SNGSE2 <sophie_song@amp.com.au>
2017-02-14 15:03:00 +1100

    add setTimeOut

--- src/app/modules/amp-captcha-block/components/amp-captcha-block/amp-captcha-block.component.ts
+++ src/app/modules/amp-captcha-block/components/amp-captcha-block/amp-captcha-block.component.ts
@@ -71,8 +71,8 @@ export class AmpCaptchaBlockComponent extends FormBlock implements AfterViewInit
         if (!this.isDestroyed) {
             this.isDestroyed = true;
             this._cd.markForCheck();
+            this.__removeSelf( this.vcf );
             setTimeout( () => {
-                this.__removeSelf( this.vcf );
                 this.onNext();
             });
         }
