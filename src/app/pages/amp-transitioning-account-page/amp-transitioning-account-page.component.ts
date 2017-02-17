ce7c02df2c24dbe16e41a6b731672d61050535e5
Carmen Ye <carmen_ye@amp.com.au>
2017-02-17 16:39:24 +1100

    AMPSA-140 fix lint

--- src/app/pages/amp-transitioning-account-page/amp-transitioning-account-page.component.ts
+++ src/app/pages/amp-transitioning-account-page/amp-transitioning-account-page.component.ts
@@ -4,18 +4,17 @@ import {
     Input
 } from '@angular/core';
 
-
 @Component( {
     selector        : 'amp-transitioning-account-page' ,
     template        : require( './amp-transitioning-account-page.component.html' ) ,
     styles          : [ require( './amp-transitioning-account-page.component.scss' ) ],
     changeDetection : ChangeDetectionStrategy.OnPush
 } )
+
 export class AmpTransitioningAccountPageComponent {
 
     @Input() title;
     @Input() message;
-  
 
     public onFinishButtonClick() {
         console.log('onFinishButtonClick');
