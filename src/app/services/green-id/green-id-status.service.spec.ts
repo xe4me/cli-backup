57cd7ea014d2e3c266d5350279954181f07d7dfa
Mingxuan Li <mingxuan.li.au@gmail.com>
2017-02-17 12:07:14 +1100

    modify the unit test

--- src/app/services/green-id/green-id-status.service.spec.ts
+++ src/app/services/green-id/green-id-status.service.spec.ts
@@ -1,4 +1,5 @@
-import {GreenIdStatusService} from './green-id-status.service';
+import { Subscription } from 'rxjs';
+import { GreenIdStatusService } from './green-id-status.service';
 
 describe( 'Service: GreenId Status', () => {
     let greenIdStatusService : GreenIdStatusService;
@@ -19,8 +20,17 @@ describe( 'Service: GreenId Status', () => {
         });
     });
 
-    describe ( 'WHEN the green id verification is triggered for Applicant 2', () => {
-        it ( 'SHOULD trigger the subscription with green id verification result set to be true for Applicant 2 ', ()  => {
+    describe ( 'WHEN the green id verification is triggered for Applicant 1 and 2', () => {
+        it ( 'SHOULD trigger the subscription with green id verification result set to be true for Applicant 1 and 2 ', ()  => {
+            let applicantOneIndex = 1;
+            greenIdStatusService.greenIdVerified( applicantOneIndex );
+            let greenIdSub : Subscription = greenIdStatusService
+                .isGreenIdVerified()
+                .subscribe( ( greenIdResults ) => {
+                    expect( greenIdResults[applicantOneIndex] ).toBe( true );
+                    greenIdSub.unsubscribe();
+                });
+
             let applicantTwoIndex = 2;
             greenIdStatusService.greenIdVerified( applicantTwoIndex );
             return greenIdStatusService
